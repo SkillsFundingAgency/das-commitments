d8d566496da62e46f2a3cc37782eeb8a4b99bf08
ijrussell <i.j.russell@tesco.net>
2016-08-22 13:09:13 +0100

    Created initial database and tables

---
+++ src/SFA.DAS.Commitments.Database/Tables/Commitment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Commitment]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [EmployerAccountId] BIGINT NOT NULL, 
+    [LegalEntityId] BIGINT NOT NULL, 
+    [ProviderId] BIGINT NULL
+)
