b6f109cf4fd1c101090ac36fc150aa7a37b58214
ijrussell <i.j.russell@tesco.net>
2016-08-24 07:42:24 +0100

    initial commit

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/CommitmentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.ProviderApprenticeshipsService.Domain;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models
+{
+    public class CommitmentViewModel
+    {
+        public List<CommitmentView> Commitments { get; set; }
+    }
+}
