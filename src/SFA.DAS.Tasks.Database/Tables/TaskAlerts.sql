fc39e1e69e1beb5413d2e0aba9b3e6228a022ce7
Mark Gwilliam <github@markgwilliam.com>
2016-09-01 11:13:36 +0100

    added database project for tasks

---
+++ src/SFA.DAS.Tasks.Database/Tables/TaskAlerts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TaskAlerts]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [TaskId] BIGINT NOT NULL, 
+    [UserId] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [CreatedOn] DATETIME NOT NULL, 
+    CONSTRAINT [FK_TaskAlerts_Tasks] FOREIGN KEY ([TaskId]) REFERENCES [Tasks]([Id]), 
+
+)
