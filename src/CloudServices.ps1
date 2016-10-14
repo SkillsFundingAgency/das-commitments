7797f9c03b12250492cc0508f943f5e2db39421c
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-14 16:54:59 +0100

    Update CloudServices.ps1

--- src/CloudServices.ps1
+++ src/CloudServices.ps1
@@ -1,20 +1,26 @@
-﻿ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
  
- $CloudServiceArray= @("das-$env:enviroment-task-cs","das-$env:enviroment-comt-cs","das-$env:enviroment-pas-cs")
- 
- foreach ($CloudService1 in $Cloudservicearray)
+ $CloudServiceArray= @("das-daniel10-task-cs","das-daniel10-comt-cs")
+ $detailarray= @(Get-AzureService)
+
+    foreach ($CloudService1 in $Cloudservicearray)
     {
-    $details= @(Get-AzureService -ServiceName $CloudService1)
-    If ($details.count -eq 1)
+  
+    foreach ($name in $detailarray){
+    If ($name.ServiceName -eq $CloudService1)
     {
-    write-host "Cloud Service Already exist"
+    write-host $name.Servicename "Exists"
+  
     }
-    else 
-    {
-    Write-Host "Creating $cloudservice1 Service"
-    New-AzureService -ServiceName "$cloudservice1" -Label "$Cloudservice1" -Location "North Europe" 
+   
     }
-    }
-    
+     if ($name.ServiceName -ne $CloudService1){
+
+
+   write-host "Building" $CloudService1
+   New-AzureService -ServiceName $CloudService1 -Label $CloudService1 -Location "North Europe"
+  
+   }
+   }
