593143e37152d01fc890fb67682c38ee2447158f
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-14 15:35:17 +0100

    Add files via upload

---
+++ src/CloudServices.ps1
@@ -0,0 +1,20 @@
+﻿ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
+ 
+ $CloudServiceArray= @("das-$env:enviroment-task-cs","das-$env:enviroment-comt-cs","das-$env:enviroment-pas-cs")
+ 
+ foreach ($CloudService1 in $Cloudservicearray)
+    {
+    $details= @(Get-AzureService -ServiceName $CloudService1 -ErrorAction SilentlyContinue)
+    If ($details.count -eq 1)
+    {
+    write-host "Cloud Service Already exist"$details.Servicename
+    }
+    else 
+    {
+    Write-Host "Creating $cloudservice1 Service"
+    New-AzureService -ServiceName "$cloudservice1" -Label "$Cloudservice1"-Location "North Europe" 
+    }
+    }
+    
---
+++ src/ResourceGroup.ps1
@@ -0,0 +1,29 @@
+﻿#Resource Group Creation
+
+#SPI Details
+
+$secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
+
+$task= "das-$env:enviroment-task-rg"
+$comt="das-$env:enviroment-comt-rg"
+$pas="das-$env:enviroment-pas-rg"
+
+#Arrays
+$ResourceGroupArray = @("$task","$comt","$pas")
+
+#Resource Group Creation Array
+foreach ($result in $ResourceGroupArray){
+Get-AzureRmResourceGroup -Name $result -ev notPresent -ea 0
+if ($notPresent)
+{
+    Write-Host -ForegroundColor Yellow "Creating Resource Group $result"
+    New-AzureRmResourceGroup -Name $result -Location "North Europe" -Force
+}
+else
+{
+    Write-Host -ForegroundColor Yellow "Group Exisits $result"
+}
+
+}
