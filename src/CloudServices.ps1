1d192c8a12d64c60c3f8c7dcaf594b0b4dc0b91f
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-14 17:01:09 +0100

    Update CloudServices.ps1

--- src/CloudServices.ps1
+++ src/CloudServices.ps1
@@ -2,7 +2,7 @@ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
  
- $CloudServiceArray= @("das-daniel10-task-cs","das-daniel10-comt-cs")
+$CloudServiceArray= @("das-$env:enviroment-task-cs","das-$env:enviroment-comt-cs","das-$env:enviroment-pas-cs")
  $detailarray= @(Get-AzureService)
 
     foreach ($CloudService1 in $Cloudservicearray)
