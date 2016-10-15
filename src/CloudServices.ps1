e29ab81ca06697f9b7d0d1b5f78490aefe83d198
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-15 12:19:54 +0100

    Update CloudServices.ps1

--- src/CloudServices.ps1
+++ src/CloudServices.ps1
@@ -1,26 +1,15 @@
 $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
- 
-$CloudServiceArray= @("das-$env:enviroment-task-cs","das-$env:enviroment-comt-cs","das-$env:enviroment-pas-cs")
+
+ $CloudServiceArray= @("das-$env:enviroment-task-cs","das-$env:enviroment-comt-cs","das-$env:enviroment-pas-cs")
  $detailarray= @(Get-AzureService)
 
-    foreach ($CloudService1 in $Cloudservicearray)
-    {
-  
-    foreach ($name in $detailarray){
-    If ($name.ServiceName -eq $CloudService1)
-    {
-    write-host $name.Servicename "Exists"
-  
+  Compare-Object -ReferenceObject $detailarray.ServiceName  -DifferenceObject $CloudServiceArray | 
+    Where-Object { $_.SideIndicator -eq '=>' } | 
+    ForEach-Object  { 
+       
+    New-AzureService -ServiceName $_.InputObject -Label $_.InputObject -Location "North Europe"
     }
-   
-    }
-     if ($name.ServiceName -ne $CloudService1){
 
 
-   write-host "Building" $CloudService1
-   New-AzureService -ServiceName $CloudService1 -Label $CloudService1 -Location "North Europe"
-  
-   }
-   }
