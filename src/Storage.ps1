b62b152d6f865160aa5d0633ec8cdc68dcba8fc3
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-15 12:47:28 +0100

    Update Storage.ps1

--- src/Storage.ps1
+++ src/Storage.ps1
@@ -1,4 +1,4 @@
-﻿$secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 
@@ -7,19 +7,18 @@ $Storagecomt='comtstr'
 $Storagepas='passtr'
 
 $StorageArray= @("das$env:enviroment$StorageTask","das$env:enviroment$StorageComt","das$env:enviroment$Storagepas")
+$GetStorage=@(Get-AzureStorageAccount)
 
-
-foreach ($result2 in $StorageArray){
-$details =@(Get-AzureStorageAccount -StorageAccountName $result2 -ErrorAction SilentlyContinue)
-
-if ($details.count -eq 1)
-{
-    Write-Host -ForegroundColor Yellow "Storage Group $Result2 already deployed"
-}
-else
-{
-     Write-Host -ForegroundColor Yellow "Creating $result2 Storage"
-    New-AzureStorageAccount -StorageAccountName $result2 -label $result2 -Location "North Europe" 
+ Compare-Object -ReferenceObject $GetStorage.StorageAccountName -DifferenceObject $StorageArray | 
+    Where-Object { $_.SideIndicator -eq '=>' } | 
+    
+    ForEach-Object  { 
+    try {
+    #write-host $_.InputObject
+    New-AzureStorageAccount -StorageAccountName $_.InputObject -label $_.InputObject -Location "North Europe" -ErrorAction Stop
     Start-Sleep -Seconds 120
-}
-}
+    }
+    catch {
+    write-host $_.Exception.Message
+    }
+    }
