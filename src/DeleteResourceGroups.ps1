288d89fb70b632a643577bfd8648bd470c1b9ba3
Dan Lewis <dan.lewis@code88.co.uk>
2016-10-16 12:11:54 +0100

    Add files via upload
    Deleted Empty Resource Groups

---
+++ src/DeleteResourceGroups.ps1
@@ -0,0 +1,14 @@
+﻿$secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
+
+$ResourceGroupDetails = Get-AzureRmResourceGroup 
+foreach($RGDetail in $ResourceGroupDetails) 
+{
+    $count = ((Find-AzureRmResource -ResourceGroupNameContains $RGDetail.ResourceGroupName).Name).count 
+    if($count -eq 0) 
+    { 
+        Write-Output "Deleting", $RGDetail 
+        Remove-AzureRmResourceGroup -Name $RGDetail.ResourceGroupName -Force
+    } 
+}
