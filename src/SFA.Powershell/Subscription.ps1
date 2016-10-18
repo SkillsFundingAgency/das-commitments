77c7c27ccaf5fbb56262617bcb0078af479c91f3
Daniel Lewis <Dan@Daniels-MBP.home>
2016-10-18 21:06:37 +0100

    Update to find cs

--- src/SFA.Powershell/Subscription.ps1
+++ src/SFA.Powershell/Subscription.ps1
@@ -3,3 +3,5 @@ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 Get-AzureSubscription
+
+Get-AzureRmResource
