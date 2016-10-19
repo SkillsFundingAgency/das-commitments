423aa913447080cb0df3fdf5ee4882c44698e809
Daniel Lewis <Dan@Daniels-MacBook-Pro.local>
2016-10-19 15:06:02 +0100

    update

--- src/SFA.Powershell/CloudService.ps1
+++ src/SFA.Powershell/CloudService.ps1
@@ -9,6 +9,7 @@ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 
+
 Set-AzureRmContext -SubscriptionName SFA-Das-comt-dev
 
 Write-Host "Preparing cloud service '$ServiceName' in resource group '$ResourceGroupName' in '$Location'..."
@@ -30,6 +31,7 @@ Function WaitForService {
             $cloudService = Get-AzurermResource -ResourceName "$ServiceName" -ResourceGroupName "$ServiceName"
             Write-Host "[service ready]" -ForegroundColor Green
             return $cloudService
+            write-host $cloudservice.resourceid
         }
         catch
         {
@@ -56,6 +58,7 @@ else
     
     $cloudService = WaitForService($ServiceName, 50); 
     $cloudServiceId = $cloudService.ResourceId;
+    write-host $cloudServiceId
     
     Write-Host "Moving '$ServiceName' ($cloudServiceId) to resource group '$ResourceGroupName'..."
     Move-AzureRmResource -DestinationResourceGroupName "$ResourceGroupName" -ResourceId $cloudServiceID -Force
