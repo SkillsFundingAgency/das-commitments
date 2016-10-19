5a14c6e3ceb7515f233a432b8a6ca58e3a863f2f
Daniel Lewis <Dan@Daniels-MacBook-Pro.local>
2016-10-19 14:21:42 +0100

    Syntax Change

--- src/SFA.Powershell/CloudService.ps1
+++ src/SFA.Powershell/CloudService.ps1
@@ -21,10 +21,10 @@ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 
+Set-AzureRmContext -SubscriptionName $env:subscription
 
 Write-Host "Preparing cloud service '$ServiceName' in resource group '$ResourceGroupName' in '$Location'..."
 
-Select-AzureSubscription -SubscriptionName SFA-DAS-Comt-Dev
 
 Function WaitForService {
     Param(
@@ -61,7 +61,7 @@ else
 {
     
     Write-Host "No service exists, creating new..."
-   Set-AzureRmContext -Confirm -SubscriptionName SFA-DAS-Comt-Dev
+
     New-AzureService -ServiceName $ServiceName -Location "$Location"
     
     Write-Host "Looking for the new cloud service..."
