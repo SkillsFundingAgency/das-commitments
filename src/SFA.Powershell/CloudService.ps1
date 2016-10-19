3cbd5ed59ccbd5c88efec7234ddbe0c13ebd2654
Daniel Lewis <Dan@Daniels-MacBook-Pro.local>
2016-10-19 14:49:27 +0100

    revised

--- src/SFA.Powershell/CloudService.ps1
+++ src/SFA.Powershell/CloudService.ps1
@@ -9,7 +9,7 @@ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 
-Set-AzureRmContext -SubscriptionName $env:subscription
+Set-AzureRmContext -SubscriptionName SFA-Das-comt-dev
 
 Write-Host "Preparing cloud service '$ServiceName' in resource group '$ResourceGroupName' in '$Location'..."
 
@@ -49,7 +49,7 @@ else
 {
     
     Write-Host "No service exists, creating new..."
-
+    Set-AzureRmContext -SubscriptionName SFA-Das-comt-dev
     New-AzureService -ServiceName $ServiceName -Location "$Location"
     
     Write-Host "Looking for the new cloud service..."
