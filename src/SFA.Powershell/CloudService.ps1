1d9a2d4b3304d0c4462cfcc5e8575eb124b06a13
Daniel Lewis <Dan@Daniels-MacBook-Pro.local>
2016-10-20 09:26:42 +0100

    SPI

--- src/SFA.Powershell/CloudService.ps1
+++ src/SFA.Powershell/CloudService.ps1
@@ -16,79 +16,74 @@ $tenantId = "1a92889b-8ea1-4a16-8132-347814051567"
 $secPwd = ConvertTo-SecureString $pwd -AsPlainText -Force;
 $credentials = New-Object System.Management.Automation.PSCredential ($uid, $secPwd);
 
-
-
-
 Add-AzureRmAccount -ServicePrincipal -Tenant $tenantId -Credential $credentials;
-
-
 
 
 Set-AzureRmContext -SubscriptionName $env:subscription
 Set-AzureSubscription –SubscriptionName $env:subscription
 
-#Write-Host "kPreparing cloud service '$ServiceName' in resource group '$ResourceGroupName' in '$Location'..."
-#            k
-#            k
-#            k
-#            k
-#$service =  Get-AzureService -ServiceName "$ServiceName" -ErrorAction SilentlyContinue
-#write-host $service.Label
-#
-#if($service)
-#{
-#	Write-Host -ForegroundColor Yellow "Service Already Exists'$ServiceName'";
-#
-#}
-#else
-#{
-#    
-#    Write-Host "No service exists, creating new..."
-#    Set-AzureRmContext -SubscriptionName $env:subscription
-#    Set-AzureSubscription –SubscriptionName $env:subscription
-#
-#    #New-AzureService -ServiceName $ServiceName -Location "$Location"
-#    
-#    Write-Host "Looking for the new cloud service..."
-#    
-#    $cloudService = WaitForService($ServiceName, 50); 
-#    $cloudServiceId = $cloudService.ResourceId;
-#
-#    
-#    Write-Host "Moving '$ServiceName' ($cloudServiceId) to resource group '$ResourceGroupName'..."
-#    #Move-AzureRmResource -DestinationResourceGroupName "$ResourceGroupName" -ResourceId $cloudService.ResourceId -Force
-#    
-#    Write-Host "Removing resoure group '$ServiceName'..."
-#    #Remove-AzureRmResourceGroup -Name "$ServiceName" -Force
-#}
-#
-#Write-Host "[service online]" -ForegroundColor Green
-#
-#
-#
-#
-#Function WaitForService {
-#    Param(
-#        [string]$ResourceGroupName,
-#        
-#        [int]$Retries = 10
-#    )
-#
-#    $tried = 0;
-#    
-#    while($tried -le $Retries)
-#    {
-#        try
-#        {
-#            $cloudService = Get-AzurermResource -ResourceName "$ServiceName" -ResourceGroupName "$ServiceName"
-#            Write-Host "[service ready]" -ForegroundColor Green
-#            return $cloudService
-#            write-host $cloudservice.resourceid
-#        }
-#        catch
-#        {
-#            Write-Host "[service not ready yet]" -ForegroundColor Red
-#            Start-Sleep 5
-#        }
-#    }
-#}
+Write-Host "Preparing cloud service '$ServiceName' in resource group '$ResourceGroupName' in '$Location'..."
+            
+            
+            
+            
+$service =  Get-AzureService -ServiceName "$ServiceName" -ErrorAction SilentlyContinue
+write-host $service.Label
+
+if($service)
+{
+	Write-Host -ForegroundColor Yellow "Service Already Exists'$ServiceName'";
+
+}
+else
+{
+    
+    Write-Host "No service exists, creating new..."
+    Set-AzureRmContext -SubscriptionName $env:subscription
+    Set-AzureSubscription –SubscriptionName $env:subscription
+
+    #New-AzureService -ServiceName $ServiceName -Location "$Location"
+    
+    Write-Host "Looking for the new cloud service..."
+    
+    #$cloudService = WaitForService($ServiceName, 50); 
+    $cloudServiceId = $cloudService.ResourceId;
+
+    
+    Write-Host "Moving '$ServiceName' ($cloudServiceId) to resource group '$ResourceGroupName'..."
+    #Move-AzureRmResource -DestinationResourceGroupName "$ResourceGroupName" -ResourceId $cloudService.ResourceId -Force
+    
+    Write-Host "Removing resoure group '$ServiceName'..."
+    #Remove-AzureRmResourceGroup -Name "$ServiceName" -Force
+}
+
+Write-Host "[service online]" -ForegroundColor Green
+
+
+
+
+Function WaitForService {
+    Param(
+        [string]$ResourceGroupName,
+        
+        [int]$Retries = 10
+    )
+
+    $tried = 0;
+    
+    while($tried -le $Retries)
+    {
+        try
+        {
+            $cloudService = Get-AzurermResource -ResourceName "$ServiceName" -ResourceGroupName "$ServiceName"
+            Write-Host "[service ready]" -ForegroundColor Green
+            return $cloudService
+            write-host $cloudservice.resourceid
+        }
+        catch
+        {
+            Write-Host "[service not ready yet]" -ForegroundColor Red
+            Start-Sleep 5
+        }
+    }
+}
