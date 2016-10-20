9c15786cebe1c83239b29a3f0e116a14ab4a8d15
Daniel Lewis <Dan@Daniels-MacBook-Pro.local>
2016-10-20 09:19:13 +0100

    Update

--- src/SFA.Powershell/CloudServiceAll.ps1
+++ src/SFA.Powershell/CloudServiceAll.ps1
@@ -1,7 +1,12 @@
-﻿ $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
-$mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
+﻿# $secpasswd = ConvertTo-SecureString "$env:spipwd" -AsPlainText -Force
+#$mycreds = New-Object System.Management.Automation.PSCredential ("e8d34963-8a5c-4d62-8778-0d47ee0f22fa",$secpasswd)
 #Login-AzureRmAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
 Add-AzureAccount -ServicePrincipal -Tenant 1a92889b-8ea1-4a16-8132-347814051567 -Credential $mycreds
+
+
+
+
+
 
 Set-AzureRmContext -SubscriptionName $env:subscription
 Set-AzureSubscription –SubscriptionName $env:subscription
