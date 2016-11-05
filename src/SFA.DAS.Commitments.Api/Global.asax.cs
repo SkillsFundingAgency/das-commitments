e6ca4c0011d7fababb4bab0df6bfd0a1906477dd
Lee Wadhams <lee.wadhams@valtech.com>
2016-11-05 12:34:47 +0000

    Change logging to use Redis

--- src/SFA.DAS.Commitments.Api/Global.asax.cs
+++ src/SFA.DAS.Commitments.Api/Global.asax.cs
@@ -1,13 +1,23 @@
-﻿using System.Web.Http;
-using System.Web.Security;
+﻿using System;
+using System.Web.Http;
+using NLog;
 
 namespace SFA.DAS.Commitments.Api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            Logger.Error(exception);
         }
     }
 }
