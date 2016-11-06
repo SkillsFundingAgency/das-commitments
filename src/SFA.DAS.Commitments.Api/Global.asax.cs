1cd3bf4f4ffc88ee49d37a4fb41dfceee3b4efa0
Lee Wadhams <lee.wadhams@valtech.com>
2016-11-06 16:52:29 +0000

    Updated NLog.Redis package

--- src/SFA.DAS.Commitments.Api/Global.asax.cs
+++ src/SFA.DAS.Commitments.Api/Global.asax.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Web.Http;
 using NLog;
+using NLog.Targets;
 
 namespace SFA.DAS.Commitments.Api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        private static RedisTarget _redisTarget; // Required to ensure assembly is copied to output.
 
         protected void Application_Start()
         {
