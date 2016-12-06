1d0f3407cfa65d7f7c92fb69826b9d3da8b7683b
Mark Gwilliam <github@markgwilliam.com>
2016-12-06 15:51:50 +0000

    updated summary queries to support both employer and provider

--- src/SFA.DAS.Commitments.Database/Views/ApprenticeshipSummary.sql
+++ src/SFA.DAS.Commitments.Database/Views/ApprenticeshipSummary.sql
@@ -1,27 +1,43 @@
-﻿
-CREATE VIEW [dbo].[ApprenticeshipSummary]
+﻿CREATE VIEW [dbo].[ApprenticeshipSummary]
 AS 
 
 SELECT 
 	c.EmployerAccountId, c.ProviderId, c.Reference, 
-	a.*, CAST(
-	(
-		SELECT COUNT(*)
-		FROM
-			Apprenticeship apps
-		WHERE 
-			apps.id = a.id AND 
-			apps.FirstName IS NOT NULL AND 
-			apps.LastName IS NOT NULL AND 
-			apps.Cost IS NOT NULL AND 
-			apps.StartDate IS NOT NULL AND 
-			apps.EndDate IS NOT NULL AND 
-			apps.TrainingCode IS NOT NULL AND 
-			apps.DateOfBirth IS NOT NULL AND 
-			apps.NINumber IS NOT NULL
-		) AS BIT) AS 'CanApproveApprenticeship'
+	a.*,
+	CASE 
+		WHEN
+			a.FirstName IS NOT NULL AND 
+			a.LastName IS NOT NULL AND 
+			a.Cost IS NOT NULL AND 
+			a.StartDate IS NOT NULL AND 
+			a.EndDate IS NOT NULL AND 
+			a.TrainingCode IS NOT NULL AND 
+			a.DateOfBirth IS NOT NULL AND 
+			a.NINumber IS NOT NULL
+		THEN
+			1
+		ELSE
+			0
+	END AS 'EmployerCanApproveApprenticeship',
+	CASE 
+		WHEN
+			a.FirstName IS NOT NULL AND 
+			a.LastName IS NOT NULL AND 
+			a.ULN IS NOT NULL AND -- ULN is required for provider approval
+			a.Cost IS NOT NULL AND 
+			a.StartDate IS NOT NULL AND 
+			a.EndDate IS NOT NULL AND 
+			a.TrainingCode IS NOT NULL AND 
+			a.DateOfBirth IS NOT NULL AND 
+			a.NINumber IS NOT NULL
+		THEN
+			1
+		ELSE
+			0
+	END AS 'ProviderCanApproveApprenticeship'
 
-	FROM Apprenticeship a
+	FROM 
+		Apprenticeship a
 	INNER JOIN 
 		Commitment c
 	ON 
