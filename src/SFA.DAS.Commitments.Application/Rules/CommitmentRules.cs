e692f080430cb3b04436b6f4380bcc642ea37005
Mark Gwilliam <github@markgwilliam.com>
2016-12-22 11:42:07 +0000

    code tidy

--- src/SFA.DAS.Commitments.Application/Rules/CommitmentRules.cs
+++ src/SFA.DAS.Commitments.Application/Rules/CommitmentRules.cs
@@ -1,22 +1,23 @@
-﻿namespace SFA.DAS.Commitments.Application.Rules
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.Commitments.Api.Types;
+using Apprenticeship = SFA.DAS.Commitments.Domain.Entities.Apprenticeship;
+
+namespace SFA.DAS.Commitments.Application.Rules
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
-    using Api.Types;
-    
-    using Apprenticeship = SFA.DAS.Commitments.Domain.Entities.Apprenticeship;
-
     public class CommitmentRules : ICommitmentRules
     {
         public AgreementStatus DetermineAgreementStatus(List<Apprenticeship> apprenticeships)
         {
             var first = apprenticeships?.FirstOrDefault();
+
             if (first == null)
             {
                 return AgreementStatus.NotAgreed;
             }
-            return (AgreementStatus)first.AgreementStatus;
+
+            return (AgreementStatus) first.AgreementStatus;
         }
     }
 }
