4eb38b63be67b7603ebc9a66b22d96d3a94a14c6
Lee Wadhams <lee.wadhams@valtech.com>
2017-02-20 12:10:05 +0000

    Update test for Emp/Prov reference validation

--- src/SFA.DAS.Commitments.Application.UnitTests/Commands/SharedValidation/Apprenticeships/ValidatingEmployerReference.cs
+++ src/SFA.DAS.Commitments.Application.UnitTests/Commands/SharedValidation/Apprenticeships/ValidatingEmployerReference.cs
@@ -6,6 +6,14 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.SharedValidation.Ap
     [TestFixture]
     public sealed class ValidatingEmployerReference : ApprenticeshipValidationTestBase
     {
-        
+        [Test]
+        public void ThenEmployerReferenceOver20CharacterIsInvalid()
+        {
+            ExampleValidApprenticeship.EmployerRef = "opopopopopopopopopopa"; // 21 characters
+
+            var result = Validator.Validate(ExampleValidApprenticeship);
+
+            result.IsValid.Should().BeFalse();
+        }
     }
 }
--- src/SFA.DAS.Commitments.Application.UnitTests/Commands/SharedValidation/Apprenticeships/ValidatingProviderReference.cs
+++ src/SFA.DAS.Commitments.Application.UnitTests/Commands/SharedValidation/Apprenticeships/ValidatingProviderReference.cs
@@ -6,6 +6,14 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.SharedValidation.Ap
     [TestFixture]
     public sealed class ValidatingProviderReference : ApprenticeshipValidationTestBase
     {
-        
+        [Test]
+        public void ThenProviderReferenceOver20CharacterIsInvalid()
+        {
+            ExampleValidApprenticeship.ProviderRef = "opopopopopopopopopopa"; // 21 characters
+
+            var result = Validator.Validate(ExampleValidApprenticeship);
+
+            result.IsValid.Should().BeFalse();
+        }
     }
 }
