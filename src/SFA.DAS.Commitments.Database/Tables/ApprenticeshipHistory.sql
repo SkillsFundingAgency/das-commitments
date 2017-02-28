d877d84c6ca7537427d7acce283a02a0f0e167c2
Lee Wadhams <lee.wadhams@valtech.com>
2017-02-28 13:19:10 +0000

    Change length of UserId field in History tables to match IDAMS limit

--- src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipHistory.sql
+++ src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipHistory.sql
@@ -2,7 +2,7 @@
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [ApprenticeshipId] BIGINT NOT NULL, 
-    [UserId] NVARCHAR(150) NOT NULL, 
+    [UserId] NVARCHAR(50) NOT NULL, 
     [UpdatedByRole] SMALLINT NOT NULL, 
     [ChangeType] SMALLINT NOT NULL,
     [CreatedOn] DATETIME NOT NULL, 
