a7d5404e24bcb788d7225812b980bd301f615da0
Michael Young <mick.young1981@googlemail.com>
2017-03-22 11:51:36 +0000

    Added missing Apprenticeship Events service tests.

---
+++ src/SFA.DAS.Commitments.Infrastructure.UnitTests/Services/ApprenticeshipEventsTests/ApprenticeshipEventsTestsBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Domain.Entities;
+using SFA.DAS.Commitments.Domain.Interfaces;
+using SFA.DAS.Commitments.Infrastructure.Services;
+using SFA.DAS.Events.Api.Client;
+
+namespace SFA.DAS.Commitments.Infrastructure.UnitTests.Services.ApprenticeshipEventsTests
+{
+    public abstract class ApprenticeshipEventsTestsBase
+    {
+        protected Mock<IEventsApi> EventsApi;
+        protected Mock<ICommitmentsLogger> CommitmentsLogger;
+        protected ApprenticeshipEvents Service;
+        protected Commitment Commitment;
+        protected Apprenticeship Apprenticeship;
+
+        [SetUp]
+        public void Arrange()
+        {
+            EventsApi = new Mock<IEventsApi>();
+            CommitmentsLogger = new Mock<ICommitmentsLogger>();
+
+            Service = new ApprenticeshipEvents(EventsApi.Object, CommitmentsLogger.Object);
+
+            Commitment = new Commitment
+            {
+                ProviderId = 123,
+                EmployerAccountId = 987,
+                LegalEntityId = "LE ID",
+                LegalEntityName = "LE Name",
+                LegalEntityOrganisationType = OrganisationType.CompaniesHouse
+            };
+
+            Apprenticeship = new Apprenticeship
+            {
+                EndDate = DateTime.Now.AddYears(3),
+                StartDate = DateTime.Now.AddDays(1),
+                Cost = 123.45m,
+                TrainingCode = "TRCODE",
+                AgreementStatus = AgreementStatus.BothAgreed,
+                Id = 34875,
+                ULN = "ULN",
+                PaymentStatus = PaymentStatus.Active,
+                TrainingType = TrainingType.Framework,
+                PaymentOrder = 213
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.Commitments.Infrastructure.UnitTests/Services/ApprenticeshipEventsTests/WhenIPublishAnEvent.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Domain.Entities;
+using SFA.DAS.Events.Api.Types;
+using AgreementStatus = SFA.DAS.Events.Api.Types.AgreementStatus;
+using PaymentStatus = SFA.DAS.Events.Api.Types.PaymentStatus;
+
+namespace SFA.DAS.Commitments.Infrastructure.UnitTests.Services.ApprenticeshipEventsTests
+{
+    [TestFixture]
+    public class WhenIPublishAnEvent : ApprenticeshipEventsTestsBase
+    {
+        private string _event = "Test";
+
+        [Test]
+        public async Task AndTheTrainingTypeIsFrameworkThenTheEventIsPublishedWithTheCorrectTrainingType()
+        {
+            Apprenticeship.TrainingType = TrainingType.Framework;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasPublished(_event);
+        }
+
+        [Test]
+        public async Task AndTheTrainingTypeIsStandardThenTheEventIsPublishedWithTheCorrectTrainingType()
+        {
+            Apprenticeship.TrainingType = TrainingType.Standard;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasPublished(_event);
+        }
+
+        [Test]
+        public async Task AndTheUlnIsNotProvidedThenTheEventIsPublishedWithADefaultValue()
+        {
+            Apprenticeship.ULN = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasPublished(_event);
+        }
+
+        [Test]
+        public async Task AndTheProviderIdIsNotCompleteThenTheEventIsNotPublished()
+        {
+            Commitment.ProviderId = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasNotPublished();
+        }
+
+        [Test]
+        public async Task AndTheEndDateIsNotCompleteThenTheEventIsNotPublished()
+        {
+            Apprenticeship.EndDate = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasNotPublished();
+        }
+
+        [Test]
+        public async Task AndTheStartDateIsNotCompleteThenTheEventIsNotPublished()
+        {
+            Apprenticeship.StartDate = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasNotPublished();
+        }
+
+        [Test]
+        public async Task AndTheCostIsNotCompleteThenTheEventIsNotPublished()
+        {
+            Apprenticeship.Cost = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasNotPublished();
+        }
+
+        [Test]
+        public async Task AndTheTrainingCodeIsNotCompleteThenTheEventIsNotPublished()
+        {
+            Apprenticeship.TrainingCode = null;
+
+            await Service.PublishEvent(Commitment, Apprenticeship, _event);
+
+            VerifyEventWasNotPublished();
+        }
+
+        private void VerifyEventWasNotPublished()
+        {
+            CommitmentsLogger.Verify(x => x.Info(It.IsAny<string>(), It.IsAny<long?>(), It.IsAny<long?>(), It.IsAny<long?>(), It.IsAny<long?>()), Times.Never);
+            EventsApi.Verify(x => x.CreateApprenticeshipEvent(It.IsAny<ApprenticeshipEvent>()), Times.Never);
+        }
+
+        private void VerifyEventWasPublished(string @event)
+        {
+            CommitmentsLogger.Verify(x => x.Info($"Create apprenticeship event: {@event}", null, null, 0, Apprenticeship.Id), Times.Once);
+            EventsApi.Verify(x => x.CreateApprenticeshipEvent(It.Is<ApprenticeshipEvent>(y => EventMatchesParameters(y, @event))), Times.Once);
+        }
+
+        private bool EventMatchesParameters(ApprenticeshipEvent apprenticeshipEvent, string @event)
+        {
+            return apprenticeshipEvent.AgreementStatus == (AgreementStatus)Apprenticeship.AgreementStatus &&
+                   apprenticeshipEvent.ApprenticeshipId == Apprenticeship.Id &&
+                   apprenticeshipEvent.EmployerAccountId == Commitment.EmployerAccountId.ToString() &&
+                   apprenticeshipEvent.Event == @event &&
+                   apprenticeshipEvent.LearnerId == (Apprenticeship.ULN ?? "NULL") &&
+                   apprenticeshipEvent.TrainingId == Apprenticeship.TrainingCode &&
+                   apprenticeshipEvent.PaymentStatus == (PaymentStatus)Apprenticeship.PaymentStatus &&
+                   apprenticeshipEvent.ProviderId == Commitment.ProviderId.ToString() &&
+                   apprenticeshipEvent.TrainingEndDate == Apprenticeship.EndDate &&
+                   apprenticeshipEvent.TrainingStartDate == Apprenticeship.StartDate &&
+                   apprenticeshipEvent.TrainingTotalCost == Apprenticeship.Cost &&
+                   apprenticeshipEvent.TrainingType == (Apprenticeship.TrainingType == TrainingType.Framework ? TrainingTypes.Framework : TrainingTypes.Standard) &&
+                   apprenticeshipEvent.PaymentOrder == Apprenticeship.PaymentOrder;
+
+        }
+    }
+}
