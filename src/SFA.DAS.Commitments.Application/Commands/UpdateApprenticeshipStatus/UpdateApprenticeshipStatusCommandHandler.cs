fcf9bed55258aeb431425989046bd3cb27202ab7
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-04-10 14:52:08 +0100

    Correct validation of Change Date

--- src/SFA.DAS.Commitments.Application/Commands/UpdateApprenticeshipStatus/UpdateApprenticeshipStatusCommandHandler.cs
+++ src/SFA.DAS.Commitments.Application/Commands/UpdateApprenticeshipStatus/UpdateApprenticeshipStatusCommandHandler.cs
@@ -53,14 +53,19 @@ namespace SFA.DAS.Commitments.Application.Commands.UpdateApprenticeshipStatus
 
         private void ValidateDateOfChange(Api.Types.Apprenticeship.Types.PaymentStatus? paymentStatus, DateTime dateOfChange, Apprenticeship apprenticeship)
         {
-            if (apprenticeship.IsWaitingToStart(_currentDate) && dateOfChange.Date != apprenticeship.StartDate.Value.Date)
-                throw new ValidationException("Invalid Date of Change. Date should be value of start date if training has not started.");
+            if (apprenticeship.IsWaitingToStart(_currentDate))
+            {
+                if (dateOfChange.Date != apprenticeship.StartDate.Value.Date)
+                    throw new ValidationException("Invalid Date of Change. Date should be value of start date if training has not started.");
+            }
+            else
+            {
+                if (dateOfChange.Date > _currentDate.Now.Date)
+                    throw new ValidationException("Invalid Date of Change. Date cannot be in the future.");
 
-            if (dateOfChange.Date > _currentDate.Now.Date)
-                throw new ValidationException("Invalid Date of Change. Date cannot be in the future.");
-
-            if (dateOfChange.Date < apprenticeship.StartDate.Value.Date)
-                throw new ValidationException("Invalid Date of Change. Date cannot be before the training start date.");
+                if (dateOfChange.Date < apprenticeship.StartDate.Value.Date)
+                    throw new ValidationException("Invalid Date of Change. Date cannot be before the training start date.");
+            }
 
             return;
         }
