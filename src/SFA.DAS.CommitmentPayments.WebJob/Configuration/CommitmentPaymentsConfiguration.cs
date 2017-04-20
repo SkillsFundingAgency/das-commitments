416d326ef0d369ce52e565d75b376e4a0548b2a0
Christopher Foster <Christopher Foster>
2017-04-20 15:29:55 +0100

    Adds initial WebJob project

---
+++ src/SFA.DAS.CommitmentPayments.WebJob/Configuration/CommitmentPaymentsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using SFA.DAS.Commitments.Domain.Interfaces;
+
+namespace SFA.DAS.CommitmentPayments.WebJob.Configuration
+{
+    public class CommitmentPaymentsConfiguration : IConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string ClientToken { get; set; }
+
+        public string DatabaseConnectionString { get; set; }
+        public string ServiceBusConnectionString { get; set; }
+    }
+}
---
+++ src/SFA.DAS.CommitmentPayments.WebJob/Updater/IDataLockUpdater.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentPayments.WebJob.Updater
+{
+    public interface IDataLockUpdater
+    {
+        Task RunUpdate();
+    }
+}
