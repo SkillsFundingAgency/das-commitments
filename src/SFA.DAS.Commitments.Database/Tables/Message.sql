b238b7094413f20cbde9e1227e239471a9baf325
Michael Young <mick.young1981@googlemail.com>
2017-04-21 09:55:20 +0100

    Added messages database table

---
+++ src/SFA.DAS.Commitments.Database/Tables/Message.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Message]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [CommitmentId] BIGINT NOT NULL, 
+    [Message] NVARCHAR(MAX) NOT NULL, 
+    [CreatedOn] DATETIME NOT NULL, 
+    [Author] NVARCHAR(255) NOT NULL, 
+    [Creator] TINYINT NOT NULL, 
+    CONSTRAINT [FK_Message_Commitment] FOREIGN KEY ([CommitmentId]) REFERENCES [Commitment]([Id])
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Message_CommitmentId] ON [dbo].[Apprenticeship] ([CommitmentId])
