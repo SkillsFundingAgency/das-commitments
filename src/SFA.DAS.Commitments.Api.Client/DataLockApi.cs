fc4c90bae133d7a5f6cd257edffc2901308ffdc6
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-27 16:19:43 +0100

    Update DataLockApi to use SecureHttpClient
    Updated the DataLockApi to use the SecureHttpClient to use
    AAD authentication.

--- src/SFA.DAS.Commitments.Api.Client/DataLockApi.cs
+++ src/SFA.DAS.Commitments.Api.Client/DataLockApi.cs
@@ -8,53 +8,63 @@ using SFA.DAS.Commitments.Api.Types.DataLock;
 
 namespace SFA.DAS.Commitments.Api.Client
 {
-    public class DataLockApi : HttpClientBase, IDataLockApi
+    public class DataLockApi : IDataLockApi
     {
         private readonly ICommitmentsApiClientConfiguration _configuration;
+        private readonly SecureHttpClient _client;
 
-        public DataLockApi(ICommitmentsApiClientConfiguration configuration) : base(configuration.ClientToken)
+        private string BaseUrl => _configuration.BaseUrl.EndsWith("/") ? _configuration.BaseUrl : _configuration.BaseUrl + "/";
+
+        public DataLockApi(ICommitmentsApiClientConfiguration configuration)
         {
             if (configuration == null)
                 throw new ArgumentNullException(nameof(configuration));
 
             _configuration = configuration;
+            _client = new SecureHttpClient(_configuration);
+        }
+
+        internal DataLockApi(ICommitmentsApiClientConfiguration configuration, SecureHttpClient client)
+        {
+            _configuration = configuration;
+            _client = client;
         }
 
         public async Task<DataLockStatus> GetDataLock(long apprenticeshipId, long dataLockEventId)
         {
-            var url = $"{_configuration.BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks/{dataLockEventId}";
+            var url = $"{BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks/{dataLockEventId}";
             return await GetDataLock(url);
         }
 
         public async Task<List<DataLockStatus>> GetDataLocks(long apprenticeshipId)
         {
-            var url = $"{_configuration.BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks";
+            var url = $"{BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks";
             return await GetDataLocks(url);
         }
 
         public async Task PatchDataLock(long apprenticeshipId, DataLockStatus dataLock)
         {
             var dataLockEventId = dataLock.DataLockEventId;
-            var url = $"{_configuration.BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks/{dataLockEventId}";
+            var url = $"{BaseUrl}api/apprenticeships/{apprenticeshipId}/datalocks/{dataLockEventId}";
             await PatchDataLock(url, dataLock);
         }
 
         private async Task<DataLockStatus> GetDataLock(string url)
         {
-            var content = await GetAsync(url);
+            var content = await _client.GetAsync(url);
             return JsonConvert.DeserializeObject<DataLockStatus>(content);
         }
 
         private async Task<List<DataLockStatus>> GetDataLocks(string url)
         {
-            var content = await GetAsync(url);
+            var content = await _client.GetAsync(url);
             return JsonConvert.DeserializeObject<List<DataLockStatus>>(content);
         }
 
         private async Task PatchDataLock(string url, DataLockStatus dataLock)
         {
             var data = JsonConvert.SerializeObject(dataLock);
-            await PatchAsync(url, data);
+            await _client.PatchAsync(url, data);
         }
     }
 }
