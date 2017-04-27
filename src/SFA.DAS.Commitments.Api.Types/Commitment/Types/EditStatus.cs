8dbaf42502f96e545e960cbd4a3cc9c763f735b7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-27 09:13:45 +0100

    Move API Client and Types project into sln

---
+++ src/SFA.DAS.Commitments.Api.Types/Commitment/Types/EditStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.Commitments.Api.Types.Commitment.Types
+{
+    public enum EditStatus
+    {
+        Both = 0,
+        EmployerOnly = 1,
+        ProviderOnly = 2,
+        Neither = 3
+    }
+}
---
+++ src/SFA.DAS.Commitments.Api.Types/Commitment/Types/LastAction.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Commitments.Api.Types.Commitment.Types
+{
+    public enum LastAction
+    {
+        None = 0,
+        Amend = 1,
+        Approve = 2
+    }
+}
