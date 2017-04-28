cdb3dd3f5757cfa4f9f2ffc45fd68efc00893d46
Michael Young <mick.young1981@googlemail.com>
2017-04-28 14:20:04 +0100

    Added generic history table

---
+++ src/SFA.DAS.Commitments.Database/Tables/History.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[History]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[EntityType] NVARCHAR(50) NOT NULL,
+	[EntityId] BIGINT NOT NULL, 
+    [UserId] NVARCHAR(50) NOT NULL, 
+    [UpdatedByRole] NVARCHAR(50) NOT NULL, 
+    [ChangeType] NVARCHAR(50) NOT NULL,
+    [CreatedOn] DATETIME NOT NULL, 
+    [UpdatedByName] NVARCHAR(255) NULL
+)
