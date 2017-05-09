aaf7e5bea58722004e59730ae5965a751b2e1a72
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-09 10:48:46 +0100

    Add StatusController for health check on API

---
+++ src/SFA.DAS.Commitments.Api/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SFA.DAS.Commitments.Api.Orchestrators;
+
+namespace SFA.DAS.Commitments.Api.Controllers
+{
+    [RoutePrefix("api/status")]
+    public class StatusController : ApiController
+    {
+        private readonly EmployerOrchestrator _orchestrator;
+
+        public StatusController(EmployerOrchestrator orchestrator)
+        {
+            _orchestrator = orchestrator;
+        }
+
+        [Route("")]
+        [Authorize(Roles = "Role1")]
+        public async Task<IHttpActionResult> Index()
+        {
+            try
+            {
+                // Do some Infrastructre work here to smoke out any issues.
+                await _orchestrator.GetApprenticeship(1, 1);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
