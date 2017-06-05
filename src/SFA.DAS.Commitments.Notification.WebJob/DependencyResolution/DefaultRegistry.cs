f02987fe74c6c8abf9ade0d5a7aa2970b2e6e18a
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-06-05 13:40:23 +0100

    Removed redundant code in Registry

--- src/SFA.DAS.Commitments.Notification.WebJob/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.Commitments.Notification.WebJob/DependencyResolution/DefaultRegistry.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Reflection;
-
+﻿using Microsoft.Azure;
 using SFA.DAS.Commitments.Domain.Data;
 using SFA.DAS.Commitments.Domain.Interfaces;
 using SFA.DAS.Commitments.Infrastructure.Data;
+using SFA.DAS.Commitments.Notification.WebJob.Configuration;
 using SFA.DAS.Configuration;
 using SFA.DAS.Configuration.AzureTableStorage;
 using SFA.DAS.EAS.Account.Api.Client;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Notifications.Api.Client.Configuration;
-
-using Microsoft.Azure;
 using StructureMap;
-using SFA.DAS.Commitments.Notification.WebJob.Configuration;
+using System;
+using System.Reflection;
 
 namespace SFA.DAS.Commitments.Notification.WebJob.DependencyResolution
 {
@@ -31,9 +29,9 @@ namespace SFA.DAS.Commitments.Notification.WebJob.DependencyResolution
 
             For<CommitmentNotificationConfiguration>().Use(config);
 
-            //For<IAccountApiClient>().Use<AccountApiClient>()
-            //    .Ctor<IAccountApiConfiguration>().Is(config.AccountApiConfiguration);
-            For<IAccountApiClient>().Use<FakeAccountApiClient>();
+            For<IAccountApiClient>().Use<AccountApiClient>()
+                .Ctor<IAccountApiConfiguration>().Is(config.AccountApiConfiguration);
+            // For<IAccountApiClient>().Use<FakeAccountApiClient>();
 
             For<IApprenticeshipRepository>().Use<ApprenticeshipRepository>().Ctor<string>().Is(config.DatabaseConnectionString);
             For<IEmailTemplatesService>().Use<EmailTemplatesService>();
@@ -42,21 +40,17 @@ namespace SFA.DAS.Commitments.Notification.WebJob.DependencyResolution
             For<INotificationsApiClientConfiguration>().Use(config.NotificationApi);
 
             For<IConfiguration>().Use(config);
-            For<ILog>().Use(x => new NLogLogger(x.ParentType, new DummyRequestContext(), null)).AlwaysUnique();
+            For<ILog>().Use(x => new NLogLogger(x.ParentType, null, null)).AlwaysUnique();
         }
         
 
         private CommitmentNotificationConfiguration GetConfiguration(string serviceName)
         {
             var environment = Environment.GetEnvironmentVariable("DASENV");
+
             if (string.IsNullOrEmpty(environment))
             {
                 environment = CloudConfigurationManager.GetSetting("EnvironmentName");
-            }
-            if (environment.Equals("LOCAL") || environment.Equals("AT") || environment.Equals("TEST"))
-            {
-                //todo: is this required?
-                PopulateSystemDetails(environment);
             }
 
             var configurationRepository = GetConfigurationRepository();
@@ -68,29 +62,9 @@ namespace SFA.DAS.Commitments.Notification.WebJob.DependencyResolution
             return result;
         }
 
-        private void PopulateSystemDetails(string envName)
-        {
-            SystemDetails.EnvironmentName = envName;
-            SystemDetails.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        }
-
-
         private static IConfigurationRepository GetConfigurationRepository()
         {
             return new AzureTableStorageConfigurationRepository(CloudConfigurationManager.GetSetting("ConfigurationStorageConnectionString"));
         }
-    }
-
-    public class DummyRequestContext : IRequestContext
-    {
-        public string Url { get; }
-
-        public string IpAddress { get; }
-    }
-
-    public static class SystemDetails
-    {
-        public static string VersionNumber { get; set; }
-        public static string EnvironmentName { get; set; }
     }
 }
