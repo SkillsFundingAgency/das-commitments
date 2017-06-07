a977a528c1bb50a1e6f7d958feaa8a274cc0adc9
froberg <christofer.froberg@gmail.com>
2017-06-07 10:27:19 +0100

    Added new table

---
+++ src/SFA.DAS.Commitments.Database/Tables/PriceEpisode.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PriceEpisode]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[ApprenticeshipId] BIGINT NOT NULL, 
+	[Cost] DECIMAL NOT NULL,
+	[FromDate] DateTime NOT NULL,
+	[EndDate] Date NULL,
+	CONSTRAINT [FK_PriceEpisode_Apprenticeship] FOREIGN KEY ([ApprenticeshipId]) REFERENCES [Apprenticeship]([Id])
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_PriceEpisode_ApprenticeshipId] ON [dbo].[PriceEpisode] ([ApprenticeshipId]) INCLUDE ([Cost], [FromDate], [EndDate]) WITH (ONLINE = ON)
