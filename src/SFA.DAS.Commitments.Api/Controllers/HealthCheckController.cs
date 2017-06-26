01ad3c922efd43a6a366358f69c60bdca55cb34c
Craig Gumbley <craiggumbley@gmail.com>
2017-06-26 14:30:33 +0100

    Adds healthcheck api endpoint

---
+++ src/SFA.DAS.Commitments.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace SFA.DAS.Commitments.Api.Controllers
+{
+    public class HealthCheckController : ApiController
+    {
+        [Route("api/HealthCheck")]
+        public IHttpActionResult GetStatus()
+        {
+            return Ok();
+        }
+    }
+}
