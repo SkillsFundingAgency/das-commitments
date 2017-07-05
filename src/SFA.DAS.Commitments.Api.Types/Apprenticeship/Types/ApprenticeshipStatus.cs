7cd1180ae5105d4ecb17e7cad5904bf13b6a9e8c
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-07-05 13:27:48 +0100

    Add Client and Types projects

---
+++ src/SFA.DAS.Commitments.Api.Types/Apprenticeship/Types/ApprenticeshipStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.Commitments.Api.Types.Apprenticeship.Types
+{
+    public enum ApprenticeshipStatus
+    {
+        None = 0,
+        WaitingToStart = 1,
+        Live = 2,
+        Paused = 3,
+        Stopped = 4,
+        Finished = 5
+    }
+}
