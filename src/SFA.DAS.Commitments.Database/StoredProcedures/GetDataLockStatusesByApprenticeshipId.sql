7df40158aaadbca39696ea492b9c325c586b6300
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-07-17 17:31:52 +0100

    Added back in the August data lock filter.

--- src/SFA.DAS.Commitments.Database/StoredProcedures/GetDataLockStatusesByApprenticeshipId.sql
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetDataLockStatusesByApprenticeshipId.sql
@@ -4,8 +4,10 @@ AS
 
 	SELECT *
 	FROM 
-    DataLockStatus
+		DataLockStatus
 	WHERE 
-    ApprenticeshipId = @ApprenticeshipId
+		ApprenticeshipId = @ApprenticeshipId
+    AND
+		SUBSTRING(PriceEpisodeIdentifier,LEN(PriceEpisodeIdentifier)-9,10) <> '01/08/2017' -- TODO: 17/07/2017 - Temporary Fix to new academic year data lock errors.
 	ORDER BY
 		IlrEffectiveFromDate, Id
