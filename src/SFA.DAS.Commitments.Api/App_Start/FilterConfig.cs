22b53dc03a1a0144728119c30061010f3afb74b7
froberg <christofer.froberg@gmail.com>
2017-08-23 16:11:12 +0100

    Updated logging

---
+++ src/SFA.DAS.Commitments.Api/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http.Filters;
+
+namespace SFA.DAS.Commitments.Api
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(HttpFilterCollection filters)
+        {
+            // ToDo: Add action filter from NLog
+            // Request and session
+            //filters.Add(new HeaderTestActionFilter());
+        }
+    }
+}
