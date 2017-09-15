d39cdc32ccca72eed716ed76ea637ba54d4047f4
Iain Stevenson <IainStevenson@live.co.uk>
2017-09-15 09:24:57 +0100

    Initial Null web job for DCM-751

---
+++ src/SFA.DAS.Comitments.AcademicYearEndProcessor.WebJob/DependencyResolution/DefaultRegistry.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.Comitments.AcademicYearEndProcessor.WebJob.Updater;
+using SFA.DAS.NLog.Logger;
+using StructureMap;
+
+namespace SFA.DAS.Comitments.AcademicYearEndProcessor.WebJob.DependencyResolution
+{
+    public class DefaultRegistry : Registry
+    {
+        public DefaultRegistry()
+        {
+            Scan(
+                scan =>
+                {
+                    scan.AssembliesAndExecutablesFromApplicationBaseDirectory( a => a.GetName().Name.StartsWith("SFA.DAS"));
+                    //scan.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
+                    scan.RegisterConcreteTypesAgainstTheFirstInterface();
+                });
+            For<ILog>().Use(x => new NLogLogger(x.ParentType, new DummyRequestContext(), null)).AlwaysUnique();
+           // For<IDummyTask>().Use(x =>  new DummyTask(x.GetInstance<ILog>())).AlwaysUnique();
+        }
+    }
+    public class DummyRequestContext : IRequestContext
+    {
+        public string Url { get; }
+
+        public string IpAddress { get; }
+    }
+}
