7e2486a1cbd1a96ebd0f3904057348b77d84ca18
froberg <christofer.froberg@gmail.com>
2017-10-03 11:52:58 +0100

    DCM-742 -> update insert history to support old repository code

--- src/SFA.DAS.Commitments.Database/StoredProcedures/InsertHistory.sql
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/InsertHistory.sql
@@ -1,6 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[InsertHistory]
-	@commitmentId BIGINT, 
-	@apprenticeshipId BIGINT, 
+	@entityType NVARCHAR(50) = NULL,
+	@entityId BIGINT = NULL, 
+	@commitmentId BIGINT NULL = NULL, 
+	@apprenticeshipId BIGINT = NULL, 
     @userId NVARCHAR(50), 
     @updatedByRole NVARCHAR(50), 
     @changeType NVARCHAR(50),
@@ -11,6 +13,21 @@
     @updatedState NVARCHAR(MAX),
 	@createdOn DATETIME
 AS
+
+IF @entityType IS NOT NULL AND @entityId IS NOT NULL
+BEGIN
+	IF @entityType = 'Commitment'
+	BEGIN
+		SET @commitmentId = @entityId
+		SET @apprenticeshipId = null
+	END
+
+	IF @entityType = 'Apprenticeship'
+	BEGIN
+		SET @commitmentId = null
+		SET @apprenticeshipId = @entityId
+	END
+END
 
 INSERT INTO [dbo].[History]
 	(CommitmentId, ApprenticeshipId, UserId, UpdatedByRole, ChangeType, CreatedOn, ProviderId, EmployerAccountId, UpdatedByName, OriginalState, UpdatedState)
