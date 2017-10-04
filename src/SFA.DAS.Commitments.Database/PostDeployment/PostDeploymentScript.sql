938e8c3ee60fbbf4b59c58a7101522b3e28237d1
Iain Stevenson <iainstevenson@live.co.uk>
2017-10-04 07:27:42 +0100

    Improved and tested upgrade script

--- src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
+++ src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
@@ -19,46 +19,29 @@ BEGIN
 	*/
 
 
-	UPDATE [dbo].[Apprenticeship] 
-	SET	[PauseDate] = CASE 
-						WHEN	JSON_VALUE(h.[UpdatedState], '$.PaymentStatus') = '2' THEN h.[CreatedOn]		/* now paused */
-						WHEN	JSON_VALUE(h.[UpdatedState], '$.PaymentStatus') = '1' THEN NULL					/* now active */
-						ELSE	a.[PauseDate]	/* catch all no change */
-						END
+	UPDATE	[dbo].[Apprenticeship] 
 	
-/*
+	SET		[PauseDate]			= h.[CreatedOn]
 
-	SELECT [Id]
-		  ,[CommitmentId]
-		  ,[StartDate]
-		  ,[EndDate]
-		  ,[AgreementStatus]
-		  ,[PaymentStatus]
-		  ,a.[CreatedOn]
-		  ,[AgreedOn]
-		  ,[PaymentOrder]
-		  ,[StopDate]
-		  ,[PauseDate]
-		  ,h.[CreatedOn]
-		  FROM
-*/	  
-	FROM [dbo].[Apprenticeship] a,
-		(	SELECT TOP 1 [EntityId]  ,MAX([CreatedOn]) as [CreatedOn]
-			FROM [dbo].[History]
+	FROM	[dbo].[Apprenticeship] a,
+
+		(	SELECT	[EntityId]  ,
+					MAX([CreatedOn]) as [CreatedOn]
+			
+			FROM	[dbo].[History]
+
 			WHERE	[EntityType]	= 'Apprenticeship'
 			AND		[ChangeType]		= 'ChangeOfStatus'
 			AND 	(
-						JSON_VALUE([OriginalState], '$.PaymentStatus') = '1' AND JSON_VALUE([UpdatedState], '$.PaymentStatus') = '2'
+							JSON_VALUE([OriginalState], '$.PaymentStatus') = '1' 
+						AND JSON_VALUE([UpdatedState] , '$.PaymentStatus') = '2'
 					)
+			
 			GROUP BY
 				  [EntityId] 
-				  ,[CreatedOn]
-			ORDER BY
-				  [EntityId]  ASC
-				  ,[CreatedOn] DESC
 		) h
+
 	 WHERE	a.[Id]				= h.[EntityId]
-	 AND	a.[PaymentStatus]	= 2
-		  
+	 AND	a.[PaymentStatus]	= 2		  
 
 END
