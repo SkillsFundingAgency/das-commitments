4b6a1299046792c46d85655c8a9590171b235640
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-10-31 11:27:02 +0000

    Re-isntated and obsleted the two enumerations for OrganisationType

--- src/SFA.DAS.Commitments.Domain/Entities/OrganisationType.cs
+++ src/SFA.DAS.Commitments.Domain/Entities/OrganisationType.cs
@@ -2,12 +2,12 @@
 
 namespace SFA.DAS.Commitments.Domain.Entities
 {
-    //    [Obsolete("Please convert your code to reference NuGet package SFA.DAS.Common.Domain and use its Types.OrganisationType enum")]
-    //    public enum OrganisationType : short
-    //    {
-    //        CompaniesHouse = 1,
-    //        Charities = 2,
-    //        PublicBodies = 3,
-    //        Other = 4
-    //    }
+    [Obsolete("Please update your code to reference NuGet package SFA.DAS.Common.Domain and use its Types.OrganisationType enum")]
+    public enum OrganisationType : short
+    {
+        CompaniesHouse = 1,
+        Charities = 2,
+        PublicBodies = 3,
+        Other = 4
+    }
 }
