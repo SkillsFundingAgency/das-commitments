76303e268d2a2532069da445f3e1c167f5407462
chrisfoster76 <chrisfoster186@googlemail.com>
2018-01-09 14:37:15 +0000

    Adds new event for scenario where cohort is edited by employer having been approved by provider already, which should emit an event

---
+++ src/SFA.DAS.Commitments.Events/ProviderCohortApprovalUndoneByEmployerUpdate.cs
@@ -0,0 +1,27 @@
+﻿using SFA.DAS.Messaging.Attributes;
+
+namespace SFA.DAS.Commitments.Events
+{
+    [MessageGroup("provider_cohort_approval_undone_by_employer_update")]
+    public class ProviderCohortApprovalUndoneByEmployerUpdate
+    {
+        //Needs a parameterless constructor to work with the message processing
+        public ProviderCohortApprovalUndoneByEmployerUpdate()
+        {
+            
+        }
+
+        public ProviderCohortApprovalUndoneByEmployerUpdate(long accountId, long providerId, long commitmentId)
+        {
+            AccountId = accountId;
+            ProviderId = providerId;
+            CommitmentId = commitmentId;
+        }
+
+        public long CommitmentId { get; set; }
+
+        public long ProviderId { get; set; }
+
+        public long AccountId { get; set; }
+    }
+}
