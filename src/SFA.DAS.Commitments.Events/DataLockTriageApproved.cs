2d6005d192846e40745281221da90913b2778a14
Michael Young <mick.young1981@googlemail.com>
2018-01-11 10:59:33 +0000

    Added event when a data lock triage is approved

---
+++ src/SFA.DAS.Commitments.Events/DataLockTriageApproved.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.Messaging.Attributes;
+
+namespace SFA.DAS.Commitments.Events
+{
+    [MessageGroup("data_lock_triage_approved")]
+    public class DataLockTriageApproved
+    {
+        //Needs a parameterless constructor to work with the message processing
+        public DataLockTriageApproved()
+        {
+
+        }
+
+        public DataLockTriageApproved(long accountId, long providerId, long apprenticeshipId)
+        {
+            AccountId = accountId;
+            ProviderId = providerId;
+            ApprenticeshipId = apprenticeshipId;
+        }
+
+        public long AccountId { get; set; }
+        public long ProviderId { get; set; }
+        public long ApprenticeshipId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Commitments.Events/DataLockTriageRejected.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.Messaging.Attributes;
+
+namespace SFA.DAS.Commitments.Events
+{
+    [MessageGroup("data_lock_triage_rejected")]
+    public class DataLockTriageRejected
+    {
+        //Needs a parameterless constructor to work with the message processing
+        public DataLockTriageRejected()
+        {
+
+        }
+
+        public DataLockTriageRejected(long accountId, long providerId, long apprenticeshipId)
+        {
+            AccountId = accountId;
+            ProviderId = providerId;
+            ApprenticeshipId = apprenticeshipId;
+        }
+
+        public long AccountId { get; set; }
+        public long ProviderId { get; set; }
+        public long ApprenticeshipId { get; set; }
+    }
+}
