c0944cf4159ae9b1c1f8deb68b7a781c60b415fc
chrisfoster76 <chrisfoster186@googlemail.com>
2018-01-24 11:01:47 +0000

    Revert "Merge pull request #310 from SkillsFundingAgency/AML-1411_AddedEventsForDataLockTriage"
    This reverts commit d3154b6a64eb2bcc215b8f43e6b991e7a9a5f24a, reversing
    changes made to cc35200544f35e079889076536ade91ec49ef360.

--- src/SFA.DAS.Commitments.Application.UnitTests/Commands/TriageDataLock/WhenTriagingDataLock.cs
+++ src/SFA.DAS.Commitments.Application.UnitTests/Commands/TriageDataLock/WhenTriagingDataLock.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentValidation;
@@ -11,8 +10,6 @@ using SFA.DAS.Commitments.Domain.Data;
 using SFA.DAS.Commitments.Domain.Entities;
 using SFA.DAS.Commitments.Domain.Entities.DataLock;
 using SFA.DAS.Commitments.Domain.Interfaces;
-using SFA.DAS.Commitments.Events;
-using SFA.DAS.Messaging.Interfaces;
 
 namespace SFA.DAS.Commitments.Application.UnitTests.Commands.TriageDataLock
 {
@@ -23,8 +20,6 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.TriageDataLock
         private Mock<AbstractValidator<TriageDataLockCommand>> _validator;
         private Mock<IDataLockRepository> _dataLockRepository;
         private Mock<IApprenticeshipUpdateRepository> _apprenticeshipUpdateRepository;
-        private Mock<IMessagePublisher> _messagePublisher;
-        private Mock<IApprenticeshipRepository> _apprenticeshipRepository;
 
         [SetUp]
         public void Arrange()
@@ -38,29 +33,21 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.TriageDataLock
                 .ReturnsAsync(new DataLockStatus
                 {
                     IlrEffectiveFromDate = new DateTime(2018, 5, 1),
-                    ErrorCode = DataLockErrorCode.Dlock07
+                    ErrorCode = DataLockErrorCode.Dlock03
                 });
 
             _dataLockRepository.Setup(x => x.UpdateDataLockTriageStatus(It.IsAny<long>(), It.IsAny<TriageStatus>()))
                 .Returns(() => Task.FromResult(1L));
-            _dataLockRepository.Setup(x => x.GetDataLocks(It.IsAny<long>(), It.IsAny<bool>())).ReturnsAsync(new List<DataLockStatus>());
 
             _apprenticeshipUpdateRepository = new Mock<IApprenticeshipUpdateRepository>();
             _apprenticeshipUpdateRepository.Setup(x => x.GetPendingApprenticeshipUpdate(It.IsAny<long>()))
                 .ReturnsAsync(null);
 
-            _messagePublisher = new Mock<IMessagePublisher>();
-
-            _apprenticeshipRepository = new Mock<IApprenticeshipRepository>();
-            _apprenticeshipRepository.Setup(x => x.GetApprenticeship(It.IsAny<long>())).ReturnsAsync(new Apprenticeship());
-
             _handler = new TriageDataLockCommandHandler(
                 _validator.Object,  
                 _dataLockRepository.Object,
                 _apprenticeshipUpdateRepository.Object,
-                Mock.Of<ICommitmentsLogger>(),
-                _messagePublisher.Object,
-                _apprenticeshipRepository.Object);
+                Mock.Of<ICommitmentsLogger>());
         }
 
         [Test]
@@ -96,7 +83,7 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.TriageDataLock
             //Arrange
             var command = new TriageDataLockCommand
             {
-                TriageStatus = TriageStatus.Change
+                TriageStatus = TriageStatus.Restart
             };
 
             //Act
@@ -148,79 +135,6 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.TriageDataLock
             Func<Task> act = async () => await _handler.Handle(command);
 
             act.ShouldThrow<ValidationException>();
-        }
-
-        [Test]
-        public async Task ThenIfATriageStatusIsUpdatedToChangeAndTheApprenticeshipHasNoChangesRequiringApprovalAnEventIsSent()
-        {
-            //Arrange
-            var command = new TriageDataLockCommand
-            {
-                ApprenticeshipId = 0,
-                DataLockEventId = 456,
-                TriageStatus = TriageStatus.Change
-            };
-
-            var existingDataLocks = new List<DataLockStatus>
-            {
-                new DataLockStatus { TriageStatus = TriageStatus.Unknown, IsResolved = false, Status = Status.Fail, ApprenticeshipId = command.ApprenticeshipId }
-            };
-            _dataLockRepository.Setup(x => x.GetDataLocks(command.ApprenticeshipId, false)).ReturnsAsync(existingDataLocks);
-
-            var apprenticeship = new Apprenticeship { EmployerAccountId = 45453, ProviderId = 94443, Id = command.ApprenticeshipId };
-            _apprenticeshipRepository.Setup(x => x.GetApprenticeship(command.ApprenticeshipId)).ReturnsAsync(apprenticeship);
-
-            //Act
-            await _handler.Handle(command);
-
-            //Assert
-            _messagePublisher.Verify(
-                x =>
-                    x.PublishAsync(
-                        It.Is<DataLockTriageRequiresApproval>(
-                            m => m.AccountId == apprenticeship.EmployerAccountId && m.ProviderId == apprenticeship.ProviderId && m.ApprenticeshipId == apprenticeship.Id)), Times.Once);
-        }
-
-        [Test]
-        public async Task ThenIfATriageStatusIsUpdatedToChangeAndTheApprenticeshipAlreadyHasAChangeRequiringApprovalThenNoEventIsSent()
-        {
-            //Arrange
-            var command = new TriageDataLockCommand
-            {
-                ApprenticeshipId = 0,
-                DataLockEventId = 456,
-                TriageStatus = TriageStatus.Change
-            };
-
-            var existingDataLocks = new List<DataLockStatus>
-            {
-                new DataLockStatus { TriageStatus = TriageStatus.Change, IsResolved = false, Status = Status.Fail, ApprenticeshipId = command.ApprenticeshipId }
-            };
-            _dataLockRepository.Setup(x => x.GetDataLocks(command.ApprenticeshipId, false)).ReturnsAsync(existingDataLocks);
-
-            //Act
-            await _handler.Handle(command);
-
-            //Assert
-            _messagePublisher.Verify(x => x.PublishAsync(It.IsAny<DataLockTriageRequiresApproval>()), Times.Never);
-        }
-
-        [Test]
-        public async Task ThenIfATriageStatusIsNotChangeThenNoEventIsSent()
-        {
-            //Arrange
-            var command = new TriageDataLockCommand
-            {
-                ApprenticeshipId = 0,
-                DataLockEventId = 456,
-                TriageStatus = TriageStatus.FixIlr
-            };
-
-            //Act
-            await _handler.Handle(command);
-
-            //Assert
-            _messagePublisher.Verify(x => x.PublishAsync(It.IsAny<DataLockTriageRequiresApproval>()), Times.Never);
         }
     }
 }
--- src/SFA.DAS.Commitments.Application/Commands/RejectDataLockTriage/RejectDataLockTriageCommandHandler.cs
+++ src/SFA.DAS.Commitments.Application/Commands/RejectDataLockTriage/RejectDataLockTriageCommandHandler.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 using MediatR;
 using SFA.DAS.Commitments.Domain.Data;
-using SFA.DAS.Commitments.Domain.Entities;
 using SFA.DAS.Commitments.Domain.Entities.DataLock;
 using SFA.DAS.Commitments.Domain.Extensions;
-using SFA.DAS.Commitments.Events;
-using SFA.DAS.Messaging.Interfaces;
 
 namespace SFA.DAS.Commitments.Application.Commands.RejectDataLockTriage
 {
@@ -18,18 +15,25 @@ namespace SFA.DAS.Commitments.Application.Commands.RejectDataLockTriage
         private readonly IDataLockRepository _dataLockRepository;
 
         private readonly IApprenticeshipRepository _apprenticeshipRepository;
-        private readonly IMessagePublisher _messagePublisher;
 
         public RejectDataLockTriageCommandHandler(
             AbstractValidator<RejectDataLockTriageCommand> validator,
             IDataLockRepository dataLockRepository,
             IApprenticeshipRepository apprenticeshipRepository,
-            IMessagePublisher messagePublisher)
+            ICommitmentRepository commitmentRepository)
         {
+            if (validator == null)
+                throw new ArgumentNullException(nameof(AbstractValidator<RejectDataLockTriageCommand>));
+            if (dataLockRepository == null)
+                throw new ArgumentNullException(nameof(IDataLockRepository));
+            if (apprenticeshipRepository == null)
+                throw new ArgumentNullException(nameof(IApprenticeshipRepository));
+            if(commitmentRepository == null)
+                throw new ArgumentNullException(nameof(ICommitmentRepository));
+
             _validator = validator;
             _dataLockRepository = dataLockRepository;
             _apprenticeshipRepository = apprenticeshipRepository;
-            _messagePublisher = messagePublisher;
         }
 
         protected override async Task HandleCore(RejectDataLockTriageCommand command)
@@ -38,8 +42,17 @@ namespace SFA.DAS.Commitments.Application.Commands.RejectDataLockTriage
             if (!validationResult.IsValid)
                 throw new ValidationException(validationResult.Errors);
 
+            var datalocks = await _dataLockRepository.GetDataLocks(command.ApprenticeshipId);
+
+            var dataLocksToBeUpdated = datalocks
+                .Where(DataLockExtensions.UnHandled)
+                .Where(x => x.TriageStatus == TriageStatus.Change);
+
             var apprenticeship = await _apprenticeshipRepository.GetApprenticeship(command.ApprenticeshipId);
-            var dataLocksToBeUpdated = await GetDataLocksToBeUpdated(command, apprenticeship);
+            if (apprenticeship.HasHadDataLockSuccess)
+            {
+                dataLocksToBeUpdated = dataLocksToBeUpdated.Where(DataLockExtensions.IsPriceOnly);
+            }
 
             if (!dataLocksToBeUpdated.Any())
                 return;
@@ -47,28 +60,6 @@ namespace SFA.DAS.Commitments.Application.Commands.RejectDataLockTriage
             await _dataLockRepository.UpdateDataLockTriageStatus(
                 dataLocksToBeUpdated.Select(m => m.DataLockEventId),
                 TriageStatus.Unknown);
-
-            await PublishEvents(apprenticeship);
-        }
-
-        private async Task<IEnumerable<DataLockStatus>> GetDataLocksToBeUpdated(RejectDataLockTriageCommand command, Apprenticeship apprenticeship)
-        {
-            var datalocks = await _dataLockRepository.GetDataLocks(command.ApprenticeshipId);
-
-            var dataLocksToBeUpdated = datalocks
-                .Where(DataLockExtensions.UnHandled)
-                .Where(x => x.TriageStatus == TriageStatus.Change);
-
-            if (apprenticeship.HasHadDataLockSuccess)
-            {
-                dataLocksToBeUpdated = dataLocksToBeUpdated.Where(DataLockExtensions.IsPriceOnly);
-            }
-            return dataLocksToBeUpdated;
-        }
-
-        private async Task PublishEvents(Apprenticeship apprenticeship)
-        {
-            await _messagePublisher.PublishAsync(new DataLockTriageRejected(apprenticeship.EmployerAccountId, apprenticeship.ProviderId, apprenticeship.Id));
         }
     }
 }
