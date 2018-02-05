ec708964c505be7862ee794d1fa3da438c542d90
phildavies <github@phildavies.co.uk>
2018-02-05 10:28:48 +0000

    unit test wip

--- src/SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob.UnitTests/WhenRunningAddEpaToApprenticeships.cs
+++ src/SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob.UnitTests/WhenRunningAddEpaToApprenticeships.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.Commitments.Domain.Data;
 
 namespace SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob.UnitTests
 {
     [TestFixture]
     public class WhenRunningAddEpaToApprenticeships
     {
+        private Mock<IApprenticeshipRepository> _apprenticeshipRepository;
+
         [SetUp]
         public void Arrange()
         {
+            _apprenticeshipRepository = new Mock<IApprenticeshipRepository>();
         }
 
         [Test]
