f9a2739137fb6c63ce29abeffd30c281f0114ed3
Dave Howlett <dave_howlett@hotmail.com>
2018-02-06 14:55:00 +0000

    DPP-988 - addition of new type for handling changes to stop date

---
+++ src/SFA.DAS.Commitments.Api.Types/Apprenticeship/ApprenticeshipStopDate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SFA.DAS.Commitments.Api.Types.Commitment.Types;
+
+namespace SFA.DAS.Commitments.Api.Types.Apprenticeship
+{
+    public sealed class ApprenticeshipStopDate
+    {
+        public DateTime DateOfChange { get; set; }
+        public string UserId { get; set; }
+        public LastUpdateInfo LastUpdatedByInfo { get; set; }
+    }
+}
