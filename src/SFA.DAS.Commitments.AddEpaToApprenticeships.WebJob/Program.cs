7b4fd1357bbe53ca7e4bf8f2e6d1a08224dccda2
phildavies <github@phildavies.co.uk>
2018-02-12 15:19:16 +0000

    tidy up program.cs

--- src/SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob/Program.cs
+++ src/SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
 using SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob.DependencyResolution;
 using SFA.DAS.NLog.Logger;
 
@@ -12,8 +7,6 @@ namespace SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob
     // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
             //todo: add config to confluence page
@@ -27,20 +20,8 @@ namespace SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob
 
             logger.Info("Starting AddEpaToApprenticeships.WebJob");
 
-            // do we need to use WebJob?
+            // we don't need to use JobHost - the other web jobs in the solution don't. here's why...
             // https://stackoverflow.com/questions/25811719/azure-webjobs-sdk-in-what-scenarios-is-creation-of-a-jobhost-object-required
-
-            //var config = new JobHostConfiguration();
-
-            //if (config.IsDevelopment)
-            //{
-            //    config.UseDevelopmentSettings();
-            //}
-
-            //var host = new JobHost(config);
-            //// The following code will invoke a function called ManualTrigger and 
-            //// pass in data (value in this case) to the function
-            //host.Call(typeof(Functions).GetMethod("ManualTrigger"), new { value = 20 });
 
             try
             {
