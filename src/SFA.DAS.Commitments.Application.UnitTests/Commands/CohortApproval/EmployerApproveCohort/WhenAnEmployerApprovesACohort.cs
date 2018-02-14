568138f8f4e4f26dafe3cf33d99a8be057f2ea76
Michael Young <mick.young1981@googlemail.com>
2018-02-14 12:10:18 +0000

    Interim checking. Implemented overlapping apprenticeship check

--- src/SFA.DAS.Commitments.Application.UnitTests/Commands/CohortApproval/EmployerApproveCohort/WhenAnEmployerApprovesACohort.cs
+++ src/SFA.DAS.Commitments.Application.UnitTests/Commands/CohortApproval/EmployerApproveCohort/WhenAnEmployerApprovesACohort.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort;
 using SFA.DAS.Commitments.Application.Exceptions;
+using SFA.DAS.Commitments.Application.Rules;
 using SFA.DAS.Commitments.Domain;
 using SFA.DAS.Commitments.Domain.Data;
 using SFA.DAS.Commitments.Domain.Entities;
+using SFA.DAS.Commitments.Domain.Entities.Validation;
 
 namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.EmployerApproveCohort
 {
@@ -15,6 +19,8 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.Empl
     {
         private AbstractValidator<EmployerApproveCohortCommand> _validator;
         private Mock<ICommitmentRepository> _commitmentRepository;
+        private Mock<IApprenticeshipRepository> _apprenticeshipRepository;
+        private Mock<IApprenticeshipOverlapRules> _overlapRules;
         private EmployerApproveCohortCommandHandler _target;
         private EmployerApproveCohortCommand _command;
         private Commitment _commitment;
@@ -23,17 +29,19 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.Empl
         public void SetUp()
         {
             _command = new EmployerApproveCohortCommand { Caller = new Caller(213, CallerType.Employer), CommitmentId = 123, LastUpdatedByName = "Test", LastUpdatedByEmail = "test@email.com" };
-            _commitment = new Commitment { CommitmentStatus = CommitmentStatus.New, EditStatus = EditStatus.ProviderOnly, Id = _command.CommitmentId };
+            _commitment = CreateCommitment();
 
             _validator = new EmployerApproveCohortCommandValidator();;
             _commitmentRepository = new Mock<ICommitmentRepository>();
             _commitmentRepository.Setup(x => x.GetCommitmentById(_command.CommitmentId)).ReturnsAsync(_commitment);
-            
-            _target = new EmployerApproveCohortCommandHandler(_validator, _commitmentRepository.Object);
+            _apprenticeshipRepository = new Mock<IApprenticeshipRepository>();
+            SetupSuccessfulOverlapCheck();
+
+            _target = new EmployerApproveCohortCommandHandler(_validator, _commitmentRepository.Object, _apprenticeshipRepository.Object, _overlapRules.Object);
         }
 
         [Test]
-        public void ThenIfValidationFailsThenTheCommitmentCannotBeApproved()
+        public void ThenIfValidationFailsTheCommitmentCannotBeApproved()
         {
             _command.Caller = null;
 
@@ -41,7 +49,7 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.Empl
         }
 
         [Test]
-        public void ThenIfTheCommitmentIsDeletedThenItCannotBeApproved()
+        public void ThenIfTheCommitmentIsDeletedItCannotBeApproved()
         {
             _commitment.CommitmentStatus = CommitmentStatus.Deleted;
 
@@ -49,7 +57,7 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.Empl
         }
 
         [Test]
-        public void ThenIfTheCommitmentCanOnlyBeEditedByTheProviderThenItCannotBeApproved()
+        public void ThenIfTheCommitmentCanOnlyBeEditedByTheProviderItCannotBeApproved()
         {
             _commitment.EditStatus = EditStatus.ProviderOnly;
 
@@ -57,11 +65,53 @@ namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.Empl
         }
 
         [Test]
-        public void ThenIfTheCommitmentCannotEditedThenItCannotBeApproved()
+        public void ThenIfTheCommitmentCannotEditedItCannotBeApproved()
         {
             _commitment.EditStatus = EditStatus.Neither;
 
             Assert.ThrowsAsync<UnauthorizedException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCallerIsNotTheEmployerForTheCommitmentItCannotBeApproved()
+        {
+            _commitment.EmployerAccountId = _command.Caller.Id + 1;
+
+            Assert.ThrowsAsync<UnauthorizedException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCommitmentIsNotReadyToBeApprovedByTheEmployerItCannotBeApproved()
+        {
+            _commitment.EmployerCanApproveCommitment = false;
+
+            Assert.ThrowsAsync<InvalidOperationException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCommitmentHasOverlappingApprenticeshipsItCannotBeApproved()
+        {
+            var apprenticeship = _commitment.Apprenticeships.Last();
+            var apprenticeshipResult = new ApprenticeshipResult { Uln = apprenticeship.ULN };
+            _apprenticeshipRepository.Setup(x => x.GetActiveApprenticeshipsByUlns(It.Is<IEnumerable<string>>(y => y.First() == _commitment.Apprenticeships.First().ULN && y.Last() == _commitment.Apprenticeships.Last().ULN))).ReturnsAsync(new List<ApprenticeshipResult> { apprenticeshipResult });
+            _overlapRules.Setup(x => x.DetermineOverlap(It.Is<ApprenticeshipOverlapValidationRequest>(r => r.Uln == apprenticeship.ULN && r.ApprenticeshipId == apprenticeship.Id && r.StartDate == apprenticeship.StartDate.Value && r.EndDate == apprenticeship.EndDate.Value), apprenticeshipResult)).Returns(ValidationFailReason.OverlappingEndDate);
+
+            Assert.ThrowsAsync<ValidationException>(() => _target.Handle(_command));
+        }
+
+        private Commitment CreateCommitment()
+        {
+            var apprenticeships = new List<Apprenticeship> { new Apprenticeship {  ULN = "1233435", Id = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddYears(1) }, new Apprenticeship { ULN = "894567645", Id = 2, StartDate = DateTime.Now.AddYears(-1), EndDate = DateTime.Now.AddYears(2) } };
+            return new Commitment { CommitmentStatus = CommitmentStatus.New, EditStatus = EditStatus.EmployerOnly, Id = _command.CommitmentId, EmployerAccountId = _command.Caller.Id, EmployerCanApproveCommitment = true, Apprenticeships = apprenticeships };
+        }
+
+        private void SetupSuccessfulOverlapCheck()
+        {
+            var apprenticeship = _commitment.Apprenticeships.First();
+            var apprenticeshipResult = new ApprenticeshipResult { Uln = apprenticeship.ULN };
+            _apprenticeshipRepository.Setup(x => x.GetActiveApprenticeshipsByUlns(It.IsAny<IEnumerable<string>>())).ReturnsAsync(new List<ApprenticeshipResult> {apprenticeshipResult});
+            _overlapRules = new Mock<IApprenticeshipOverlapRules>();
+            _overlapRules.Setup(x => x.DetermineOverlap(It.Is<ApprenticeshipOverlapValidationRequest>(r => r.Uln == apprenticeship.ULN && r.ApprenticeshipId == apprenticeship.Id && r.StartDate == apprenticeship.StartDate.Value && r.EndDate == apprenticeship.EndDate.Value), apprenticeshipResult)).Returns(ValidationFailReason.None);
         }
     }
 }
--- src/SFA.DAS.Commitments.Application/Commands/EmployerApproveCohort/EmployerApproveCohortCommandHandler.cs
+++ src/SFA.DAS.Commitments.Application/Commands/EmployerApproveCohort/EmployerApproveCohortCommandHandler.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
 using MediatR;
 using SFA.DAS.Commitments.Application.Exceptions;
+using SFA.DAS.Commitments.Application.Queries.GetOverlappingApprenticeships;
+using SFA.DAS.Commitments.Application.Rules;
+using SFA.DAS.Commitments.Application.Services;
 using SFA.DAS.Commitments.Domain.Data;
 using SFA.DAS.Commitments.Domain.Entities;
+using SFA.DAS.Commitments.Domain.Entities.Validation;
 
 namespace SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort
 {
@@ -12,11 +18,13 @@ namespace SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort
     {
         private readonly AbstractValidator<EmployerApproveCohortCommand> _validator;
         private readonly ICommitmentRepository _commitmentRepository;
+        private readonly OverlappingApprenticeshipService _overlappingApprenticeshipService;
 
-        public EmployerApproveCohortCommandHandler(AbstractValidator<EmployerApproveCohortCommand> validator, ICommitmentRepository commitmentRepository)
+        public EmployerApproveCohortCommandHandler(AbstractValidator<EmployerApproveCohortCommand> validator, ICommitmentRepository commitmentRepository, IApprenticeshipRepository apprenticeshipRepository, IApprenticeshipOverlapRules overlapRules)
         {
             _validator = validator;
             _commitmentRepository = commitmentRepository;
+            _overlappingApprenticeshipService = new OverlappingApprenticeshipService(apprenticeshipRepository, overlapRules);
         }
 
         protected override async Task HandleCore(EmployerApproveCohortCommand message)
@@ -24,13 +32,24 @@ namespace SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort
             _validator.ValidateAndThrow(message);
 
             var commitment = await GetCommitment(message.CommitmentId);
-            CheckCommitmentCanBeApproved(commitment);
+            await CheckCommitmentCanBeApproved(commitment, message.Caller.Id);
         }
 
-        private static void CheckCommitmentCanBeApproved(Commitment commitment)
+        private async Task CheckCommitmentCanBeApproved(Commitment commitment, long callerEmployerAccountId)
         {
             CheckCommitmentStatus(commitment);
             CheckEditStatus(commitment);
+            CheckAuthorization(callerEmployerAccountId, commitment);
+            CheckStateForApproval(commitment);
+            await CheckOverlaps(commitment);
+        }
+
+        private async Task CheckOverlaps(Commitment commitment)
+        {
+            if (await _overlappingApprenticeshipService.CommitmentHasOverlappingApprenticeships(commitment))
+            {
+                throw new ValidationException("Unable to approve commitment with overlapping apprenticeships");
+            }
         }
 
         private async Task<Commitment> GetCommitment(long commitmentId)
@@ -52,6 +71,22 @@ namespace SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort
             if (commitment.EditStatus != EditStatus.Both && commitment.EditStatus != EditStatus.EmployerOnly)
             {
                 throw new UnauthorizedException($"Employer not allowed to edit commitment: {commitment.Id}");
+            }
+        }
+
+        private static void CheckAuthorization(long employerAccountId, Commitment commitment)
+        {
+            if (commitment.EmployerAccountId != employerAccountId)
+            {
+                throw new UnauthorizedException($"Employer {employerAccountId} not authorised to access commitment: {commitment.Id}, expected employer {commitment.EmployerAccountId}");
+            }
+        }
+
+        private static void CheckStateForApproval(Commitment commitment)
+        {
+            if (!commitment.EmployerCanApproveCommitment)
+            {
+                throw new InvalidOperationException($"Commitment {commitment.Id} cannot be approved because apprentice information is incomplete");
             }
         }
     }
