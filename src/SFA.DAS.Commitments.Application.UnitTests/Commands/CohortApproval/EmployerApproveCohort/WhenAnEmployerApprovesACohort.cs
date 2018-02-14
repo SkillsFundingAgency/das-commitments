8ef1940e7124db17e60d19c75aed391105d634ae
Michael Young <mick.young1981@googlemail.com>
2018-02-14 10:43:07 +0000

    Interim checkin of EmployerApproveCohortCommandHandler implementation

--- src/SFA.DAS.Commitments.Application.UnitTests/Commands/CohortApproval/EmployerApproveCohort/WhenAnEmployerApprovesACohort.cs
+++ src/SFA.DAS.Commitments.Application.UnitTests/Commands/CohortApproval/EmployerApproveCohort/WhenAnEmployerApprovesACohort.cs
@@ -1,6 +1,67 @@
-﻿namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.EmployerApproveCohort
+﻿using System;
+using FluentValidation;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort;
+using SFA.DAS.Commitments.Application.Exceptions;
+using SFA.DAS.Commitments.Domain;
+using SFA.DAS.Commitments.Domain.Data;
+using SFA.DAS.Commitments.Domain.Entities;
+
+namespace SFA.DAS.Commitments.Application.UnitTests.Commands.CohortApproval.EmployerApproveCohort
 {
+    [TestFixture]
     public class WhenAnEmployerApprovesACohort
     {
+        private AbstractValidator<EmployerApproveCohortCommand> _validator;
+        private Mock<ICommitmentRepository> _commitmentRepository;
+        private EmployerApproveCohortCommandHandler _target;
+        private EmployerApproveCohortCommand _command;
+        private Commitment _commitment;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _command = new EmployerApproveCohortCommand { Caller = new Caller(213, CallerType.Employer), CommitmentId = 123, LastUpdatedByName = "Test", LastUpdatedByEmail = "test@email.com" };
+            _commitment = new Commitment { CommitmentStatus = CommitmentStatus.New, EditStatus = EditStatus.ProviderOnly, Id = _command.CommitmentId };
+
+            _validator = new EmployerApproveCohortCommandValidator();;
+            _commitmentRepository = new Mock<ICommitmentRepository>();
+            _commitmentRepository.Setup(x => x.GetCommitmentById(_command.CommitmentId)).ReturnsAsync(_commitment);
+            
+            _target = new EmployerApproveCohortCommandHandler(_validator, _commitmentRepository.Object);
+        }
+
+        [Test]
+        public void ThenIfValidationFailsThenTheCommitmentCannotBeApproved()
+        {
+            _command.Caller = null;
+
+            Assert.ThrowsAsync<ValidationException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCommitmentIsDeletedThenItCannotBeApproved()
+        {
+            _commitment.CommitmentStatus = CommitmentStatus.Deleted;
+
+            Assert.ThrowsAsync<InvalidOperationException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCommitmentCanOnlyBeEditedByTheProviderThenItCannotBeApproved()
+        {
+            _commitment.EditStatus = EditStatus.ProviderOnly;
+
+            Assert.ThrowsAsync<UnauthorizedException>(() => _target.Handle(_command));
+        }
+
+        [Test]
+        public void ThenIfTheCommitmentCannotEditedThenItCannotBeApproved()
+        {
+            _commitment.EditStatus = EditStatus.Neither;
+
+            Assert.ThrowsAsync<UnauthorizedException>(() => _target.Handle(_command));
+        }
     }
 }
--- src/SFA.DAS.Commitments.Application/Commands/EmployerApproveCohort/EmployerApproveCohortCommandHandler.cs
+++ src/SFA.DAS.Commitments.Application/Commands/EmployerApproveCohort/EmployerApproveCohortCommandHandler.cs
@@ -1,13 +1,58 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
+using SFA.DAS.Commitments.Application.Exceptions;
+using SFA.DAS.Commitments.Domain.Data;
+using SFA.DAS.Commitments.Domain.Entities;
 
 namespace SFA.DAS.Commitments.Application.Commands.EmployerApproveCohort
 {
     public sealed class EmployerApproveCohortCommandHandler : AsyncRequestHandler<EmployerApproveCohortCommand>
     {
-        protected override Task HandleCore(EmployerApproveCohortCommand message)
+        private readonly AbstractValidator<EmployerApproveCohortCommand> _validator;
+        private readonly ICommitmentRepository _commitmentRepository;
+
+        public EmployerApproveCohortCommandHandler(AbstractValidator<EmployerApproveCohortCommand> validator, ICommitmentRepository commitmentRepository)
         {
-            throw new System.NotImplementedException();
+            _validator = validator;
+            _commitmentRepository = commitmentRepository;
+        }
+
+        protected override async Task HandleCore(EmployerApproveCohortCommand message)
+        {
+            _validator.ValidateAndThrow(message);
+
+            var commitment = await GetCommitment(message.CommitmentId);
+            CheckCommitmentCanBeApproved(commitment);
+        }
+
+        private static void CheckCommitmentCanBeApproved(Commitment commitment)
+        {
+            CheckCommitmentStatus(commitment);
+            CheckEditStatus(commitment);
+        }
+
+        private async Task<Commitment> GetCommitment(long commitmentId)
+        {
+            var commitment = await _commitmentRepository.GetCommitmentById(commitmentId);
+            return commitment;
+        }
+
+        private static void CheckCommitmentStatus(Commitment commitment)
+        {
+            if (commitment.CommitmentStatus == CommitmentStatus.Deleted)
+            {
+                throw new InvalidOperationException($"Commitment {commitment.Id} cannot be updated because status is {commitment.CommitmentStatus}");
+            }
+        }
+
+        private static void CheckEditStatus(Commitment commitment)
+        {
+            if (commitment.EditStatus != EditStatus.Both && commitment.EditStatus != EditStatus.EmployerOnly)
+            {
+                throw new UnauthorizedException($"Employer not allowed to edit commitment: {commitment.Id}");
+            }
         }
     }
 }
