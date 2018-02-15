14974fa4c3c1d140ae68606c127c3f3729d7cc86
phildavies <github@phildavies.co.uk>
2018-02-15 14:21:25 +0000

    remove todo only

--- src/SFA.DAS.Commitments.Infrastructure/Services/PaymentEventsService.cs
+++ src/SFA.DAS.Commitments.Infrastructure/Services/PaymentEventsService.cs
@@ -59,9 +59,6 @@ namespace SFA.DAS.Commitments.Infrastructure.Services
                 ?? new DataLockStatus[0];
         }
 
-        //todo: if we change this to return a enumerable mapped version of submissionevent, we should remove
-        // SFA.DAS.Provider.Events.Api.Client package from SFA.DAS.Commitments.Domain
-        // (and ..client && ..types references from there too)
         public async Task<PageOfResults<SubmissionEvent>> GetSubmissionEvents(long sinceEventId = 0, DateTime? sinceTime = null, long ukprn = 0, int page = 1)
         {
             return await _retryPolicy.ExecuteAsync(() => _paymentsEventsApi.GetSubmissionEvents(sinceEventId, sinceTime, ukprn, page));
