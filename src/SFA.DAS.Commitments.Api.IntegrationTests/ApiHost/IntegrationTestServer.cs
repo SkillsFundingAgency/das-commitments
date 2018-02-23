b7db560ffcb4587bd497bdd5bf7c709baa40c60b
phildavies <github@phildavies.co.uk>
2018-02-23 14:06:05 +0000

    first (non-working) in-memory self hosting int test

--- src/SFA.DAS.Commitments.Api.IntegrationTests/ApiHost/IntegrationTestServer.cs
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/ApiHost/IntegrationTestServer.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Microsoft.Owin.Testing;
+using Owin;
+using SFA.DAS.Commitments.Api.Controllers;
 
 namespace SFA.DAS.Commitments.Api.IntegrationTests.ApiHost
 {
     class IntegrationTestServer
     {
+        public static HttpClient Client => Instance.TestClient;
+
+        private static readonly IntegrationTestServer Instance = new IntegrationTestServer();
+
+        private static TestServer _server;
+
+        private HttpClient TestClient { get; }
+
+        protected IntegrationTestServer()
+        {
+            StartServer();
+            //TestClient = StartClient();
+            TestClient = _server.HttpClient; // todo: remove levels of indirection if not required
+        }
+
+        void StartServer()
+        {
+            _server = TestServer.Create<Startup>();
+        }
+
+        public static void Shutdown()
+        {
+            _server.Dispose();
+        }
     }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            config.Services.Replace(typeof(IAssembliesResolver), new TestWebApiResolver());
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+
+    public class TestWebApiResolver : DefaultAssembliesResolver
+    {
+        public override ICollection<Assembly> GetAssemblies()
+        {
+            return new List<Assembly> { typeof(EmployerController).Assembly };
+        }
+    }
+
+    //HttpResponseMessage response = await server.HttpClient.GetAsync("/");
+    //// TODO: Validate response
+    //}
+
+    //Requests can also be constructed and submitted with the following helper methods:
+
+    //HttpResponseMessage response = await server.CreateRequest("/")
+    //.AddHeader("header1", "headervalue1")
+    //.GetAsync();
 }
---
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/Tests/WhenSimulatingRealWorldApprenticeshipLoad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Api.IntegrationTests.ApiHost;
+using SFA.DAS.Commitments.Api.Types.Apprenticeship;
+
+namespace SFA.DAS.Commitments.Api.IntegrationTests.Tests
+{
+    [TestFixture]
+    public class WhenSimulatingRealWorldApprenticeshipLoad
+    {
+        [Test]
+        public async Task ThenSumfinkOrNuffink()
+        {
+            //todo: the test will have to create these of course mf
+            string employerAccountId = "8315";
+            string apprenticeshipId = "1";
+
+            var results = await IntegrationTestServer.Client.GetAsync(
+                    $"/api/employer/{employerAccountId}/apprenticeships/{apprenticeshipId}");
+
+            var resultsAsString = await results.Content.ReadAsStringAsync();
+            var apprenticeship = JsonConvert.DeserializeObject<Apprenticeship>(resultsAsString);
+        }
+    }
+}
