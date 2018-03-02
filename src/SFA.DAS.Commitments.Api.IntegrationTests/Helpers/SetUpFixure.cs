0e44de1a822ac10aeafe845b3e60877884409cf3
phildavies <github@phildavies.co.uk>
2018-03-02 13:17:59 +0000

    setupfixture wip

--- src/SFA.DAS.Commitments.Api.IntegrationTests/Helpers/SetUpFixure.cs
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/Helpers/SetUpFixure.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NUnit.Framework;
 using SFA.DAS.Commitments.Api.IntegrationTests.ApiHost;
 using SFA.DAS.Commitments.Api.IntegrationTests.DatabaseSetup;
