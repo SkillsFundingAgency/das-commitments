c6abd39d5f14f8d15f5376053844e85c7f817b98
phildavies <github@phildavies.co.uk>
2018-03-16 10:41:22 +0000

    tidy up

--- src/SFA.DAS.Commitments.Api.IntegrationTests/DatabaseSetup/TestDataVolume.cs
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/DatabaseSetup/TestDataVolume.cs
@@ -11,18 +11,21 @@ namespace SFA.DAS.Commitments.Api.IntegrationTests.DatabaseSetup
         public const int MinNumberOfApprenticeships = 250000;
         //MinNumberOfCohorts?
         public const int MaxNumberOfApprenticeshipsInCohort = 80;
+
+        #region ApprenticeshipUpdate
+
         public const double ApprenticeshipUpdatesToApprenticeshipsRatio = 0.025d;
-        public const double ErrorDataLockStatusProbability = 0.025d;
         public const int MaxApprenticeshipUpdatesPerApprenticeship = 5; //todo: skew to lower?
+
+        #endregion ApprenticeshipUpdate
 
         #region DataLockStatus
 
-        //todo: change to 0 - 3
+        public const double ErrorDataLockStatusProbability = 0.025d;
+
         // some apprenticeships don't have datalockstatuses (yet)
         // you get 1 per price episode identifier, and per academic year, so currently there are about 1 per apprenticeship
         // but in subsequent years (with courses lasting say 3 years), the average will rise to about 3
-        // we should probably define percentages here, and not use the 2 ratios above for the volume, just ratio of success to error
-        //public const int MaxDataLockStatusesPerApprenticeship = 5; // status & error versions?
 
         public static DataLockStatusCountProbability DataLockStatusesPerApprenticeshipProbability = new DataLockStatusCountProbability(
             new[]
