e595ee073d71b399737fba43c52072d69448b996
phildavies <github@phildavies.co.uk>
2018-03-20 14:28:28 +0000

    split out classes into own files

--- src/SFA.DAS.Commitments.Api.IntegrationTests/ApiHost/IntegrationTestServer.cs
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/ApiHost/IntegrationTestServer.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Reflection;
-using System.Web;
-using System.Web.Http;
-using System.Web.Http.Dispatcher;
-using System.Web.SessionState;
+﻿using System.Net.Http;
 using Microsoft.Owin.Testing;
-using Owin;
-using StructureMap;
-using SFA.DAS.Commitments.Api.Controllers;
-using SFA.DAS.Commitments.Api.DependencyResolution;
-using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.Commitments.Api.IntegrationTests.ApiHost
 {
@@ -35,69 +22,6 @@ namespace SFA.DAS.Commitments.Api.IntegrationTests.ApiHost
         public static void Shutdown()
         {
             _server.Dispose();
-        }
-    }
-
-    public class Startup
-    {
-        public void Configuration(IAppBuilder appBuilder)
-        {
-            var config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-
-            config.Services.Replace(typeof(IAssembliesResolver), new TestWebApiResolver());
-
-            var container = IoC.Initialize();
-
-            container.Configure(c => c.AddRegistry<TestRegistry>());
-
-            config.DependencyResolver = new StructureMapWebApiDependencyResolver(container);
-
-            appBuilder.UseWebApi(config);
-        }
-    }
-
-    public class TestRegistry : Registry
-    {
-        public TestRegistry()
-        {
-            // OWIN self-hosting doesn't have a HttpContext, but we need one because
-            // StructureMapDependencyScope uses the HttpContext to stash a NestedContainer
-            // if we get the authorization working, we could change StructureMapDependencyScope
-            // (also WebLoggingContext uses it, but we replace the one in the container with a null htppcontext - which it handles fine)
-            var httpContext = FakeHttpContext("http://localhost");
-            var httpContextBase = new HttpContextWrapper(httpContext);
-            For<HttpContextBase>().Use(httpContextBase);
-
-            //For<ILoggingContext>().ClearAll().Use(x => new WebLoggingContext(httpContextBase));
-            For<ILoggingContext>().ClearAll().Use(x => new WebLoggingContext(null));
-        }
-
-        public static HttpContext FakeHttpContext(string url)
-        {
-            var uri = new Uri(url);
-            var httpRequest = new HttpRequest(string.Empty, uri.ToString(), uri.Query.TrimStart('?'));
-            var stringWriter = new StringWriter();
-            var httpResponse = new HttpResponse(stringWriter);
-            var httpContext = new HttpContext(httpRequest, httpResponse);
-
-            var sessionContainer = new HttpSessionStateContainer("id",
-                new SessionStateItemCollection(),
-                new HttpStaticObjectsCollection(),
-                10, true, HttpCookieMode.AutoDetect,
-                SessionStateMode.InProc, false);
-
-            SessionStateUtility.AddHttpSessionStateToContext(httpContext, sessionContainer);
-
-            return httpContext;
-        }
-    }
-
-    public class TestWebApiResolver : DefaultAssembliesResolver
-    {
-        public override ICollection<Assembly> GetAssemblies()
-        {
-            return new List<Assembly> { typeof(EmployerController).Assembly };
         }
     }
 }
