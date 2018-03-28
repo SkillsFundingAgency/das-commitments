aafaf2299aa3c7c881b60123784d4d03e9d87148
Paul Graham <paul.graham@amido.com>
2018-03-28 15:56:48 +0100

    new structures for transfer request. includes sparse columns and filter index

---
+++ src/SFA.DAS.Commitments.Database/Tables/TransferRequest.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[TransferRequest]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [CommitmentId] BIGINT NOT NULL,
+	[TrainingCourses] NVARCHAR(MAX) NOT NULL,
+	[Cost] MONEY NOT NULL,
+	[Status] TINYINT NOT NULL,
+	[TransferApprovalActionedByEmployerName] NVARCHAR(255),
+	[TransferApprovalActionedByEmployerEmail] NVARCHAR(255),
+	[TransferApprovalActionedOn] DATETIME2,
+
+    CONSTRAINT [FK_TransferRequest_Commitment] FOREIGN KEY ([CommitmentId]) REFERENCES [Commitment]([Id])
+
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_TransferRequest_CommitmentId] ON [dbo].[TransferRequest] ([CommitmentId])
