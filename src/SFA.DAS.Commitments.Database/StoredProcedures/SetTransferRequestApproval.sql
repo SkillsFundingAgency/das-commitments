e345249bbeca983458cc690d9188f7b506af6da3
Paul Graham <paul.graham@amido.com>
2018-03-29 09:22:26 +0100

    Added SPs for Starting a Transfer Request and for Approving/Rejecting a request

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/SetTransferRequestApproval.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[SetTransferRequestApproval]
+	@transferRequestId BIGINT,
+	@commitmentId BIGINT,
+	@transferApprovalStatus TINYINT,
+	@transferStatusSetByEmployerName NVARCHAR(255),
+	@transferStatusSetByEmployerEmail NVARCHAR(255)
+AS
+BEGIN
+
+	DECLARE @OldApprovalStatus AS TINYINT = NULL
+
+	UPDATE [dbo].[Commitment] SET 
+		@OldApprovalStatus = TransferApprovalStatus,
+		TransferApprovalStatus = @transferApprovalStatus
+	WHERE Id = @commitmentId;
+
+	IF @@ROWCOUNT != 1 
+		THROW 50000, 'Incorrect Number of Commitment records were updated', 1;
+
+	IF @OldApprovalStatus != 0 
+		THROW 51000, 'Transfer Approval Status is not in the Pending State on the Commitment Record', 1;
+
+	UPDATE [dbo].[TransferRequest] SET 
+		@OldApprovalStatus = [Status],
+		[Status] = @transferApprovalStatus,
+		TransferApprovalActionedByEmployerEmail = @transferStatusSetByEmployerEmail,
+		TransferApprovalActionedByEmployerName = @transferStatusSetByEmployerName,
+		TransferApprovalActionedOn = GETDATE()
+	WHERE Id = @transferRequestId AND CommitmentId = @commitmentId;
+
+	IF @@ROWCOUNT != 1 
+		THROW 50000, 'Incorrect Number of TransferRequest records were updated', 1;
+
+	IF @OldApprovalStatus != 0 
+		THROW 51000, 'Transfer Approval Status is not in the Pending State on the TransferRequest Record', 1;
+
+
+END
---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/StartATransferRequest.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[StartATransferRequest]
+	@commitmentid BIGINT,
+	@cost MONEY,
+	@trainingCourses NVARCHAR(MAX)
+AS
+BEGIN
+
+	INSERT INTO [dbo].[TransferRequest] (CommitmentId, Cost, TrainingCourses, [Status]) VALUES (@commitmentid, @cost, @trainingCourses, 0)
+
+	IF @@ROWCOUNT != 1 
+		THROW 50000, 'Failed to Insert TransferRequest record', 1;
+	
+	UPDATE [dbo].[Commitment] SET 
+		TransferApprovalStatus = 0
+	WHERE Id = @commitmentid;
+
+	IF @@ROWCOUNT != 1 
+		THROW 50000, 'Incorrect Number of Commitment records were updated', 1;
+
+END
