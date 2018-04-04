48472ebe072b7968175e35601d1a1fe8dbba5a5f
Paul Graham <paul.graham@amido.com>
2018-04-04 08:54:59 +0100

    Added Unit tests for new controllers and queries

---
+++ src/SFA.DAS.Commitments.Api.UnitTests/Orchestrators/Employer/WhenGettingTransferRequestsForAReciever.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Application.Queries.GetTransferRequestsForReceiver;
+using SFA.DAS.Commitments.Domain.Entities;
+
+namespace SFA.DAS.Commitments.Api.UnitTests.Orchestrators.Employer
+{
+    [TestFixture]
+    public class WhenGettingTransferRequestsForAReciever : EmployerOrchestratorTestBase
+    {
+        private List<TransferRequestSummary> _matches;
+
+        [SetUp]
+        public new void SetUp()
+        {
+            _matches = new List<TransferRequestSummary>();
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<GetTransferRequestsForReceiverRequest>()))
+                .ReturnsAsync(new GetTransferRequestsForReceiverResponse() { Data = _matches });
+
+        }
+
+        [Test]
+        public async Task ThenAppropriateCommandIsSentToMediator()
+        {
+            //Act
+            await Orchestrator.GetTransferRequestsForReceiver(123);
+             
+            //Assert
+            MockMediator.Verify(
+                x => x.SendAsync(It.Is<GetTransferRequestsForReceiverRequest>(p =>
+                    p.TransferReceiverAccountId == 123)), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenShouldReturnFoundMatches()
+        {
+            // Arrange
+
+            //Act
+            var result = await Orchestrator.GetTransferRequestsForReceiver(123);
+
+            Assert.AreSame(_matches, result);
+        }
+    }
+}
---
+++ src/SFA.DAS.Commitments.Api.UnitTests/Orchestrators/Employer/WhenGettingTransferRequestsForASender.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Application.Queries.GetTransferRequestsForSender;
+using SFA.DAS.Commitments.Domain.Entities;
+
+namespace SFA.DAS.Commitments.Api.UnitTests.Orchestrators.Employer
+{
+    [TestFixture]
+    public class WhenGettingTransferRequestsForASender : EmployerOrchestratorTestBase
+    {
+        private List<TransferRequestSummary> _matches;
+
+        [SetUp]
+        public new void SetUp()
+        {
+            _matches = new List<TransferRequestSummary>();
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<GetTransferRequestsForSenderRequest>()))
+                .ReturnsAsync(new GetTransferRequestsForSenderResponse { Data = _matches });
+
+        }
+
+        [Test]
+        public async Task ThenAppropriateCommandIsSentToMediator()
+        {
+            //Act
+            await Orchestrator.GetTransferRequestsForSender(123);
+             
+            //Assert
+            MockMediator.Verify(
+                x => x.SendAsync(It.Is<GetTransferRequestsForSenderRequest>(p =>
+                    p.TransferSenderAccountId == 123)), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenShouldReturnFoundMatches()
+        {
+            // Arrange
+
+            //Act
+            var result = await Orchestrator.GetTransferRequestsForSender(123);
+
+            Assert.AreSame(_matches, result);
+        }
+    }
+}
