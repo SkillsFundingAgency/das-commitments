13e351665bdcfed4646e457613d81f3da0aa08b9
phildavies <github@phildavies.co.uk>
2018-04-20 11:07:20 +0100

    add (placeholder) integration test for GetApprenticeships

---
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/Tests/API/GetApprenticeships.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Api.IntegrationTests.ApiHost;
+using SFA.DAS.Commitments.Api.Types.Apprenticeship;
+using SFA.DAS.Commitments.Api.Types.Commitment;
+
+namespace SFA.DAS.Commitments.Api.IntegrationTests.Tests.API
+{
+    [TestFixture]
+    public sealed class GetApprenticeships
+    {
+        //todo: if we make optimisation to GetApprenticeships we need to cover the refactoring with integrationtest(s)
+        //      which means we have to set up a specific data set
+        [Test]
+        public async Task GetEmployerApprenticeships()
+        {
+            long employerId = 187;
+            var url = $"api/employer/{employerId}/apprenticeships";
+
+            var stopwatch = Stopwatch.StartNew();
+            // block on result, rather than awaiting as it gives a more realistic timing
+            var result = IntegrationTestServer.Client.GetAsync(url).Result;
+            //stopwatch.Elapsed;
+
+            Assert.IsTrue(result.IsSuccessStatusCode);
+
+            var resultsAsString = await result.Content.ReadAsStringAsync();
+            var apprenticeships = JsonConvert.DeserializeObject<IEnumerable<Apprenticeship>>(resultsAsString);
+        }
+    }
+}
