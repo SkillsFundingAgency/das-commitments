05dae849c9f58ee34176d26f1b62327082919073
phildavies <github@phildavies.co.uk>
2018-04-25 13:44:17 +0100

    split GetActiveApprenticeships into 1 version for employer and 1 for provider (untested)

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetActiveApprenticeshipsForEmployer.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetActiveApprenticeshipsForEmployer]
+	@id BIGINT
+AS
+
+SELECT 
+	s.*
+	FROM ApprenticeshipSummary s
+	WHERE @id = s.EmployerAccountId
+	AND NOT s.PaymentStatus IN (0,5); --Not deleted or pre-approved
