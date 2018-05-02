55db47c060ebd927070c69e4bc7e8e4d76586f97
phildavies <github@phildavies.co.uk>
2018-05-02 10:50:57 +0100

    start of generating PriceHistory data in integration test data (price history is required to test GetApprenticeshipWithPriceHistory)

---
+++ src/SFA.DAS.Commitments.Api.IntegrationTests/DatabaseSetup/Generators/PriceHistoryGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.Commitments.Api.IntegrationTests.DatabaseSetup.Entities;
+using SFA.DAS.Commitments.Api.IntegrationTests.Helpers;
+
+namespace SFA.DAS.Commitments.Api.IntegrationTests.DatabaseSetup.Generators
+{
+    public class PriceHistoryGenerator : Generator
+    {
+        //public async Task<IEnumerable<DbSetupPriceHistory>> Generate(int numberOfNewApprenticeships,
+        //    long firstNewApprenticeshipId, long firstNewPriceHistoryId)
+        //{
+        //    await TestLog.Progress($"Generating {totalPriceHistoriesToGenerate} PriceHistories");
+
+        //}
+
+        private DbSetupPriceHistory GenerateDbSetupPriceHistory()
+        {
+            return new DbSetupPriceHistory
+            {
+                
+            };
+        }
+    }
+}
