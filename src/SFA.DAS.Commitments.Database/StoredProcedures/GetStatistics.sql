872dad81eae3ffa0fdd0486b05cf1fa1d833c573
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-05-11 15:45:44 +0100

    Create statistics api endpoint to be used by RDS for consistency checks

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetStatistics.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetStatistics]
+AS
+SELECT
+(
+	SELECT COUNT(Id)
+	FROM [Commitment]
+) AS TotalCohorts,
+(
+	SELECT COUNT(Id)
+	FROM [Apprenticeship]
+) AS TotalAprrenticeships,
+(
+	SELECT COUNT(Id)
+	FROM [Apprenticeship]
+	WHERE PaymentStatus = 1
+) AS ActiveApprenticeships
---
+++ src/SFA.DAS.Commitments.Domain/Entities/Statistics.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Commitments.Domain.Entities
+{
+    public class Statistics
+    {
+        public long TotalCohorts { get; set; }
+        public long TotalApprenticeships { get; set; }
+        public long ActiveApprenticeships { get; set; }
+    }
+}
