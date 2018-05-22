96a79197ef0636effa4759780b7d66fe8b0bf0b8
phildavies <github@phildavies.co.uk>
2018-05-22 14:11:38 +0100

    uncomment the PendingUpdateOriginator one-time post deployment sql + remove earlier one-time deployment sql that gets run each db deployment

--- src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
+++ src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
@@ -10,63 +10,11 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
------- Extended EDIT
-
--- Marking Apprenticeships as HasHadDataLockSuccess so that they cannot be used in extended edit. 
--- Only if they have a DataLockStatus with ErrirCode Success (0)
-
-UPDATE Apprenticeship 
-SET HasHadDataLockSuccess = 1
-WHERE Id IN (
-  SELECT ApprenticeshipId FROM DataLockStatus
-  WHERE ErrorCode = 0
-)
-
-
--- Setting CommitmentId or ApprenticeshipId values on new Columns
-
-BEGIN
-
-	UPDATE History
-	SET CommitmentId = EntityId, 
-	EntityId = NULL, 
-	EntityType = NULL
-	WHERE EntityType = 'Commitment'
-
-	UPDATE History
-	SET ApprenticeshipId = EntityId,
-	EntityId = NULL,  
-	EntityType = NULL
-	WHERE EntityType = 'Apprenticeship'
-
-
-	UPDATE History
-	SET ProviderId = c.ProviderId, EmployerAccountId = c.EmployerAccountId
-	FROM History h JOIN Commitment c
-	ON h.CommitmentId = c.Id
-
-	UPDATE History
-	SET ProviderId = a.ProviderId, EmployerAccountId = a.EmployerAccountId
-	FROM History h JOIN ApprenticeshipSummary a
-	ON h.ApprenticeshipId = a.Id
-
-END
-
--- This is a One time Action. We only need to import the old tansfer records on upgrade. New records will always add a TransferRequest record (when both parties approve)
-IF NOT EXISTS(SELECT * FROM [dbo].[TransferRequest]) 
-BEGIN
-
-  	INSERT INTO TransferRequest (CommitmentId, TrainingCourses, Cost, [Status], TransferApprovalActionedByEmployerEmail, TransferApprovalActionedByEmployerName, TransferApprovalActionedOn)
-	SELECT Id, '[]', (SELECT SUM(Cost) FROM Apprenticeship a WHERE a.CommitmentId = c.Id),  ISNULL(TransferApprovalStatus, 0), 
-		TransferApprovalActionedByEmployerEmail, TransferApprovalActionedByEmployerName, TransferApprovalActionedOn  
-	FROM [dbo].[Commitment] c WHERE c.TransferSenderId IS NOT NULL AND c.EditStatus = 0 AND c.CommitmentStatus = 1
-	
-END
-
---todo: one-time update to be run when the system quiesces
---UPDATE Apprenticeship
---SET    PendingUpdateOriginator = AU.Originator
---FROM   Apprenticeship A
---INNER JOIN ApprenticeshipUpdate AU
---ON     A.Id = AU.ApprenticeshipId
---	   AND AU.Status = 0;
+--one-time update to set the new PendingUpdateOriginator column on Apprenticeship from the ApprenticeshipUpdateColumn
+--todo: needs to be removes once has been deployed to production
+UPDATE Apprenticeship
+SET    PendingUpdateOriginator = AU.Originator
+FROM   Apprenticeship A
+INNER JOIN ApprenticeshipUpdate AU
+ON     A.Id = AU.ApprenticeshipId
+	   AND AU.Status = 0;
