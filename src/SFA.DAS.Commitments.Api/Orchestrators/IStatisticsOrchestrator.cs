62a50e9cd1258480a15626d8ffe5b8a4a2f12254
suniara <35219355+suniara@users.noreply.github.com>
2018-06-05 14:15:28 +0100

    Create statistics api endpoint to be used by RDS for consistency checks (#373)
    * Create statistics api endpoint to be used by RDS for consistency checks
    
    * Fixed typo in sproc GetStatistics and added index to Apprenticeship table
    
    * Included Paused PaymentStatus in ActiveApprenticeships statistic

---
+++ src/SFA.DAS.Commitments.Api/Orchestrators/IStatisticsOrchestrator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Commitments.Api.Types;
+
+namespace SFA.DAS.Commitments.Api.Orchestrators
+{
+    public interface IStatisticsOrchestrator
+    {
+        Task<ConsistencyStatistics> GetStatistics();
+    }
+}
