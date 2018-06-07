d48ac9b99120785d72ba488946bde01b6f223bb8
Phil Davies <github@phildavies.co.uk>
2018-06-07 11:52:06 +0100

    DPP-1402 - remove one-hit wonder (#380)

--- src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
+++ src/SFA.DAS.Commitments.Database/PostDeployment/PostDeploymentScript.sql
@@ -9,22 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-
---one-time update to set the new PendingUpdateOriginator column on Apprenticeship from the ApprenticeshipUpdateColumn
---todo: needs to be removes once has been deployed to production
-UPDATE Apprenticeship
-SET    PendingUpdateOriginator = AU.Originator
-FROM   Apprenticeship A
-INNER JOIN ApprenticeshipUpdate AU
-ON     A.Id = AU.ApprenticeshipId
-	   AND AU.Status = 0;
-
---after deployment, we need to check that the update didn't miss any in-flight apprenticeshipupdate changes
---this query should return 0 rows. if it doesn't, then the returned rows will need updating, either manually, or by running the update above again (rinse and repeat)
-
---select a.Id, a.PendingUpdateOriginator
---from apprenticeship a
---inner join apprenticeshipupdate au
---on a.Id = au.ApprenticeshipId
---where (au.Status = 0 and (a.PendingUpdateOriginator <> au.Originator or a.PendingUpdateOriginator is null))
---or (au.Status <> 0 and a.PendingUpdateOriginator is not null)
