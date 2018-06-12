828f313f194af0fcda6f5bb2b0531fef466bb29b
Phil Davies <github@phildavies.co.uk>
2018-06-12 09:26:42 +0100

    include apprenticeships that are transfer funded and sitting waiting for approval by the sender when checking for apprenticeship overlaps

--- src/SFA.DAS.Commitments.Database/StoredProcedures/GetActiveApprenticeshipsByULNs.sql
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetActiveApprenticeshipsByULNs.sql
@@ -1,4 +1,6 @@
-﻿CREATE PROCEDURE [dbo].[GetActiveApprenticeshipsByULNs]
+﻿-- gets all appenticeships that have been approved by employer and provider (unless subsequently completed or deleted)
+-- if transfer funded, we still return apprenticeships that haven't been approved by the sender yet
+CREATE PROCEDURE [dbo].[GetActiveApprenticeshipsByULNs]
 (
 	@ULNs [ULNTable] READONLY
 )
@@ -18,5 +20,6 @@ INNER JOIN [dbo].[Apprenticeship] a ON u.ULN = a.ULN
 INNER JOIN [dbo].[Commitment] c ON c.Id = a.CommitmentId
 WHERE
 a.AgreementStatus = 3 -- Both parties agreed - "right of the line"
-AND a.PaymentStatus IN (1,2,3) --Active, Paused or Cancelled
+AND a.PaymentStatus <= 3 --PendingApproval, Active, Paused or Cancelled
+-- (allowing PaymentStatus == 0 (PendingApproval) && AgreementStatus == 3, we catch transfer apprenticeships that have been approved by the receiver and provider, but not yet by the sender)
 END
