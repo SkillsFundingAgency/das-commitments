8c9a7bb7fb5cb046cb917efc211f62672a781472
Phil Davies <github@phildavies.co.uk>
2018-07-17 11:09:56 +0100

    add new AccountLegalEntityPublicHashedId to types & domain Commitment

--- src/SFA.DAS.Commitments.Api.Types/Commitment/Commitment.cs
+++ src/SFA.DAS.Commitments.Api.Types/Commitment/Commitment.cs
@@ -18,6 +18,7 @@ namespace SFA.DAS.Commitments.Api.Types.Commitment
         public string LegalEntityName { get; set; }
         public string LegalEntityAddress { get; set; }
         public SFA.DAS.Common.Domain.Types.OrganisationType LegalEntityOrganisationType { get; set; }
+        public string AccountLegalEntityPublicHashedId { get; set; }
         public long? ProviderId { get; set; }
         public string ProviderName { get; set; }
         public CommitmentStatus CommitmentStatus { get; set; }
