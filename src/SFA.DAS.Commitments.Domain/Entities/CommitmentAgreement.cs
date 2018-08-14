6b94ea5a8828312a4a6e69034ec67684bf296120
Phil Davies <github@phildavies.co.uk>
2018-08-14 09:10:31 +0100

    add unit test for CommitmentAgreement mapping

--- src/SFA.DAS.Commitments.Domain/Entities/CommitmentAgreement.cs
+++ src/SFA.DAS.Commitments.Domain/Entities/CommitmentAgreement.cs
@@ -6,7 +6,6 @@ namespace SFA.DAS.Commitments.Domain.Entities
     /// </summary>
     public class CommitmentAgreement
     {
-        //public long Id { get; set; }    // todo: ?? prob not
         public string Reference { get; set; }
         public string LegalEntityName { get; set; }
         public string AccountLegalEntityPublicHashedId { get; set; }
