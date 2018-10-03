9f9049b792b011120b17862a67287f6f95e3907d
chrisfoster76 <chrisfoster186@googlemail.com>
2018-10-03 11:47:40 +0100

    Re-adds deleted procs otherwise will break app during release window

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetActiveApprenticeshipsForEmployer.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetActiveApprenticeshipsForEmployer]
+	@id BIGINT
+AS
+
+SELECT 
+	s.*
+	FROM ApprenticeshipSummary s
+	WHERE @id = s.EmployerAccountId
+	AND NOT s.PaymentStatus IN (0,5); --Not deleted or pre-approved
+
+	/*
+	This proc should be deleted and has been retained only to prevent issues during release window
+	*/
---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/GetActiveApprenticeshipsForProvider.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetActiveApprenticeshipsForProvider]
+	@id BIGINT
+AS
+
+SELECT 
+	s.*
+	FROM ApprenticeshipSummary s
+	WHERE @id = s.ProviderId
+	AND NOT s.PaymentStatus IN (0,5); --Not deleted or pre-approved
+
+
+	/*
+	This proc should be deleted and has been retained only to prevent issues during release window
+	*/
