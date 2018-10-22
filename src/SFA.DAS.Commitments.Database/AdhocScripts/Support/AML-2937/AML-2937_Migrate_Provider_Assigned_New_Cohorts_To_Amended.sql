efe8979794bd05c0d7adf57147e55937ce2ad985
chrisfoster76 <chrisfoster186@googlemail.com>
2018-10-22 13:47:49 +0100

    Adds data script for migrating cohorts

---
+++ src/SFA.DAS.Commitments.Database/AdhocScripts/Support/AML-2937/AML-2937_Migrate_Provider_Assigned_New_Cohorts_To_Amended.sql
@@ -0,0 +1,20 @@
+﻿/*
+Provider draft cohorts will, in future, be denoted as those having an EditStatus=2 and LastAction=0.
+This pair of values currently denotes a cohort that has been sent straight to the provider during the employer
+cohort creation journey.
+
+This script updates the LastAction to 1 for cohorts sent straight to the provider. This will make no material difference,
+since the cohorts are gathered into the same "For Review" bingo box anyway.
+
+This will then free up this set of values for use later to denote Provider drafts.
+*/
+
+
+update Commitment
+set LastAction = 1 --Set to 1 ("Amend")
+where
+EditStatus = 2 --Provider assigned 
+and LastAction = 0 --None ("New")
+
+
+
