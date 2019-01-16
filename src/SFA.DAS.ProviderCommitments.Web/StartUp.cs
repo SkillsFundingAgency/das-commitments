0162d2689b754be0c976dd7969bcdd3734dab271
Paul Graham <paul.graham@amido.com>
2019-01-16 10:38:33 +0000

    Added Owin Startup class

---
+++ src/SFA.DAS.ProviderCommitments.Web/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Owin;
+using Microsoft.Owin;
+using SFA.DAS.ProviderCommitments.Web;
+
+[assembly: OwinStartup(typeof(StartUp))]
+
+namespace SFA.DAS.ProviderCommitments.Web
+{
+
+
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var a = 1;
+            a++;
+            //var container = StructuremapMvc.StructureMapDependencyScope.Container;
+            //var logger = container.GetInstance<ILog>();
+
+            //logger.Info("Starting Provider Relationships web application");
+        }
+    }
+}
+
