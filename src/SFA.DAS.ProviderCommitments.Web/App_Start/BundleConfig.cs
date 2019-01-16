516793a96065d0d1a5ca97bbb7b87621b3e22fb0
Paul Graham <paul.graham@amido.com>
2019-01-16 09:12:28 +0000

    Added basic MVC project and added
    <RestoreProjectStyle>PackageReference</RestoreProjectStyle>
    
    so it uses Package references

---
+++ src/SFA.DAS.ProviderCommitments.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Optimization;
+
+namespace SFA.DAS.ProviderCommitments.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
