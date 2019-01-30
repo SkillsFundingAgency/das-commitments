b5f2e88a75adc7719dd22bb3057b73ddc3791751
Paul Graham <paul.graham@amido.com>
2019-01-30 12:46:35 +0000

    Added basic Logging to API project

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace SFA.DAS.CommitmentsV2.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseNLog();
+    }
+}
