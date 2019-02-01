04b35d592aa593f9bf9f6a80d52475e3effdffa2
Paul Graham <paul.graham@amido.com>
2019-02-01 16:36:12 +0000

    removed approval projects

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -15,36 +15,6 @@ Target "Dotnet Restore" (fun _ ->
                 Project = ".\\SFA.DAS.Commitments.Api.Types" })
     DotNetCli.Restore(fun p ->
         { p with
-                Project = ".\\Approvals\\Tools\\SFA.DAS.Commitments.EFCoreTester" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.UnitTests" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.Api" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.Host" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.Jobs" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.Messages" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.MessageHandlers" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.MessageHandlers.UnitTests" })
-    DotNetCli.Restore(fun p ->
-        { p with
-                Project = ".\\Approvals\\SFA.DAS.ProviderCommitments.Web" })
-    DotNetCli.Restore(fun p ->
-        { p with
                 Project = ".\\CommitmentsV2\\SFA.DAS.CommitmentsV2.Api" })
     DotNetCli.Restore(fun p ->
         { p with
