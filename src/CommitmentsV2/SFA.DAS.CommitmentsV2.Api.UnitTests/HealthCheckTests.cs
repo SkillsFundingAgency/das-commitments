a39c98ce3fa1fa03649272259461545da81e6d15
Paul Graham <paul.graham@amido.com>
2019-02-02 07:28:53 +0000

    Added a basic pipeline test and used the in-memory test server

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/HealthCheckTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.ErrorHandler;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.CommitmentsV2.Api.UnitTests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class HealthCheckTests : FluentTest<HealthCheckFixture>
+    {
+        [Test]
+        public Task CallHealthCheckEndpoint_ThenShouldReturnOkResponse()
+        {
+            return TestAsync(f => f.Client.GetAsync("/api/health-check"), (f,r) => r.StatusCode.Should().Be(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public Task CallUnknownPage_ThenShouldReturnNotFoundResponse()
+        {
+            return TestAsync(f => f.Client.GetAsync("/no-such-page"), (f, r) => r.StatusCode.Should().Be(HttpStatusCode.NotFound));
+        }
+    }
+
+    public class HealthCheckFixture
+    {
+        public readonly WebApplicationFactory<Startup> Factory;
+        public readonly HttpClient Client;
+
+        public HealthCheckFixture()
+        {
+            Factory = new WebApplicationFactory<Startup>();
+            Client = Factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+    }
+}
