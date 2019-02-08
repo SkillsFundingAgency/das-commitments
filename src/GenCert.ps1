f2fc5f5d0092d32149eeba2f1a3db6dfc87d8add
Iain Stevenson <IainStevenson@live.co.uk>
2019-02-08 09:08:20 +0000

    Fix to GenCert powershell script to be more specific about the localhost.pfx location. this repo follows the pattern in das-support-portal

--- src/GenCert.ps1
+++ src/GenCert.ps1
@@ -3,4 +3,4 @@ $extended_date = $date_now.AddYears(10)
 $cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname localhost, localhost -notafter $extended_date -KeyLength 4096
 $pwd = ConvertTo-SecureString -String ‘password’ -Force -AsPlainText
 $path = ‘cert:\localMachine\my\’ + $cert.thumbprint
-Export-PfxCertificate -cert $path -FilePath certificates\localhost.pfx -Password $pwd
+Export-PfxCertificate -cert $path -FilePath $PSScriptpath\certificates\localhost.pfx -Password $pwd
