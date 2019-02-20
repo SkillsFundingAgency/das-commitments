8d03d090d2999085b933aa8a88706bdc320cded4
Paul Graham <paul.graham@amido.com>
2019-02-20 11:55:53 +0000

    Added Accounts and LegalEntities tables to DB

---
+++ src/SFA.DAS.Commitments.Database/Tables/Accounts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(                                           
+    [Id] BIGINT NOT NULL,
+    [HashedId] CHAR(6) NOT NULL,
+    [PublicHashedId] CHAR(6) NOT NULL,
+    [Name] NVARCHAR(100) NOT NULL,
+    [Created] DATETIME2 NOT NULL,
+    [Updated] DATETIME2 NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
