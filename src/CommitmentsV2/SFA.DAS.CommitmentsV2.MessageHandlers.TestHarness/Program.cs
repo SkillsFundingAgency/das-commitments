502e48b8e3ba260f092d3bfddc5a7b69d1c71cf4
Paul Graham <paul.graham@amido.com>
2019-02-25 09:23:07 +0000

    Added environment variables to be able to test locally and remotely

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.TestHarness/Program.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.TestHarness/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using NServiceBus;
-using SFA.DAS.EmployerAccounts.Messages.Events;
+using SFA.DAS.CommitmentsV2.Configuration;
+using SFA.DAS.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
 using SFA.DAS.NServiceBus;
 using SFA.DAS.NServiceBus.AzureServiceBus;
 using SFA.DAS.NServiceBus.NewtonsoftJsonSerializer;
@@ -12,8 +17,20 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.TestHarness
     {
         public static async Task Main()
         {
+            var builder = new ConfigurationBuilder()
+                .AddAzureTableStorage(CommitmentsConfigurationKeys.CommitmentsV2MessageHandler);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var provider = new ServiceCollection()
+                .AddOptions()
+                .Configure<CommitmentsV2Configuration>(configuration.GetSection(CommitmentsConfigurationKeys.CommitmentsV2MessageHandler)).BuildServiceProvider();
+
+            var config = provider.GetService<IOptions<CommitmentsV2Configuration>>().Value.NServiceBusConfiguration;
+            var isDevelopment = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName) == "LOCAL";
+
             var endpointConfiguration = new EndpointConfiguration("SFA.DAS.CommitmentsV2.MessageHandlers.TestHarness")
-                .UseAzureServiceBusTransport(true, () => "", r => { })
+                .UseAzureServiceBusTransport(isDevelopment, () => config.ServiceBusConnectionString, r => { })
                 .UseErrorQueue()
                 .UseInstallers()
                 .UseMessageConventions()
