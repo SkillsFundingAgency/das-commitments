1c15f432d1384f1498b4a4523afd9f62c7a841f2
Paul Graham <paul.graham@amido.com>
2019-03-04 12:18:18 +0000

    moved code to branch off master

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/CreateCohortRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SFA.DAS.CommitmentsV2.Api.Types
+{
+    public sealed class CreateCohortRequest
+    {
+        public string UserId { get; set; }
+
+        public NewCohort Cohort { get; set; }
+
+        public NewDraftApprenticeship DraftApprenticeship { get; set; }
+
+        public sealed class NewCohort
+        {
+            public long EmployerAccountId { get; set; }
+            public string LegalEntityId { get; set; }
+            public long ProviderId { get; set; }
+        }
+
+        public sealed class NewDraftApprenticeship
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public string ULN { get; set; }
+            public string TrainingCode { get; set; }
+            public string TrainingName { get; set; }
+            public int? Cost { get; set; }
+            public DateTime? StartDate { get; set; }
+            public DateTime? EndDate { get; set; }
+            public string ProviderReference { get; set; }
+        }
+    }
+
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/CreateCohortResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.CommitmentsV2.Api.Types
+{
+    public sealed class CreateCohortResponse
+    {
+        public long CohortId { get; set; }
+
+        public string CohortReference { get; set; }
+
+        public long DraftApprenticeshipId { get; set; }
+    }
+}
