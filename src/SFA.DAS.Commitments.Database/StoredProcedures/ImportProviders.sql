46fe2e8d359f7b6705dd34b3f4388d2552e4a6f3
Paul Graham <paul.graham@coprime.co.uk>
2019-03-04 11:01:20 +0000

    Add Stored Proc and Providers tables to DB

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/ImportProviders.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[ImportProviders]
+    @providers [dbo].[Providers] READONLY,
+    @now DATETIME
+AS
+    UPDATE [dbo].[Providers]
+    SET [Name] = f.[Name], [Updated] = @now
+    FROM @providers f
+    INNER JOIN [dbo].[Providers] t ON t.[Ukprn] = f.[Ukprn]
+    WHERE t.[Name] <> f.[Name]
+    
+    INSERT INTO [dbo].[Providers] ([Ukprn], [Name], [Created])
+    SELECT f.[Ukprn], f.[Name], @now
+    FROM @providers f
+    LEFT JOIN [dbo].[Providers] t ON t.[Ukprn] = f.[Ukprn]
+    WHERE t.[Ukprn] IS NULL
---
+++ src/SFA.DAS.Commitments.Database/UserDefinedTypes/Providers.sql
@@ -0,0 +1,5 @@
+﻿CREATE TYPE [dbo].[Providers] AS TABLE
+(
+    [Ukprn] BIGINT NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(100) NOT NULL
+)
