b9f4a3b9bb4cdab12ee55030326b2580486230f6
Peter Williams <peter.david.williams.1991@gmail.com>
2019-03-12 13:08:29 +0000

    actually added shell controller

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/Controllers/WhenCreatingACohort.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Controllers;
+using SFA.DAS.CommitmentsV2.Api.Types;
+
+namespace SFA.DAS.CommitmentsV2.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class WhenCreatingACohort
+    {
+        [Test]
+        public void ThenACreateCohortResponseIsReturnedOnSuccessfulCreation()
+        {
+            //Arrange
+            var controller = new CohortController();
+            //Act
+            var actual = controller.Post(new CreateCohortRequest());
+            //Assert
+            Assert.IsInstanceOf(typeof(CreateCohortResponse), actual);
+        }
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/CohortController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Api.Types;
+
+namespace SFA.DAS.CommitmentsV2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CohortController : Controller
+    {
+        public CreateCohortResponse Post([FromBody]CreateCohortRequest request)
+        {
+            return new CreateCohortResponse();
+        }
+    }
+}
