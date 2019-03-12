7257a4860d2619e2731ad5379d6e319c3d832b3c
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-03-12 09:44:37 +0000

    Revert "[CON-164] Code cleanup"
    This reverts commit 014006a9070f629d28f1ca40c66fdb8b4f27a5a1.

--- src/SFA.DAS.Commitments.Support.SubSite/GlobalConstants/ApplicationConstants.cs
+++ src/SFA.DAS.Commitments.Support.SubSite/GlobalConstants/ApplicationConstants.cs
@@ -1,4 +1,9 @@
-﻿namespace SFA.DAS.Commitments.Support.SubSite.GlobalConstants
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SFA.DAS.Commitments.Support.SubSite.GlobalConstants
 {
     public class ApplicationConstants
     {
