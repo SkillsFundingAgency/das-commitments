ffe0368e0df320d08c63cb6471422b8135df179a
Peter Williams <peter.david.williams.1991@gmail.com>
2019-03-22 11:50:18 +0000

    added validation

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Validators/UlnValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
+using SFA.DAS.CommitmentsV2.Validators;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Validators
+{
+    [TestFixture]
+    public class UlnValidatorTests
+    {
+        [TestCase(null, false)]
+        [TestCase("", false)]
+        [TestCase("123456789", false)]
+        [TestCase("Ten digits", false)]
+        public void Validate_Uln(string value, bool expectedValid)
+        {
+            AssertValidationResult(request => request.ULN, value, expectedValid);
+        }
+
+        private void AssertValidationResult<T>(Expression<Func<CreateCohortRequest,T>> property,T value, bool expectedValid)
+        {
+            var validator = new UlnValidator();
+
+            if (expectedValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(property, value);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(property, value);
+            }
+        }
+    }
+}
--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Validators/CreateCohortRequestValidator.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Validators/CreateCohortRequestValidator.cs
@@ -8,6 +8,7 @@ namespace SFA.DAS.CommitmentsV2.Validators
         public CreateCohortRequestValidator()
         {
             RuleFor(model => model).SetValidator(new NameValidator());
+            RuleFor(model => model).SetValidator(new UlnValidator());
             RuleFor(model => model.UserId).NotEmpty().WithMessage("The user id must be supplied");
             RuleFor(model => model.AccountLegalEntityId).Must(accountLegalEntityId => accountLegalEntityId > 0).WithMessage("The Account Legal Entity must be valid"); 
             RuleFor(model => model.ProviderId).Must(providerId => providerId > 0).WithMessage("The provider id must be positive");
