ec8f85cd32b01c37c77d3ad2699cf17f475eee07
Ian Gledhill <ian@gledhills.mobi>
2019-03-28 12:47:02 +0000

    CV-15 Add reservation client

---
+++ src/SFA.DAS.Reservations.Api.Client/DependencyResolution/ReservationsApiClientRegistry.cs
@@ -0,0 +1,23 @@
+﻿using SFA.DAS.Http;
+using SFA.DAS.Reservations.Api.Client.Configuration;
+using StructureMap;
+
+namespace SFA.DAS.Reservations.Api.Client.DependencyResolution
+{
+    public class ReservationsApiClientRegistry : Registry
+    {
+        public ReservationsApiClientRegistry()
+        {
+            For<IReservationsApiClient>().Use(ctx => CreateClient(ctx)).Singleton();
+        }
+
+        private IReservationsApiClient CreateClient(IContext ctx)
+        {
+            var config = ctx.GetInstance<ReservationsClientApiConfiguration>();
+            var httpClientFactory = new AzureActiveDirectoryHttpClientFactory(config);
+            var httpClient = httpClientFactory.CreateHttpClient();
+            var restHttpClient = new RestHttpClient(httpClient);
+            return new ReservationsApiClient(restHttpClient);
+        }
+    }
+}
