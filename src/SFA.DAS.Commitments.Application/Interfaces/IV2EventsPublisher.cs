f0c5716d1bbb32086834d337e55a53f56b57a907
Ian Gledhill <ian@gledhills.mobi>
2019-04-03 11:15:52 +0100

    Add log message to show messages published

--- src/SFA.DAS.Commitments.Application/Interfaces/IV2EventsPublisher.cs
+++ src/SFA.DAS.Commitments.Application/Interfaces/IV2EventsPublisher.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NServiceBus;
+﻿using System.Threading.Tasks;
 using SFA.DAS.Commitments.Domain.Entities;
-using SFA.DAS.CommitmentsV2.Messages.Events;
 
 namespace SFA.DAS.Commitments.Application.Interfaces
 {
@@ -15,29 +9,5 @@ namespace SFA.DAS.Commitments.Application.Interfaces
     public interface IV2EventsPublisher
     {
         Task PublishApprenticeshipDeleted(Commitment commitment, Apprenticeship apprenticeship);
-    }
-
-    public class V2EventsPublisher : IV2EventsPublisher
-    {
-        private readonly IEndpointInstance _endpointInstance;
-       
-        public V2EventsPublisher(IEndpointInstance endpointInstance)
-        {
-            this._endpointInstance = endpointInstance;
-        }
-
-        public async Task PublishApprenticeshipDeleted(Commitment commitment, Apprenticeship apprenticeship)
-        {
-            await _endpointInstance.Publish<IApprenticeshipDeletedEvent>(ev =>
-            {
-                ev.CourseCode = apprenticeship.TrainingCode;
-                ev.Apprenticeship = apprenticeship.Id;
-                ev.CommitmentId = commitment.Id;
-                ev.CourseStartDate = apprenticeship.StartDate;
-                ev.DeletedOn = DateTime.UtcNow;
-                ev.ReservationId = apprenticeship.ReservationId;
-                ev.Uln = apprenticeship.ULN;
-            });
-        }
     }
 }
