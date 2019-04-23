5bbe35e24a8aa0da07578878fbd9347b629b365b
chrisfoster76 <chrisfoster186@googlemail.com>
2019-04-23 09:23:21 +0100

    IoC

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/DependencyResolution/DomainServiceRegistry.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/DependencyResolution/DomainServiceRegistry.cs
@@ -8,6 +8,8 @@ namespace SFA.DAS.CommitmentsV2.DependencyResolution
     {
         public DomainServiceRegistry()
         {
+            For<ICohortDomainService>().Use<CohortDomainService>();
+
             For<IUlnValidator>().Use<UlnValidator>();
             //todo: below line doesn't belong here. ideally, push this into a registry in the package itself, or an extension thereof
             For<Learners.Validators.IUlnValidator>().Use<Learners.Validators.UlnValidator>(); 
