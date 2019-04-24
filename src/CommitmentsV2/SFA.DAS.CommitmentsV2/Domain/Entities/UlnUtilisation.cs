2f0f0607da64620d44c4a3d6325f1b27fb16ee13
chrisfoster76 <chrisfoster186@googlemail.com>
2019-04-24 13:09:21 +0100

    Adds Uln Utilisation service Renames ConfirmedApprenticeship -> ApprovedApprenticeship

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Domain/Entities/UlnUtilisation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SFA.DAS.CommitmentsV2.Domain.Entities
+{
+    public class UlnUtilisation
+    {
+        public UlnUtilisation(long apprenticeshipId, string uln, DateTime startDate, DateTime endDate)
+        {
+            ApprenticeshipId = apprenticeshipId;
+            Uln = uln;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public long ApprenticeshipId { get; private set; }
+        public string Uln { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+    }
+}
