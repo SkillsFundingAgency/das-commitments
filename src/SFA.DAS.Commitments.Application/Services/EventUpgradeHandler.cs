574e9685785297c8faa47980cf2a274225251ba5
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-05-23 16:09:16 +0100

    [CON-479] add debugging log line

--- src/SFA.DAS.Commitments.Application/Services/EventUpgradeHandler.cs
+++ src/SFA.DAS.Commitments.Application/Services/EventUpgradeHandler.cs
@@ -1,5 +1,6 @@
 ﻿using NServiceBus;
 using SFA.DAS.Commitments.Application.Interfaces;
+using SFA.DAS.NLog.Logger;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Commitments.Application.Services
@@ -8,14 +9,17 @@ namespace SFA.DAS.Commitments.Application.Services
         IEventUpgradeHandler<Events.CohortApprovalRequestedByProvider>
     {
         private readonly IEndpointInstance _endpointInstance;
+        private readonly ILog _logger;
 
-        public EventUpgradeHandler(IEndpointInstance endpointInstance)
+        public EventUpgradeHandler(IEndpointInstance endpointInstance, ILog logger)
         {
             _endpointInstance = endpointInstance;
+            _logger = logger;
         }
 
         public Task Execute(Events.CohortApprovalRequestedByProvider @event)
         {
+            _logger.Debug($"Upgrading {nameof(Events.CohortApprovalRequestedByProvider)} to publish with NServiceBus");
             return _endpointInstance.Publish(new CommitmentsV2.Messages.Events.CohortApprovalRequestedByProvider()
             {
                 AccountId = @event.AccountId,
