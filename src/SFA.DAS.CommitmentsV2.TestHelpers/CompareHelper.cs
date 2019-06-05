cded2956305ab7e6e352774d7cf05daefafc368b
chrisfoster186 <chrisfoster186@googlemail.com>
2019-06-05 10:40:19 +0100

    Adds unit test for validation request Adds test helper project

---
+++ src/SFA.DAS.CommitmentsV2.TestHelpers/CompareHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KellermanSoftware.CompareNetObjects;
+
+namespace SFA.DAS.CommitmentsV2.TestHelpers
+{
+    public static class CompareHelper
+    {
+        public static bool AreEqualIgnoringTypes(object object1, object object2)
+        {
+            var compareLogic = new CompareLogic(new ComparisonConfig{ IgnoreObjectTypes = true });
+            var result = compareLogic.Compare(object1, object2);
+            return result.AreEqual;
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Api.Client.UnitTests/WhenCallingTheValidationEndpoint.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.TestHelpers;
+using SFA.DAS.Http;
+using SFA.DAS.Reservations.Api.Client.Types;
+
+namespace SFA.DAS.Reservations.Api.Client.UnitTests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class WhenCallingTheValidationEndpoint
+    {
+        private ReservationsClientTestFixture _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new ReservationsClientTestFixture();   
+        }
+
+        [Test]
+        public async Task ThenTheRequestUriIsCorrectlyFormed()
+        {
+            await _fixture.ValidateRequest();
+            _fixture.AssertUriCorrectlyFormed();
+        }
+
+        [Test]
+        public async Task ThenTheRequestPayloadIsCorrectlyFormed()
+        {
+            await _fixture.ValidateRequest();
+            _fixture.AssertPayloadCorrectlyFormed();
+        }
+
+        private class ReservationsClientTestFixture
+        {
+            private readonly ReservationsApiClient _reservationsApiClient;
+            private readonly Mock<IRestHttpClient> _restHttpClient;
+
+            private readonly ValidationReservationMessage _request;
+
+            public ReservationsClientTestFixture()
+            {
+                _restHttpClient = new Mock<IRestHttpClient>();
+                _restHttpClient.Setup(x => x.Get<ValidationResult>(It.IsAny<string>(),
+                        It.IsAny<object>(),
+                        It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(new ValidationResult());
+
+                _reservationsApiClient = new ReservationsApiClient(_restHttpClient.Object);
+
+                var autoFixture = new Fixture();
+                _request = new ValidationReservationMessage
+                {
+                    AccountId = autoFixture.Create<long>(),
+                    CourseCode = autoFixture.Create<string>(),
+                    ReservationId = autoFixture.Create<Guid>(),
+                    StartDate = autoFixture.Create<DateTime>()
+                };
+            }
+
+            public async Task<ReservationsClientTestFixture> ValidateRequest()
+            {
+                await _reservationsApiClient.ValidateReservation(_request, new CancellationToken());
+                return this;
+            }
+
+            public void AssertUriCorrectlyFormed()
+            {
+                var expectedUrl = $"api/accounts/{_request.AccountId}/reservations/{_request.ReservationId}";
+
+                _restHttpClient.Verify(x => x.Get<ValidationResult>(It.Is<string>(s => s == expectedUrl),
+                    It.IsAny<object>(),
+                    It.IsAny<CancellationToken>()));
+            }
+
+            public void AssertPayloadCorrectlyFormed()
+            {
+                var expectedPayload = new
+                {
+                    StartDate = _request.StartDate.ToString("yyyy-MM-dd"),
+                    _request.CourseCode
+                };
+
+                _restHttpClient.Verify(x => x.Get<ValidationResult>(It.IsAny<string>(),
+                    It.Is<object>(o => CompareHelper.AreEqualIgnoringTypes(expectedPayload, o)),
+                    It.IsAny<CancellationToken>()));
+            }
+        }
+    }
+}
