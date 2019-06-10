d40c13dcf3dfbd0cd12e2f2f3f848d1220c5b3f4
Kevin <kevin@stephek.co.uk>
2019-06-10 15:40:39 +0100

    CON-536-publish-CohortApprovedByEmployer-to-nServiceBus

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/CohortApprovedByEmployer.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.CommitmentsV2.Messages.Events
+{
+    public class CohortApprovedByEmployer
+    {
+        public long AccountId { get; set; }
+        public long ProviderId { get; set; }
+        public long CommitmentId { get; set; }
+    }
+}
