95c9f374b7211f8f395bb3a5fe0c39d964d946bf
Peter Williams <peter.david.williams.1991@gmail.com>
2019-06-26 15:35:49 +0100

    added shells

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetProvider/GetProviderHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.CommitmentsV2.Application.Queries.GetProvider
+{
+    class GetProviderHandler
+    {
+    }
+}
