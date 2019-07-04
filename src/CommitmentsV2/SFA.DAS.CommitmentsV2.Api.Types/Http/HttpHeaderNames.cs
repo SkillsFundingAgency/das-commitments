d4e34848eea9c5e85099106ec99e4df9a6394c32
Elliot Ritchie <elliotritchie@hotmail.com>
2019-07-04 09:48:18 +0100

    Renamed Sub-Status-Code header to X-Sfa-Sub-Status-Code (X to show its custom and Sfa to show its SFA specific)

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Http/HttpHeaderNames.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Http/HttpHeaderNames.cs
@@ -2,6 +2,6 @@ namespace SFA.DAS.CommitmentsV2.Api.Types.Http
 {
     public static class HttpHeaderNames
     {
-        public const string SubStatusCode = "Sub-Status-Code";
+        public const string SubStatusCode = "X-Sfa-Sub-Status-Code";
     }
 }
