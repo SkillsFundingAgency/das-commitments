ec68a8105ea8cd82dd863460dd1ea1b5ee18c9bb
Ian Gledhill <ian@gledhills.mobi>
2019-07-30 08:18:49 +0100

    Remove reservation validation from API and add IoC registration to reservation client

---
+++ src/SFA.DAS.Reservations.Api.Client/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Reservations.Api.Client.Configuration
+{
+    public static class ConfigurationKeys
+    {
+        public const string ReservationsClientApiConfiguration = "SFA.DAS.ReservationClient";
+    }
+}
