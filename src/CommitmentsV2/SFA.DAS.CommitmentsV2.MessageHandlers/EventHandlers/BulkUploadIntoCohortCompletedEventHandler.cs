37a33a76c6934e3d6f74cae7068afc39f03b605a
Paul Graham <paul.graham@coprime.co.uk>
2019-08-28 08:44:25 +0100

    added tests for messages and upgraded a sfa.domain package so it was the same version accross v1 projects

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/BulkUploadIntoCohortCompletedEventHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/BulkUploadIntoCohortCompletedEventHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetDraftApprenticeshipCreatedEventsForCohort;
