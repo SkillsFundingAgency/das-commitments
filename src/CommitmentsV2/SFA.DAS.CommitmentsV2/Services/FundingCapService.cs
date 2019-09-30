9072c8bf201c82e06b01c4952118275e66c66dad
Paul Graham <paul.graham@coprime.co.uk>
2019-09-30 15:03:39 +0100

    removed test code

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Services/FundingCapService.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Services/FundingCapService.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SFA.DAS.Apprenticeships.Api.Client;
-using SFA.DAS.Apprenticeships.Api.Types;
 using SFA.DAS.CommitmentsV2.Domain.Entities;
 using SFA.DAS.CommitmentsV2.Domain.Interfaces;
 using SFA.DAS.CommitmentsV2.Models;
-//using TrainingProgrammeStatus = SFA.DAS.CommitmentsV2.Domain.Entities.TrainingProgrammeStatus;
 
 namespace SFA.DAS.CommitmentsV2.Services
 {
@@ -53,46 +51,4 @@ namespace SFA.DAS.CommitmentsV2.Services
             return courseSummary.ToList();
         }
     }
-
-    public static class ITrainingProgrammeExtensions2
-  {
-    public static bool IsActiveOn(this ITrainingProgramme course, DateTime date)
-    {
-      return ITrainingProgrammeExtensions2.GetStatusOn(course.EffectiveFrom, course.EffectiveTo, date) == SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus.Active;
-    }
-
-    public static SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus GetStatusOn(
-      this ITrainingProgramme course,
-      DateTime date)
-    {
-      return ITrainingProgrammeExtensions2.GetStatusOn(course.EffectiveFrom, course.EffectiveTo, date);
-    }
-
-    public static int FundingCapOn(this ITrainingProgramme course, DateTime date)
-    {
-      if (!course.IsActiveOn(date))
-        return 0;
-      IFundingPeriod fundingPeriod = course.FundingPeriods.FirstOrDefault<IFundingPeriod>((Func<IFundingPeriod, bool>) (x => ITrainingProgrammeExtensions2.GetStatusOn(x.EffectiveFrom, x.EffectiveTo, date) == SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus.Active));
-      if (fundingPeriod == null)
-        return 0;
-      return fundingPeriod.FundingCap;
-    }
-
-    private static SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus GetStatusOn(
-      DateTime? effectiveFrom,
-      DateTime? effectiveTo,
-      DateTime date)
-    {
-      DateTime date1 = date.Date;
-      if (effectiveFrom.HasValue && effectiveFrom.Value.FirstOfMonth() > date1)
-        return SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus.Pending;
-      return !effectiveTo.HasValue || effectiveTo.Value >= date1 ? SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus.Active : SFA.DAS.Apprenticeships.Api.Types.TrainingProgrammeStatus.Expired;
-    }
-
-    private static DateTime FirstOfMonth(this DateTime value)
-    {
-      return new DateTime(value.Year, value.Month, 1);
-    }
-  }
-
 }
