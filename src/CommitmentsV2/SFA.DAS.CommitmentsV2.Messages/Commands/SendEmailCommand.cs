5ca1c5e4578c79128f5d6a6ff6f5b35e919bf8d5
chrisfoster186 <chrisfoster186@googlemail.com>
2019-10-09 09:44:03 +0100

    Adds SendEmail low-level command

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Commands/SendEmailCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.CommitmentsV2.Messages.Commands
+{
+    public class SendEmailCommand
+    {
+        public string TemplateId { get; }
+        public string RecipientsAddress { get; }
+        public string ReplyToAddress { get; set; }
+        public Dictionary<string, string> Tokens { get; set; }
+
+        public SendEmailCommand(string templateId, string recipientsAddress, string replyToAddress, Dictionary<string, string> tokens)
+        {
+            TemplateId = templateId;
+            RecipientsAddress = recipientsAddress;
+            ReplyToAddress = replyToAddress;
+            Tokens = tokens;
+        }
+    }
+}
