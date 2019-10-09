add8433e5d75b445af86da4915cb96aecfb56a77
chrisfoster186 <chrisfoster186@googlemail.com>
2019-10-09 09:47:54 +0100

    Unimplemented implementation of handler

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/CommandHandlers/SendEmailCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.CommitmentsV2.Messages.Commands;
+
+namespace SFA.DAS.CommitmentsV2.MessageHandlers.CommandHandlers
+{
+    public class SendEmailCommandHandler : IHandleMessages<SendEmailCommand>
+    {
+        public Task Handle(SendEmailCommand message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
