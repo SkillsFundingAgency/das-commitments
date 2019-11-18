90425a164ebc68676349371aed1c650b6030f256
chrisfoster186 <chrisfoster186@googlemail.com>
2019-11-18 13:13:57 +0000

    Adding a query and handler

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/AccountsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Api.Extensions;
+using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetAccountSummary;
+
+namespace SFA.DAS.CommitmentsV2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _logger;
+        private readonly IMediator _mediator;
+
+        public AccountsController(
+            ILogger<AccountsController> logger,
+            IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("{AccountId}")]
+        public async Task<IActionResult> GetAccount(long accountId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.CreateErrorResponse());
+            }
+
+            var employer = await _mediator.Send(new GetAccountSummaryRequest
+            {
+                AccountId = accountId
+            });
+
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new AccountResponse
+            {
+                AccountId = employer.AccountId,
+                HasApprenticeships = employer.HasApprenticeships,
+                HasCohorts = employer.HasCohorts
+            });
+        }
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetAccountSummary/GetAccountSummaryHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Types;
+
+namespace SFA.DAS.CommitmentsV2.Application.Queries.GetAccountSummary
+{
+    public class GetAccountSummaryHandler : IRequestHandler<GetAccountSummaryRequest, GetAccountSummaryResponse>
+    {
+        private readonly Lazy<ProviderCommitmentsDbContext> _dbContext;
+
+        public GetAccountSummaryHandler(Lazy<ProviderCommitmentsDbContext> dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<GetAccountSummaryResponse> Handle(GetAccountSummaryRequest request, CancellationToken cancellationToken)
+        {
+            //todo: what if account doesn't exist? do we pay the cost of account.GetById in order to do a 404 if not?
+
+            var result = _dbContext.Value
+                .Cohorts
+                .Where(c => c.EmployerAccountId == request.AccountId)
+                .Select(c =>
+                    new
+                    {
+                        IsApproved = c.EditStatus == EditStatus.Both &&
+                                     (!c.TransferSenderId.HasValue ||
+                                      c.TransferApprovalStatus == TransferApprovalStatus.Approved)
+                    });
+
+            return Task.FromResult(new GetAccountSummaryResponse
+            {
+                AccountId = request.AccountId,
+                HasCohorts = result.Any(x => !x.IsApproved),
+                HasApprenticeships = result.Any(x => x.IsApproved)
+            });
+
+        }
+    }
+}
