b231719acfa52b95d4d083b20372b59f6274f26c
Paul Graham <paul.graham@coprime.co.uk>
2019-11-25 11:30:40 +0000

    fix build error by making SQL adhoc scripts as None build

--- src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CV-449_AddMaLegalEntityIdColumn.sql
+++ src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CV-449_AddMaLegalEntityIdColumn.sql
@@ -2,10 +2,6 @@
 Script to add new column MaLegalEntityId to AccountLegalEntities table
 */
 
-/* Manual Fix
+
 ALTER TABLE AccountLegalEntities 
 ADD MaLegalEntityId bigint
-
-*/
-
-
--- src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CV-504-Create-Missing-PriceHistory.sql
+++ src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CV-504-Create-Missing-PriceHistory.sql
@@ -3,13 +3,9 @@ Adhoc script to add PriceHistory records for 10 approved Apprenticeship records 
 as being in an invalid state.
 */
 
-/*
-Manual Script 
-
 insert into PriceHistory
 (ApprenticeshipId, Cost, FromDate, ToDate)
 select
 a.Id, a.Cost, a.StartDate, null
 from Apprenticeship a
 where a.Id in (132462,132360,665328,627949,665325,666932,95072,132361,665332,494495)
-*/
