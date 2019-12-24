f37fa143fc29617ce26457af3aa6518c639c3da9
Paul Graham <paul.graham@coprime.co.uk>
2019-12-24 10:11:50 +0000

    removed ref to reservations API client

--- src/SFA.DAS.Commitments.Api/Controllers/HealthCheckController.cs
+++ src/SFA.DAS.Commitments.Api/Controllers/HealthCheckController.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Web.Http;
 using SFA.DAS.Reservations.Api.Types;
 
 namespace SFA.DAS.Commitments.Api.Controllers
 {
     public class HealthCheckController : ApiController
     {
-        private readonly IReservationsApiClient _reservationClient;
-
         public HealthCheckController(IReservationsApiClient reservationClient)
         {
-            _reservationClient = reservationClient;
         }
 
 
