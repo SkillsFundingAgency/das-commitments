0619df788c5ed8beb6018a11f48bb47bc529f7fb
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-12-27 11:40:49 +0000

    Move apprenticeshipDetails response to v2ApiTypes

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Client/CommitmentsApiClient.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Client/CommitmentsApiClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.CommitmentsV2.Api.Types.Responses;
 using SFA.DAS.Http;
