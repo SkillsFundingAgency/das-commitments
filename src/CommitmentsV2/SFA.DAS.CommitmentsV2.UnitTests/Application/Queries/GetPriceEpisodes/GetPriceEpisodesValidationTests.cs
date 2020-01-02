fbbd7628bcab203f661356dde50c2ab376c48830
chrisfoster186 <chrisfoster186@googlemail.com>
2020-01-02 09:26:04 +0000

    Adds query classes WIP

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetPriceEpisodes/GetPriceEpisodesValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetPriceEpisodes;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetPriceEpisodes
+{
+    [TestFixture]
+    public class GetPriceEpisodesValidationTests
+    {
+            [TestCase(-1, false)]
+            [TestCase(0, false)]
+            [TestCase(1, true)]
+            public void Validate_WithSpecifiedCohortId_ShouldSetIsValidCorrectly(int apprenticeshipId, bool expectedIsValid)
+            {
+                // arrange
+                var validator = new GetPriceEpisodesQueryValidator();
+                var validationResults = validator.Validate(new GetPriceEpisodesQuery(apprenticeshipId));
+
+                // act
+                var actualIsValid = validationResults.IsValid;
+
+                // Assert
+                Assert.AreEqual(expectedIsValid, actualIsValid);
+            }
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetPriceEpisodes/GetPriceEpisodesQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace SFA.DAS.CommitmentsV2.Application.Queries.GetPriceEpisodes
+{
+    public class GetPriceEpisodesQuery: IRequest<GetPriceEpisodesQueryResult>
+    {
+        public long ApprenticeshipId { get; private set; }
+
+        public GetPriceEpisodesQuery(long apprenticeshipId)
+        {
+            ApprenticeshipId = apprenticeshipId;
+        }
+    }
+}
