039848e006aa8fe589abd985cfd2cef7270836bf
Paul Graham <paul.graham@coprime.co.uk>
2020-01-02 16:00:48 +0000

    added access-apprenticeship endpoint

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Requests/ApprenticeshipAccessRequest.cs
@@ -0,0 +1,11 @@
+﻿using SFA.DAS.CommitmentsV2.Types;
+
+namespace SFA.DAS.CommitmentsV2.Api.Types.Requests
+{
+    public class ApprenticeshipAccessRequest
+    {
+        public Party Party { get; set; }
+        public long PartyId { get; set; }
+        public long ApprenticeshipId { get; set; }
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprenticeshipUpdateController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Api.Extensions;
+using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetAccountSummary;
+
+namespace SFA.DAS.CommitmentsV2.Api.Controllers
+{
+    [Route("api/apprenticeships/{ApprenticeshipId}")]
+    [ApiController]
+    [Authorize]
+    public class ApprenticeshipUpdateController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ApprenticeshipUpdateController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/pending-update")]
+        public async Task<IActionResult> GetPendingUpdate(long accountId)
+        {
+            var employer = await _mediator.Send(new GetAccountSummaryRequest
+            {
+                AccountId = accountId
+            });
+
+            return Ok(new AccountResponse
+            {
+                AccountId = employer.AccountId,
+                HasApprenticeships = employer.HasApprenticeships,
+                HasCohorts = employer.HasCohorts
+            });
+        }
+    }
+}
