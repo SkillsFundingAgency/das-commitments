25fd8f6596f167a313752046e43b9238ec998704
chrisfoster186 <chrisfoster186@googlemail.com>
2020-01-02 10:51:28 +0000

    QueryResult -> Response mapper

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/PriceEpisodesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetPriceEpisodes;
+
+namespace SFA.DAS.CommitmentsV2.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/apprenticeships/{apprenticeshipId}/price-episodes")]
+    public class PriceEpisodesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PriceEpisodesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task Get(long apprenticeshipId)
+        {
+            var query = new GetPriceEpisodesQuery(apprenticeshipId);
+            var result = await _mediator.Send(query);
+
+
+
+        }
+    }
+}
