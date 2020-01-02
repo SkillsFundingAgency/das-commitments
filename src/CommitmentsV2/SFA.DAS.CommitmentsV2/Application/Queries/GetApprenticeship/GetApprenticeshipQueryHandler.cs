046aa4a5de2f13c7b23746ac597864b713b5142f
chrisfoster186 <chrisfoster186@googlemail.com>
2020-01-02 14:46:12 +0000

    WIP query and handler

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetApprenticeship/GetApprenticeshipQueryHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.CommitmentsV2.Authentication;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Data.QueryExtensions;
+using SFA.DAS.CommitmentsV2.Types;
+
+namespace SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeship
+{
+    public class GetApprenticeshipQueryHandler: IRequestHandler<GetApprenticeshipQuery, GetApprenticeshipQueryResult>
+    {
+        private readonly Lazy<ProviderCommitmentsDbContext> _dbContext;
+        private readonly IAuthenticationService _authenticationService;
+
+        public GetApprenticeshipQueryHandler(Lazy<ProviderCommitmentsDbContext> dbContext, IAuthenticationService authenticationService)
+        {
+            _dbContext = dbContext;
+            _authenticationService = authenticationService;
+        }
+
+
+        public async Task<GetApprenticeshipQueryResult> Handle(GetApprenticeshipQuery request, CancellationToken cancellationToken)
+        {
+            var requestingParty = _authenticationService.GetUserParty();
+
+            var x = await _dbContext.Value
+                .ApprovedApprenticeships.GetById(request.ApprenticeshipId, apprenticeship => 
+                    new GetApprenticeshipQueryResult
+                    {
+                        Id = apprenticeship.Id,
+                        CourseCode = apprenticeship.CourseCode
+                        //StartDate = apprenticeship.StartDate.Value,
+                        //DateOfBirth = apprenticeship.DateOfBirth.Value,
+                        //EndDate = apprenticeship.EndDate,
+                        //FirstName = apprenticeship.FirstName,
+                        //LastName = apprenticeship.LastName,
+                        //Reference = requestingParty == Party.Provider ? apprenticeship.ProviderRef : apprenticeship.EmployerRef,
+                        //Uln = apprenticeship.Uln
+                    },
+                    cancellationToken);
+
+            return x;
+        }
+    }
+}
