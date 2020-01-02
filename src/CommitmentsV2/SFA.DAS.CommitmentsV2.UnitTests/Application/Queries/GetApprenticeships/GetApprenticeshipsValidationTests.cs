365c4cbfee597df902f4911c37f0c041aa23ec0a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-01-02 09:20:44 +0000

    Add validation for the sort field

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetApprenticeships/GetApprenticeshipsValidationTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetApprenticeships/GetApprenticeshipsValidationTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeships;
+using SFA.DAS.CommitmentsV2.Models;
 
 namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetApprenticeships
 {
@@ -19,6 +20,28 @@ namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetApprenticeships
 
             // Assert
             Assert.AreEqual(expectedIsValid, actualIsValid);
+        }
+
+        [TestCase("test",false)]
+        [TestCase(null,true)]
+        [TestCase("",true)]
+        [TestCase(nameof(Apprenticeship.FirstName),true)]
+        //[TestCase(nameof(Apprenticeship.Cohort.LegalEntityName),true)]
+        public void Validate_WithSpecifiedSortField_ShouldOnlyBeAllowedIfPropertyOnApprenticeship(string fieldName, bool expected)
+        {
+            // arrange
+            var validator = new GetApprenticeshipsValidator();
+            var validationResults = validator.Validate(new GetApprenticeshipsRequest
+            {
+                ProviderId = 1, 
+                SortField = fieldName
+            });
+
+            // act
+            var actual = validationResults.IsValid;
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
