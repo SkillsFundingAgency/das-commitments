ac0f77c954a1aa5468f07f93c8f4f83f7dfe4c0a
Shahzad <shahzad.khan@digital.education.gov.uk>
2020-01-07 11:59:38 +0000

    CV-555 Removed unused references

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/Controllers/CohortControllerTests/CreateEmptyCohortTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/Controllers/CohortControllerTests/CreateEmptyCohortTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture;
-using FluentAssertions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
