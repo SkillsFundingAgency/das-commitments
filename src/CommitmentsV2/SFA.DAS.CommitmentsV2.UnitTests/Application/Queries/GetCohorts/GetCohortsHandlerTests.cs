a3d18764e7f4f6bb2e69864f673216dcca7f8035
Paul Graham <paul.graham@coprime.co.uk>
2020-01-10 09:26:22 +0000

    fix test so it uses a random DB name, so it doesn;t clash

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetCohorts/GetCohortsHandlerTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetCohorts/GetCohortsHandlerTests.cs
@@ -242,7 +242,7 @@ namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetCohorts
         public Task<T> RunWithDbContext<T>(Func<ProviderCommitmentsDbContext, Task<T>> action)
         {
             var options = new DbContextOptionsBuilder<ProviderCommitmentsDbContext>()
-                .UseInMemoryDatabase("SFA.DAS.Commitments.Database")
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
 
             using (var dbContext = new ProviderCommitmentsDbContext(options))
