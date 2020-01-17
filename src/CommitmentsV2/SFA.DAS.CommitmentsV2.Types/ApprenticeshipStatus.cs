279c7486fda2339135982e13e5e71e6737872d28
chrisfoster186 <chrisfoster186@googlemail.com>
2020-01-17 10:52:34 +0000

    Adds Description attr to type

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/ApprenticeshipStatus.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/ApprenticeshipStatus.cs
@@ -1,11 +1,18 @@
-﻿namespace SFA.DAS.CommitmentsV2.Types
+﻿using System.ComponentModel;
+
+namespace SFA.DAS.CommitmentsV2.Types
 {
     public enum ApprenticeshipStatus : short
     {
+        [Description("Waiting to start")]
         WaitingToStart = 0,
+        [Description("Live")]
         Live = 1,
+        [Description("Paused")]
         Paused = 2,
+        [Description("Stopped")]
         Stopped = 3,
+        [Description("Completed")]
         Completed = 4
     }
 }
