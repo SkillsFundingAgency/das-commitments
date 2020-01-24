60a978717be7a60d951e86bf7fc94e8cdf8265c8
chrisfoster186 <chrisfoster186@googlemail.com>
2020-01-24 10:31:05 +0000

    Adds "EmployerTransferPendingFinalApproval" email send

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/CohortTransferApprovalRequestedEventHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/CohortTransferApprovalRequestedEventHandler.cs
@@ -1,23 +1,54 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using MediatR;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Application.Commands.AddTransferRequest;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetCohortSummary;
+using SFA.DAS.CommitmentsV2.Messages.Commands;
 using SFA.DAS.CommitmentsV2.Messages.Events;
+using SFA.DAS.CommitmentsV2.Types;
+using SFA.DAS.Encoding;
+using SFA.DAS.NServiceBus.Services;
 
 namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
 {
     public class CohortTransferApprovalRequestedEventHandler : IHandleMessages<CohortTransferApprovalRequestedEvent>
     {
         private readonly IMediator _mediator;
+        private readonly IEventPublisher _eventPublisher;
+        private readonly IEncodingService _encodingService;
 
-        public CohortTransferApprovalRequestedEventHandler(IMediator mediator)
+        public CohortTransferApprovalRequestedEventHandler(IMediator mediator, IEventPublisher eventPublisher, IEncodingService encodingService)
         {
             _mediator = mediator;
+            _eventPublisher = eventPublisher;
+            _encodingService = encodingService;
         }
 
-        public Task Handle(CohortTransferApprovalRequestedEvent message, IMessageHandlerContext context)
+        public async Task Handle(CohortTransferApprovalRequestedEvent message, IMessageHandlerContext context)
         {
-            return _mediator.Send(new AddTransferRequestCommand { CohortId = message.CohortId, LastApprovedByParty = message.LastApprovedByParty });
+            await _mediator.Send(new AddTransferRequestCommand { CohortId = message.CohortId, LastApprovedByParty = message.LastApprovedByParty });
+
+            var cohortSummary = await _mediator.Send(new GetCohortSummaryQuery(message.CohortId));
+
+            if (message.LastApprovedByParty == Party.Employer)
+            {
+                //send "TransferPendingFinalApproval" to the Provider to go here
+            }
+            else if (message.LastApprovedByParty == Party.Provider)
+            {
+                var tokens = new Dictionary<string, string>
+                {
+                    {"provider_name", cohortSummary.ProviderName },
+                    {"sender_name", cohortSummary.TransferSenderName },
+                    {"employer_hashed_account", _encodingService.Encode(cohortSummary.AccountId, EncodingType.AccountId) },
+                    {"cohort_reference", _encodingService.Encode(cohortSummary.CohortId, EncodingType.CohortReference)}
+                };
+
+                await _eventPublisher.Publish(new SendEmailToEmployerCommand(cohortSummary.AccountId,
+                    "EmployerTransferPendingFinalApproval", tokens,
+                    cohortSummary.LastUpdatedByEmployerEmail));
+            }
         }
     }
 }
