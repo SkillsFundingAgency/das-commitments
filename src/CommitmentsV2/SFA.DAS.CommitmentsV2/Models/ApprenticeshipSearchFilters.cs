99ae9d35b4c662480f186294e4df6d1bf62e223c
Scott Wakefield <scott@expertcc.co.uk>
2020-01-28 16:16:21 +0000

    MF-409 mapping search term to handler.

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprenticeshipSearchFilters.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprenticeshipSearchFilters.cs
@@ -4,17 +4,11 @@ namespace SFA.DAS.CommitmentsV2.Models
 {
     public class ApprenticeshipSearchFilters
     {
+        public string SearchTerm { get; set; }
         public string EmployerName { get; set; }
         public string CourseName { get; set; }
         public string Status { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-
-        public bool HasFilters =>
-            !string.IsNullOrEmpty(EmployerName) ||
-            !string.IsNullOrEmpty(CourseName) ||
-            !string.IsNullOrEmpty(Status) ||
-            StartDate.HasValue ||
-            EndDate.HasValue;
     }
 }
