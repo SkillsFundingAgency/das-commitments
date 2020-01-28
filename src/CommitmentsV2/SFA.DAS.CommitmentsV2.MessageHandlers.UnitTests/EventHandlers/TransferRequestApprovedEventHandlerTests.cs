3e90fb39751a0d82c28bfebdc979b763ea6e9bfd
Paul Graham <paul.graham@coprime.co.uk>
2020-01-28 08:47:51 +0000

    chnaged style of tests

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/TransferRequestApprovedEventHandlerTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/TransferRequestApprovedEventHandlerTests.cs
@@ -1,48 +1,42 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Microsoft.Extensions.Logging;
 using Moq;
-using NServiceBus;
 using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Application.Commands.ApproveCohort;
 using SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers;
 using SFA.DAS.CommitmentsV2.Messages.Events;
 using SFA.DAS.CommitmentsV2.Types;
-using SFA.DAS.Testing;
 
 namespace SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests.EventHandlers
 {
     [TestFixture]
-    [Parallelizable(ParallelScope.None)]
-    public class TransferRequestApprovedEventHandlerTests : FluentTest<TransferRequestApprovedEventHandlerTestsFixture>
+    [Parallelizable]
+    public class TransferRequestApprovedEventHandlerTests
     {
         [Test]
-        public Task Handle_WhenHandlingTransferRequestApprovedEvent_ThenShouldCallMediatorWithCorrectCommand()
+        public async Task Handle_WhenHandlingTransferRequestApprovedEvent_ThenShouldCallMediatorWithCorrectCommand()
         {
-            return TestAsync(f => f.Handle(), f => f.VerifySend<ApproveCohortCommand>((c, m) =>
+            var f = new TransferRequestApprovedEventHandlerTestsFixture();
+            await f.Handle();
+
+            f.VerifySend<ApproveCohortCommand>((c, m) =>
                 c.CohortId == m.CohortId && c.Message == null &&
-                c.Party == Party.TransferSender && c.UserInfo == m.UserInfo));
+                c.Party == Party.TransferSender && c.UserInfo == m.UserInfo);
         }
 
         [Test]
-        public Task Handle_WhenHandlingTransferRequestApprovedEventAndMediatorThrowsException_ThenWelogErrorAndRethrowError()
+        public void Handle_WhenHandlingTransferRequestApprovedEventAndMediatorThrowsException_ThenWelogErrorAndRethrowError()
         {
-            return TestExceptionAsync(f => f.SetupMediatorExecuteException(), 
-                f => f.Handle(), 
-                f => f.VerifySend<ApproveCohortCommand>((c, m) =>
-                c.CohortId == m.CohortId && c.Message == null &&
-                c.Party == Party.TransferSender && c.UserInfo == m.UserInfo));
+            var f = new TransferRequestApprovedEventHandlerTestsFixture().SetupMediatorExecuteException();
+            Assert.ThrowsAsync<Exception>(() => f.Handle());
+            Assert.IsTrue(f.Logger.HasErrors);
         }
-
-
-
     }
 
     public class TransferRequestApprovedEventHandlerTestsFixture : EventHandlerTestsFixture<TransferRequestApprovedEvent, TransferRequestApprovedEventHandler>
     {
-        public static ILogger<TransferRequestCreatedEvent> Logger { get; set; }
+        public FakeLogger<TransferRequestCreatedEvent> Logger { get; set; }
 
         public TransferRequestApprovedEventHandlerTestsFixture() : base((m) => null)
         {
