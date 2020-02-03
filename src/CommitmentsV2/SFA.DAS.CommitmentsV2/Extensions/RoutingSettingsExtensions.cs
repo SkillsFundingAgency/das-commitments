5bb20951b103010519a6e9bc2a46e10af8ba838e
chrisfoster186 <chrisfoster186@googlemail.com>
2020-02-03 09:04:34 +0000

    Adds SendProviderEmailCommand, handler, and tests

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/RoutingSettingsExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/RoutingSettingsExtensions.cs
@@ -13,6 +13,7 @@ namespace SFA.DAS.CommitmentsV2.Extensions
         {
             routingSettings.RouteToEndpoint(typeof(RunHealthCheckCommand), CommitmentsV2MessageHandler);
             routingSettings.RouteToEndpoint(typeof(SendEmailToEmployerCommand), CommitmentsV2MessageHandler);
+            routingSettings.RouteToEndpoint(typeof(SendEmailToProviderCommand), CommitmentsV2MessageHandler);
             routingSettings.RouteToEndpoint(typeof(ApproveTransferRequestCommand), CommitmentsV2MessageHandler);
             routingSettings.RouteToEndpoint(typeof(SendEmailCommand), NotificationsMessageHandler);
         }
