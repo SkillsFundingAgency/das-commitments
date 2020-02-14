42d43859e71ffc335f6c65cc2fdeb3a7086c889b
Scott Wakefield <scott@expertcc.co.uk>
2020-02-14 10:34:15 +0000

    MF-431 adding csv service to shared.

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Shared/Interfaces/ICreateCsvService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.CommitmentsV2.Shared.Interfaces
+{
+    public interface ICreateCsvService
+    {
+        byte[] GenerateCsvContent<T>(IEnumerable<T> results);
+    }
+}
