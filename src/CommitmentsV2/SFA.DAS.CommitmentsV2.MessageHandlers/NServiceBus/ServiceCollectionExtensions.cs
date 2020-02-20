ef014283963987a03813657e8db1e937c03eba8b
chrisfoster186 <chrisfoster186@googlemail.com>
2020-02-20 09:58:30 +0000

    NSB Package bump Specifies error queues for all endpoints

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/NServiceBus/ServiceCollectionExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/NServiceBus/ServiceCollectionExtensions.cs
@@ -18,6 +18,8 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.NServiceBus
 {
     public static class ServiceCollectionExtensions
     {
+        private const string EndpointName = "SFA.DAS.CommitmentsV2.MessageHandlers";
+
         public static IServiceCollection AddNServiceBus(this IServiceCollection services)
         {
             return services
@@ -28,7 +30,8 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.NServiceBus
                     var configuration = p.GetService<CommitmentsV2Configuration>().NServiceBusConfiguration;
                     var isDevelopment = hostingEnvironment.IsDevelopment();
 
-                    var endpointConfiguration = new EndpointConfiguration("SFA.DAS.CommitmentsV2.MessageHandlers")
+                    var endpointConfiguration = new EndpointConfiguration(EndpointName)
+                        .UseErrorQueue($"{EndpointName}-errors")
                         .UseInstallers()
                         .UseLicense(configuration.NServiceBusLicense)
                         .UseMessageConventions()
