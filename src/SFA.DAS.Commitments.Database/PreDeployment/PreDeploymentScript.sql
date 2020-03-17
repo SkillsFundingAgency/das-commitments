68ff8e05614946f66c988dad204feabe44cb8c5a
chrisfoster186 <chrisfoster186@googlemail.com>
2020-03-17 13:18:53 +0000

    Adds script to mop up old left over procs

--- src/SFA.DAS.Commitments.Database/PreDeployment/PreDeploymentScript.sql
+++ src/SFA.DAS.Commitments.Database/PreDeployment/PreDeploymentScript.sql
@@ -18,3 +18,22 @@ from
 Commitment c
 join AccountLegalEntities ale on ale.PublicHashedId = c.AccountLegalEntityPublicHashedId
 where c.AccountLegalEntityId IS NULL
+
+
+--Remove leftover procs
+DROP PROCEDURE IF EXISTS [dbo].[SetTransferRequestApproval]
+DROP PROCEDURE IF EXISTS [dbo].[GetApprovedApprenticeship]
+DROP PROCEDURE IF EXISTS [dbo].[SetTransferApproval]
+DROP PROCEDURE IF EXISTS [dbo].[StartATransferRequest]
+DROP PROCEDURE IF EXISTS [dbo].[SetPaymentOrder]
+DROP PROCEDURE IF EXISTS [dbo].[GetActiveApprenticeshipsForProvider]
+DROP PROCEDURE IF EXISTS [dbo].[GetActiveApprenticeshipsForEmployer]
+
+
+
+
+
+
+
+
+
