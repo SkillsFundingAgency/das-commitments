13ae389e9b4be6254e97ebeda54dd9518520d58e
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-24 13:11:08 +0000

    CV-500 ApprenticeshipCompleted Event

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/ApprenticeshipCompletedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.CommitmentsV2.Messages.Events
+{
+    public class ApprenticeshipCompletedEvent
+    {
+        public long  ApprenticeshipId { get; set; }
+        public DateTime CompletionDate { get; set; }
+    }
+}
