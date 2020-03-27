c966c7096335ee4d245e2f983792af1f98b61296
Akshay Chugh <akshay.chugh30@outlook.com>
2020-03-27 09:25:23 +0000

    CV-500 Apprenticeship Complete method tests

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Models/Apprenticeship/WhenCompletingApprenticeship.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Linq;
+using AutoFixture;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Messages.Events;
+using SFA.DAS.CommitmentsV2.Types;
+using SFA.DAS.UnitOfWork.Context;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Models.Apprenticeship
+{
+    [Parallelizable]
+    [TestFixture]
+    public class WhenCompletingApprenticeship
+    {
+        private WhenCompletingApprenticeshipFixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new WhenCompletingApprenticeshipFixture();
+        }
+
+        [TestCase(ApprenticeshipStatus.Live, false)]
+        [TestCase(ApprenticeshipStatus.Completed, true)]
+        [TestCase(ApprenticeshipStatus.Paused, true)]
+        [TestCase(ApprenticeshipStatus.Stopped, true)]
+        [TestCase(ApprenticeshipStatus.WaitingToStart, true)]
+        [TestCase(ApprenticeshipStatus.Unknown, true)]
+        public void ThenThrowsExceptionBasedOnApprenticeshipStatus(ApprenticeshipStatus status, bool isExceptionExpected)
+        {
+            _fixture.WithApprenticeshipStatus(status);
+
+            if (isExceptionExpected)
+            {
+                Assert.Throws<InvalidOperationException>(() => _fixture.Act());
+            }
+            else
+            {
+                _fixture.Act();
+            }
+        }
+
+        [Test]
+        public void ThenVerifyChangeTracking()
+        {
+            _fixture.WithApprenticeshipStatus(ApprenticeshipStatus.Live);
+
+            _fixture.Act();
+
+            _fixture.VerifyApprenticeshipTracking();
+        }
+
+        [Test]
+        public void ThenPaymentStatusIsUpdated()
+        {
+            _fixture.WithApprenticeshipStatus(ApprenticeshipStatus.Live);
+
+            _fixture.Act();
+
+            _fixture.VerifyPaymentStatusChanged();
+        }
+
+        [Test]
+        public void ThenCompletionDateIsUpdated()
+        {
+            _fixture.WithApprenticeshipStatus(ApprenticeshipStatus.Live);
+
+            _fixture.Act();
+
+            _fixture.VerifyCompletionDateChanged();
+        }
+
+        [Test]
+        public void ThenApprenticeshipCompletedEventIsPublished()
+        {
+            _fixture.WithApprenticeshipStatus(ApprenticeshipStatus.Live);
+
+            _fixture.Act();
+
+            _fixture.VerifyApprenticeshipCompletedEventIsPublished();
+        }
+
+    }
+
+    public class WhenCompletingApprenticeshipFixture
+    {
+        private UnitOfWorkContext _unitOfWorkContext;
+        private CommitmentsV2.Models.Apprenticeship _apprenticeship;
+        private CommitmentsV2.Models.Cohort _cohort;
+        private DateTime _completionDate;
+        private Fixture _fixture;
+
+        public WhenCompletingApprenticeshipFixture()
+        {
+            _fixture = new Fixture();
+            _completionDate = _fixture.Create<DateTime>();
+            _unitOfWorkContext = new UnitOfWorkContext();
+            _cohort = new CommitmentsV2.Models.Cohort
+            {
+                Id = 234,
+                EmployerAccountId = 234,
+                ProviderId = 234
+            };
+            _apprenticeship = new CommitmentsV2.Models.Apprenticeship
+            {
+                Id = 64,
+                Cohort = _cohort,
+                StartDate = new DateTime(2020,1,1)
+            };
+        }
+
+        public CommitmentsV2.Models.Apprenticeship Act()
+        {
+            _apprenticeship.Complete(_completionDate);
+            return _apprenticeship;
+        }
+
+        public WhenCompletingApprenticeshipFixture WithApprenticeshipStatus(ApprenticeshipStatus status)
+        {
+            switch (status)
+            {
+                case ApprenticeshipStatus.Live:
+                    _apprenticeship.StartDate = DateTime.UtcNow.AddDays(-1);
+                    _apprenticeship.PaymentStatus = PaymentStatus.Active;
+                    break;
+                case ApprenticeshipStatus.Stopped:
+                    _apprenticeship.PaymentStatus = PaymentStatus.Withdrawn;
+                    break;
+                case ApprenticeshipStatus.WaitingToStart:
+                    _apprenticeship.StartDate = DateTime.UtcNow.AddDays(1);
+                    _apprenticeship.PaymentStatus = PaymentStatus.Active;
+                    break;
+                case ApprenticeshipStatus.Paused:
+                    _apprenticeship.PaymentStatus = PaymentStatus.Paused;
+                    break;
+                case ApprenticeshipStatus.Completed:
+                    _apprenticeship.PaymentStatus = PaymentStatus.Completed;
+                    break;
+            }
+            return this;
+        }
+
+        public void VerifyPaymentStatusChanged()
+        {
+            Assert.AreEqual(PaymentStatus.Completed, _apprenticeship.PaymentStatus);
+        }
+
+
+        public void VerifyCompletionDateChanged()
+        {
+            Assert.AreEqual(_completionDate, _apprenticeship.CompletionDate);
+        }
+
+        public void VerifyApprenticeshipCompletedEventIsPublished()
+        {
+            var apprenticeshipCompletedEvent = _unitOfWorkContext
+                .GetEvents()
+                .OfType<ApprenticeshipCompletedEvent>()
+                .First(x => 
+                    x.CompletionDate == _completionDate &&
+                    x.ApprenticeshipId == _apprenticeship.Id);
+
+            Assert.NotNull(apprenticeshipCompletedEvent);
+        }
+
+        public void VerifyApprenticeshipTracking()
+        {
+            Assert.IsNotNull(_unitOfWorkContext.GetEvents().SingleOrDefault(x => x is EntityStateChangedEvent @event
+                                                                                && @event.EntityType == nameof(CommitmentsV2.Models.Apprenticeship)
+                                                                                && @event.ProviderId == _apprenticeship.Cohort.ProviderId
+                                                                                && @event.EmployerAccountId == _apprenticeship.Cohort.EmployerAccountId));
+        }
+    }
+}
