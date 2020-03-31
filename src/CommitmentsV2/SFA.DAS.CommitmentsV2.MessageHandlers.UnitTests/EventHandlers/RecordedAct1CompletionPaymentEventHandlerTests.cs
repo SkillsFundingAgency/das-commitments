c0a59a8ba5abc6826375367e29b045f4b4425410
chrisfoster186 <chrisfoster186@googlemail.com>
2020-03-31 10:07:12 +0100

    Re-adds missing import

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/RecordedAct1CompletionPaymentEventHandlerTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/RecordedAct1CompletionPaymentEventHandlerTests.cs
@@ -9,8 +9,8 @@ using SFA.DAS.CommitmentsV2.Data;
 using SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers;
 using SFA.DAS.CommitmentsV2.Messages.Events;
 using SFA.DAS.CommitmentsV2.Models;
-using SFA.DAS.CommitmentsV2.TestHelpers;
 using SFA.DAS.CommitmentsV2.Types;
+using SFA.DAS.Testing.Fakes;
 
 namespace SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests.EventHandlers
 {
