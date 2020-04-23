e5b28181ce4b4e38376dbfa2120c80f136b3b512
Akshay Chugh <akshay.chugh30@outlook.com>
2020-04-24 00:01:48 +0100

    CV-730 Added logging to RemovedLegalEntity Handler

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/RemovedLegalEntityEventHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/RemovedLegalEntityEventHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Application.Commands.RemoveAccountLegalEntity;
 using SFA.DAS.EmployerAccounts.Messages.Events;
@@ -9,15 +11,28 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
     public class RemovedLegalEntityEventHandler : IHandleMessages<RemovedLegalEntityEvent>
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<RemovedLegalEntityEventHandler> _logger;
 
-        public RemovedLegalEntityEventHandler(IMediator mediator)
+        public RemovedLegalEntityEventHandler(IMediator mediator, ILogger<RemovedLegalEntityEventHandler> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
-        public Task Handle(RemovedLegalEntityEvent message, IMessageHandlerContext context)
+        public async Task Handle(RemovedLegalEntityEvent message, IMessageHandlerContext context)
         {
-            return _mediator.Send(new RemoveAccountLegalEntityCommand(message.AccountId, message.AccountLegalEntityId, message.Created));
+            try
+            {
+                await _mediator.Send(new RemoveAccountLegalEntityCommand(message.AccountId,
+                    message.AccountLegalEntityId, message.Created));
+
+                _logger.LogInformation($"RemoveLegalEntity - successfully handled {nameof(RemovedLegalEntityEvent)} with created date: {message.Created}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"RemoveLegalEntity - failed to handle {nameof(RemovedLegalEntityEvent)} with created date: {message.Created}");
+                throw;
+            }
         }
     }
 }
