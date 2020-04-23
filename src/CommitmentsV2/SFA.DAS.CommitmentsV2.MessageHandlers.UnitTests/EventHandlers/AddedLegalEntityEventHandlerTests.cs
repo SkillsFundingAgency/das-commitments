44b51b3a6bb4b77735ddff4e3a26d1c58aba2631
Akshay Chugh <akshay.chugh30@outlook.com>
2020-04-23 23:48:10 +0100

    CV-730 Added logging to AddedLegalEntity handler

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/AddedLegalEntityEventHandlerTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/AddedLegalEntityEventHandlerTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Application.Commands.AddAccountLegalEntity;
 using SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers;
+using SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests;
 using SFA.DAS.EmployerAccounts.Messages.Events;
 using SFA.DAS.Testing;
+using SFA.DAS.Testing.Fakes;
+using It = Moq.It;
 
 namespace SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests.EventHandlers
 {
@@ -15,17 +21,66 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests.EventHandlers
         [Test]
         public Task Handle_WhenHandlingAddedLegalEntityEvent_ThenShouldSendAddAccountLegalEntityCommand()
         {
-            return TestAsync(f => f.Handle(), f => f.VerifySend<AddAccountLegalEntityCommand>((c, m) =>
-                c.AccountId == m.AccountId &&
-                c.AccountLegalEntityId == m.AccountLegalEntityId &&
-                c.MaLegalEntityId == m.LegalEntityId &&
-                c.AccountLegalEntityPublicHashedId == m.AccountLegalEntityPublicHashedId &&
-                c.OrganisationName == m.OrganisationName &&
-                c.Created == m.Created));
+            return TestAsync(
+                f => f.Handle(),
+                f => f.VerifySend<AddAccountLegalEntityCommand>((c, m) =>
+                  c.AccountId == m.AccountId &&
+                  c.AccountLegalEntityId == m.AccountLegalEntityId &&
+                  c.MaLegalEntityId == m.LegalEntityId &&
+                  c.AccountLegalEntityPublicHashedId == m.AccountLegalEntityPublicHashedId &&
+                  c.OrganisationName == m.OrganisationName &&
+                  c.Created == m.Created));
+        }
+
+        [Test]
+        public Task Handle_WhenHandlingAddedLegalEntityEvent_AndNoExceptionIsThrown_ThenSuccessInformationIsLogged()
+        {
+            return TestAsync(
+                fixture => fixture.Handle(),
+                (fixture) => { fixture.VerifyLoggerHasInformation();});
+        }
+
+        [Test]
+        public Task Handle_WhenHandlingAddedLegalEntityEvent_AndMediatorThrowsException_ThenErrorIsLogged()
+        {
+            return TestExceptionAsync(
+                (fixture) => { fixture.WithMediatorThrowingException(); },
+                fixture => fixture.Handle(),
+                (fixture, task) =>
+                {
+                    task.Should().Throw<Exception>();
+                    fixture.VerifyLoggerHasErrors();
+                });
         }
     }
 
     public class AddedLegalEntityEventHandlerTestsFixture : EventHandlerTestsFixture<AddedLegalEntityEvent, AddedLegalEntityEventHandler>
     {
+        public FakeLogger<AddedLegalEntityEventHandler> Logger;
+
+        public AddedLegalEntityEventHandlerTestsFixture() : base((m) => null)
+        {
+            Logger = new FakeLogger<AddedLegalEntityEventHandler>();
+
+            Handler = new AddedLegalEntityEventHandler(Mediator.Object, Logger);
+        }
+
+        public AddedLegalEntityEventHandlerTestsFixture WithMediatorThrowingException()
+        {
+            Mediator
+                .Setup(x => x.Send(It.IsAny<AddAccountLegalEntityCommand>(), CancellationToken.None))
+                .ThrowsAsync(new Exception());
+            return this;
+        }
+
+        public void VerifyLoggerHasErrors()
+        {
+            Assert.True(Logger.HasErrors);
+        }
+
+        public void VerifyLoggerHasInformation()
+        {
+            Assert.True(Logger.HasInfo);
+        }
     }
 }
--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/AddedLegalEntityEventHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/AddedLegalEntityEventHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Application.Commands.AddAccountLegalEntity;
 using SFA.DAS.EmployerAccounts.Messages.Events;
@@ -9,20 +11,32 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
     public class AddedLegalEntityEventHandler : IHandleMessages<AddedLegalEntityEvent>
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<AddedLegalEntityEventHandler> _logger;
 
-        public AddedLegalEntityEventHandler(IMediator mediator)
+        public AddedLegalEntityEventHandler(IMediator mediator, ILogger<AddedLegalEntityEventHandler> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
-        public Task Handle(AddedLegalEntityEvent message, IMessageHandlerContext context)
+        public async Task Handle(AddedLegalEntityEvent message, IMessageHandlerContext context)
         {
-            return _mediator.Send(new AddAccountLegalEntityCommand(message.AccountId, message.AccountLegalEntityId, message.LegalEntityId,
-                message.AccountLegalEntityPublicHashedId, message.OrganisationName, 
-                (Models.OrganisationType)message.OrganisationType,
-                message.OrganisationReferenceNumber,
-                message.OrganisationAddress,
-                message.Created));
+            try
+            { 
+                await _mediator.Send(new AddAccountLegalEntityCommand(message.AccountId, message.AccountLegalEntityId, message.LegalEntityId,
+                    message.AccountLegalEntityPublicHashedId, message.OrganisationName,
+                    (Models.OrganisationType)message.OrganisationType,
+                    message.OrganisationReferenceNumber,
+                    message.OrganisationAddress,
+                    message.Created));
+
+                _logger.LogInformation($"AddLegalEntity - successfully handled {nameof(AddedLegalEntityEvent)} with {nameof(message.AccountLegalEntityPublicHashedId)}: {message.AccountLegalEntityPublicHashedId}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"AddLegalEntity - failed to handle {nameof(AddedLegalEntityEvent)} with {nameof(message.AccountLegalEntityPublicHashedId)}: {message.AccountLegalEntityPublicHashedId}");
+                throw;
+            }
         }
     }
 }
