07ec26cedd9e13cb17762e2d604a8a511a301b71
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2020-06-16 12:05:29 +0100

    NlogFix

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Program.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using NLog.Web;
 using SFA.DAS.CommitmentsV2.Api.Extensions;
 using StructureMap.AspNetCore;
+using System;
 
 namespace SFA.DAS.CommitmentsV2.Api
 {
@@ -10,7 +11,8 @@ namespace SFA.DAS.CommitmentsV2.Api
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var logger = NLogBuilder.ConfigureNLog(environment == "Development" ? "nlog.Development.config" : "nlog.config").GetCurrentClassLogger();
             logger.Info("Starting up host");
 
             CreateWebHostBuilder(args).Build().Run();
