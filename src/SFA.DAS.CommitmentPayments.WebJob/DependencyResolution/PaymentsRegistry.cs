f9251266b6c5152b5ead8f8c5ff9ba3a14665d52
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-07-24 10:19:37 +0100

    CON-2171 correctly register PayentsEventsApi for CommitmentPayments.webjob

---
+++ src/SFA.DAS.CommitmentPayments.WebJob/DependencyResolution/PaymentsRegistry.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using SFA.DAS.CommitmentPayments.WebJob.Configuration;
+using SFA.DAS.Http;
+using SFA.DAS.Http.TokenGenerators;
+using SFA.DAS.NLog.Logger.Web.MessageHandlers;
+using SFA.DAS.Provider.Events.Api.Client;
+using SFA.DAS.Provider.Events.Api.Client.Configuration;
+using StructureMap;
+
+namespace SFA.DAS.CommitmentPayments.WebJob.DependencyResolution
+{
+    internal class PaymentsRegistry : Registry
+    {
+        public PaymentsRegistry()
+        {
+            For<PaymentEventsApi>().Use(c => c.GetInstance<CommitmentPaymentsConfiguration>().PaymentEventsApi);
+            For<IPaymentsEventsApiConfiguration>().Use(c => c.GetInstance<PaymentEventsApi>());
+            For<IPaymentsEventsApiClient>().Use<PaymentsEventsApiClient>().Ctor<HttpClient>().Is(c => CreateClient(c));
+        }
+
+        private HttpClient CreateClient(IContext context)
+        {
+            var config = context.GetInstance<CommitmentPaymentsConfiguration>().PaymentEventsApi;
+
+            HttpClient httpClient = new HttpClientBuilder()
+                    .WithBearerAuthorisationHeader(new AzureActiveDirectoryBearerTokenGenerator(config))
+                    .WithHandler(new RequestIdMessageRequestHandler())
+                    .WithHandler(new SessionIdMessageRequestHandler())
+                    .WithDefaultHeaders()
+                    .Build();
+
+
+            return httpClient;
+        }
+    }
+}
