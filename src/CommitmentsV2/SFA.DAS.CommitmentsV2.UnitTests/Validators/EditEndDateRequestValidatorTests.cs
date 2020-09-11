1d50affeebc90e2681bbccb31a8c79ec23a01e33
Shahzad <hkf-tech@users.noreply.github.com>
2020-09-11 10:49:11 +0100

    CON-2032

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Validators/EditEndDateRequestValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
+using SFA.DAS.CommitmentsV2.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Validators
+{
+    [TestFixture]
+    public class EditEndDateRequestValidatorTests
+    {
+        [Test]
+        public void InvalidEndDateValidationFails()
+        {
+
+            var model = new EditEndDateRequest { EndDate = null};
+            AssertValidationResult(request => request.EndDate, model, false);
+        }
+
+        [Test]
+        public void ValidEndDateValidationPasses()
+        {
+
+            var model = new EditEndDateRequest { EndDate = DateTime.Now };
+            AssertValidationResult(request => request.EndDate, model, true);
+        }
+
+        [TestCase(5143541, true)]
+        [TestCase(0, false)]
+        [TestCase(null, false)]
+        public void ThenApprenticeshipIdIsValidated(long apprenticeshipId, bool expectedValid)
+        {
+            var viewModel = new EditEndDateRequest() { ApprenticeshipId = apprenticeshipId };
+            AssertValidationResult(x => x.ApprenticeshipId, viewModel, expectedValid);
+        }
+
+        private void AssertValidationResult<T>(Expression<Func<EditEndDateRequest, T>> property, EditEndDateRequest instance, bool expectedValid)
+        {
+            var validator = new EditEndDateRequestValidator();
+
+            if (expectedValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(property, instance);
+            }
+            else
+            {
+                validator.ShouldHaveValidationErrorFor(property, instance);
+            }
+        }
+    }
+}
