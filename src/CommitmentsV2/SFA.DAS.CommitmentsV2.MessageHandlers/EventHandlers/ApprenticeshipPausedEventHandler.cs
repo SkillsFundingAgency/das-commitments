e09564b042b07a0970448a596908b7dd76647045
Shahzad <hkf-tech@users.noreply.github.com>
2020-10-06 11:02:15 +0100

    CON-2419

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/ApprenticeshipPausedEventHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeship;
+using SFA.DAS.CommitmentsV2.Messages.Commands;
+using SFA.DAS.CommitmentsV2.Messages.Events;
+using SFA.DAS.PAS.Account.Api.ClientV2;
+using SFA.DAS.PAS.Account.Api.Types;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
+{
+    public class ApprenticeshipPausedEventHandler : IHandleMessages<ApprenticeshipPausedEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly IPasAccountApiClient _pasAccountApiClient;
+        private readonly ILogger<ApprenticeshipPausedEventHandler> _logger;
+
+        public ApprenticeshipPausedEventHandler(IMediator mediator, IPasAccountApiClient pasAccountApiClient, ILogger<ApprenticeshipPausedEventHandler> logger)
+        {
+            _mediator = mediator;
+            _pasAccountApiClient = pasAccountApiClient;
+            _logger = logger;
+        }
+
+        public async Task Handle(ApprenticeshipPausedEvent message, IMessageHandlerContext context)
+        {
+            _logger.LogInformation($"Received {nameof(ApprenticeshipPausedEventHandler)} for apprentice {message?.ApprenticeshipId}");
+            var apprenticeshipQueryResult = await _mediator.Send(new GetApprenticeshipQuery(message.ApprenticeshipId));
+
+            var emailToProviderCommand = BuildEmailToProviderCommand(apprenticeshipQueryResult);
+
+            await context.Send(emailToProviderCommand, new SendOptions());
+        }
+
+        private SendEmailToProviderCommand BuildEmailToProviderCommand(GetApprenticeshipQueryResult apprenticeship)
+        {
+            var sendEmailToProviderCommand = new SendEmailToProviderCommand(apprenticeship.ProviderId,
+                "ProviderApprenticeshipPauseNotification",
+                      new Dictionary<string, string>
+                      {
+                                  {"EMPLOYER", apprenticeship.EmployerName},
+                                  {"APPRENTICE", apprenticeship.FirstName},
+                                  {"DATE", apprenticeship.PauseDate?.ToString("dd/MM/yyyy")},
+                                  {"URL", $"{apprenticeship.ProviderId}/apprentices/manage/Hashed/details"}
+                      });
+
+            return sendEmailToProviderCommand;
+        }
+    }
+}
