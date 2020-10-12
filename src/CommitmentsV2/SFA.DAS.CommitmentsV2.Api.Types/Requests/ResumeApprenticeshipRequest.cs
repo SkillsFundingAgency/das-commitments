32e6e76c32cb7783568ac0e78b3d543e4559bbf4
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-10-12 10:30:55 +0100

    CON-2434-resume apprenticehship

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Requests/ResumeApprenticeshipRequest.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SFA.DAS.CommitmentsV2.Api.Types.Requests
+{
+    public class ResumeApprenticeshipRequest : SaveDataRequest
+    {
+        public long ApprenticeshipId { get; set; }
+    }
+}
