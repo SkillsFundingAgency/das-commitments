6ca16b647d00278239cc6b359c283b71bba22a76
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-10-20 13:24:52 +0100

    CON-2434-send email notification.

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests/EventHandlers/ApprenticeshipResumedEventHandlerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using NServiceBus;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeship;
+using SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers;
+using SFA.DAS.CommitmentsV2.Messages.Commands;
+using SFA.DAS.CommitmentsV2.Messages.Events;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.MessageHandlers.UnitTests.EventHandlers
+{
+    [TestFixture]
+    public class ApprenticeshipResumedEventHandlerTests
+    {
+        private ApprenticeshipResumedEventHandlerTestsFixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new ApprenticeshipResumedEventHandlerTestsFixture();
+
+            _fixture.Mediator.Setup(m => m.Send(It.IsAny<GetApprenticeshipQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(GetTestApprenticeshipQueryResult());
+        }
+
+        [Test]
+        public async Task WhenHandlingApprenticeshipResumeEvent_ThenGetApprenticeshipQueryIsCalled()
+        {
+            await _fixture.Handler.Handle(new ApprenticeshipResumedEvent { }, _fixture.MessageHandlerContext.Object);
+
+            _fixture.Mediator.Verify(c => c.Send(It.IsAny<GetApprenticeshipQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public async Task WhenHandlingApprenticeshipResumeEvent_ThenSendEmailToProviderIsCalled()
+        {
+            await _fixture.Handler.Handle(new ApprenticeshipResumedEvent { }, _fixture.MessageHandlerContext.Object);
+
+            _fixture.MessageHandlerContext.Verify(m => m.Send(It.IsAny<SendEmailToProviderCommand>(), It.IsAny<SendOptions>()), Times.Once);
+        }
+
+
+        private GetApprenticeshipQueryResult GetTestApprenticeshipQueryResult()
+        {
+            return new GetApprenticeshipQueryResult
+            {
+                ProviderId = 12345678,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                PauseDate = DateTime.UtcNow
+            };
+        }
+    }
+
+    public class ApprenticeshipResumedEventHandlerTestsFixture : EventHandlerTestsFixture<ApprenticeshipResumedEvent, ApprenticeshipResumedEventHandler>
+    {
+        public Mock<ILogger<ApprenticeshipResumedEventHandler>> Logger { get; set; }
+
+        public ApprenticeshipResumedEventHandlerTestsFixture() : base((m) => null)
+        {
+            Logger = new Mock<ILogger<ApprenticeshipResumedEventHandler>>();
+
+            Handler = new ApprenticeshipResumedEventHandler(Mediator.Object, Logger.Object);
+        }
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/ApprenticeshipResumedEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeship;
+using SFA.DAS.CommitmentsV2.Messages.Commands;
+using SFA.DAS.CommitmentsV2.Messages.Events;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
+{
+    public class ApprenticeshipResumedEventHandler : IHandleMessages<ApprenticeshipResumedEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ApprenticeshipResumedEventHandler> _logger;
+
+        public ApprenticeshipResumedEventHandler(IMediator mediator, ILogger<ApprenticeshipResumedEventHandler> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Handle(ApprenticeshipResumedEvent message, IMessageHandlerContext context)
+        {
+            _logger.LogInformation($"Received {nameof(ApprenticeshipResumedEventHandler)} for apprentice {message?.ApprenticeshipId}");
+
+            var apprenticeshipQueryResult = await _mediator.Send(new GetApprenticeshipQuery(message.ApprenticeshipId));
+
+            var emailToProviderCommand = BuildEmailToProviderCommand(apprenticeshipQueryResult, message.ResumedOn);
+
+            await context.Send(emailToProviderCommand, new SendOptions());
+        }
+
+        private SendEmailToProviderCommand BuildEmailToProviderCommand(GetApprenticeshipQueryResult apprenticeship, DateTime resumeDate)
+        {
+            var sendEmailToProviderCommand = new SendEmailToProviderCommand(apprenticeship.ProviderId,
+                "ProviderApprenticeshipResumeNotification",
+                      new Dictionary<string, string>
+                      {
+                                  {"EMPLOYER", apprenticeship.EmployerName},
+                                  {"APPRENTICE", apprenticeship.FirstName},
+                                  {"DATE", resumeDate.ToString("dd/MM/yyyy")},
+                                  {"URL", $"{apprenticeship.ProviderId}/apprentices/manage/Hashed/details"}
+                      });
+
+            return sendEmailToProviderCommand;
+        }
+    }
+}
