3fb36e8c1588d9b37825d898f4f8ae329ac1e77f
Ben Stone <ben1.stone@education.gov.uk>
2020-10-23 16:12:31 +0100

    CON-2505 Add get all providers endpoint

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetAllProviders/GetAllProvidersHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetAllProviders;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetAllProviders
+{
+    public class GetAllProvidersHandlerTests
+    {
+        private GetAllProvidersHandlerTestsFixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new GetAllProvidersHandlerTestsFixture();
+        }
+
+        [Test]
+        public async Task WhenGettingAllProviders_ThenProvidersAreMappedToProviderResponse()
+        {
+            _fixture.AddProviders().SeedDb();
+
+            var result = await _fixture.Handle();
+
+            Assert.AreEqual(3, result.Providers.Count);
+        }
+    }
+
+    class GetAllProvidersHandlerTestsFixture
+    {
+        private readonly GetAllProvidersQueryHandler _handler;
+        private readonly ProviderCommitmentsDbContext _dbContext;
+
+        public List<Provider> Providers { get; set; }
+
+        public GetAllProvidersHandlerTestsFixture()
+        {
+            _dbContext = new ProviderCommitmentsDbContext(new DbContextOptionsBuilder<ProviderCommitmentsDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+
+            _handler = new GetAllProvidersQueryHandler(new Lazy<ProviderCommitmentsDbContext>(() => _dbContext));
+        }
+
+        public GetAllProvidersHandlerTestsFixture AddProviders()
+        {
+            Providers = new List<Provider>
+            {
+                new Provider { UkPrn = 10000001, Name = "Provider 1"},
+                new Provider { UkPrn = 10000002, Name = "Provider 2"},
+                new Provider { UkPrn = 10000003, Name = "Provider 3"}
+            };
+
+            return this;
+        }
+
+        public GetAllProvidersHandlerTestsFixture SeedDb()
+        {
+            _dbContext.Providers.AddRange(Providers);
+            _dbContext.SaveChanges();
+
+            return this;
+        }
+
+        public Task<GetAllProvidersQueryResult> Handle()
+        {
+            return _handler.Handle(new GetAllProvidersQuery(), CancellationToken.None);
+        }
+    }
+}
