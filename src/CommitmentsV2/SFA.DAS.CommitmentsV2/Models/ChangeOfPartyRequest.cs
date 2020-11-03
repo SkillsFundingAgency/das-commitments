1535b73b2786d8bcbe0be571d80748148ff3f4c5
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-11-03 16:42:42 +0000

    CON-2586-Resolved PR comments

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ChangeOfPartyRequest.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ChangeOfPartyRequest.cs
@@ -47,7 +47,7 @@ namespace SFA.DAS.CommitmentsV2.Models
         {
             CheckOriginatingParty(originatingParty);
             CheckRequestType(originatingParty, changeOfPartyType);
-            CheckPrice(price);
+            CheckPrice(changeOfPartyType, price);
 
             StartTrackingSession(UserAction.CreateChangeOfPartyRequest, originatingParty, apprenticeship.Cohort.AccountLegalEntityId, apprenticeship.Cohort.ProviderId, userInfo);
 
@@ -100,8 +100,9 @@ namespace SFA.DAS.CommitmentsV2.Models
             }
         }
 
-        private void CheckPrice(int? price)
+        private void CheckPrice(ChangeOfPartyRequestType changeOfPartyType, int? price)
         {
+            if (changeOfPartyType == ChangeOfPartyRequestType.ChangeProvider) return;
             if (price != null && (price <= 0 || price > Constants.MaximumApprenticeshipCost))
             {
                 throw new DomainException(nameof(Price), $"Change of Party for  Apprenticeship {ApprenticeshipId} requires Price between 1 and {Constants.MaximumApprenticeshipCost}; {price} is not valid");
