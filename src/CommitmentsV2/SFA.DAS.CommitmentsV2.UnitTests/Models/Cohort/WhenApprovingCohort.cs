80f7a0556d751c6f18a9cb45eec0ae92e3a9c2f0
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-11-24 09:31:34 +0000

    con-2714

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Models/Cohort/WhenApprovingCohort.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Models/Cohort/WhenApprovingCohort.cs
@@ -323,23 +323,7 @@ namespace SFA.DAS.CommitmentsV2.UnitTests.Models.Cohort
             
             _fixture.Cohort.TransferApprovalStatus.Should().Be(TransferApprovalStatus.Approved);
             _fixture.Cohort.WithParty.Should().Be(Party.None);
-        }
-
-        [TestCase(Party.Employer)]
-        [TestCase(Party.Provider)]
-        public void AndChangeOfPartyRequestedAndCohortSetWithTransferSenderIdThenShouldSetPartyNone(Party modifyingParty)
-        {
-            _fixture
-                .SetChangeOfPartyRequestId()
-                .SetTransferSenderId()
-                .SetModifyingParty(modifyingParty)
-                .SetWithParty(modifyingParty)
-                .SetApprovals(modifyingParty.GetOtherParty())
-                .AddDraftApprenticeship()
-                .Approve();
-
-            _fixture.Cohort.WithParty.Should().Be(Party.None);
-        }
+        }        
 
         [TestCase(Party.Employer)]
         [TestCase(Party.Provider)]
