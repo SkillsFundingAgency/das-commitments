9bd4431e7335e4e6616f967942869ab3e83c885b
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-12-02 14:02:47 +0000

    CON-2825-remove party check

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetApprenticeship/GetApprenticeshipQueryResult.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetApprenticeship/GetApprenticeshipQueryResult.cs
@@ -20,7 +20,8 @@ namespace SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeship
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Reference { get; set; }
+        public string EmployerReference { get; set; }
+        public string ProviderReference { get; set; }
         public ApprenticeshipStatus Status { get; set; }
         public DateTime? StopDate { get; set; }
         public DateTime? PauseDate { get; set; }
