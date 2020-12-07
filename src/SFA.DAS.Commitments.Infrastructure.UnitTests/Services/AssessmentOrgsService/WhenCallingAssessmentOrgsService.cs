9ec31017cb337884516b37e757fcc60572787142
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-07 10:08:01 +0000

    Update asessment orgs service to get data from APIM

--- src/SFA.DAS.Commitments.Infrastructure.UnitTests/Services/AssessmentOrgsService/WhenCallingAssessmentOrgsService.cs
+++ src/SFA.DAS.Commitments.Infrastructure.UnitTests/Services/AssessmentOrgsService/WhenCallingAssessmentOrgsService.cs
@@ -4,8 +4,10 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.Apprenticeships.Api.Types.AssessmentOrgs;
-using SFA.DAS.AssessmentOrgs.Api.Client;
+using SFA.DAS.Commitments.Domain.Api.Requests;
+using SFA.DAS.Commitments.Domain.Api.Types;
+using SFA.DAS.Commitments.Domain.Interfaces;
+using SFA.DAS.Commitments.Infrastructure.Api.Requests;
 using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.Commitments.Infrastructure.UnitTests.Services.AssessmentOrgsService
@@ -15,36 +17,36 @@ namespace SFA.DAS.Commitments.Infrastructure.UnitTests.Services.AssessmentOrgsSe
     {
         private Infrastructure.Services.AssessmentOrgsService _sut;
 
-        private Mock<IAssessmentOrgsApiClient> _assessmentOrgsApi;
+        private Mock<IApiClient> _assessmentOrgsApi;
 
         [SetUp]
         public void SetUp()
         {
-            _assessmentOrgsApi = new Mock<IAssessmentOrgsApiClient>();
+            _assessmentOrgsApi = new Mock<IApiClient>();
             _sut = new Infrastructure.Services.AssessmentOrgsService(_assessmentOrgsApi.Object, Mock.Of<ILog>());
         }
 
         [Test]
         public void WhenGettingExceptionsFromApi()
         {
-            _assessmentOrgsApi.Setup(m => m.FindAllAsync()).Throws<Exception>();
+            _assessmentOrgsApi.Setup(m => m.Get<EpaoResponse>(It.IsAny<GetEpaoOrganisationsRequest>())).Throws<Exception>();
 
             Func<Task<IEnumerable<OrganisationSummary>>> act = async () => await _sut.All();
 
             act.ShouldThrow<Exception>();
 
-            _assessmentOrgsApi.Verify(m => m.FindAllAsync(), Times.Exactly(4));
+            _assessmentOrgsApi.Verify(m => m.Get<EpaoResponse>(It.IsAny<GetEpaoOrganisationsRequest>()), Times.Exactly(4));
         }
 
         [Test]
         public async Task WhenCallingPaymentService()
         {
-            _assessmentOrgsApi.Setup(m => m.FindAllAsync())
-                .ReturnsAsync(new OrganisationSummary[0]);
+            _assessmentOrgsApi.Setup(m => m.Get<EpaoResponse>(It.IsAny<GetEpaoOrganisationsRequest>()))
+                .ReturnsAsync(new EpaoResponse{Epaos = new List<OrganisationSummary>()});
 
             await _sut.All();
 
-            _assessmentOrgsApi.Verify(m => m.FindAllAsync(), Times.Once);
+            _assessmentOrgsApi.Verify(m => m.Get<EpaoResponse>(It.IsAny<GetEpaoOrganisationsRequest>()), Times.Once);
         }
     }
 }
