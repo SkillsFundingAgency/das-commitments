403ec0ae1e7aaf45849200430b62161a47b750ec
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-10 06:32:17 +0000

    Add requests for getting standards and frameworks

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Models/Api/WhenBuildingGetFrameworksRequest.cs
@@ -0,0 +1,20 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Models.Api;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Models.Api
+{
+    public class WhenBuildingGetFrameworksRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed()
+        {
+            //Arrange
+            var actual = new GetFrameworksRequest();
+            
+            //Assert
+            actual.GetUrl.Should().Be("frameworks");
+        }
+    }
+}
