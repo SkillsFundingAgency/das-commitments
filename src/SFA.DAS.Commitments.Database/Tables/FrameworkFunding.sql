8423594ca8a082ff7dfb09031986c40ee28328fc
Scott Wakefield <scott@expertcc.co.uk>
2020-12-11 10:04:54 +0000

    FAT2-294 fixed typo.

--- src/SFA.DAS.Commitments.Database/Tables/FrameworkFunding.sql
+++ src/SFA.DAS.Commitments.Database/Tables/FrameworkFunding.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[FrameworkFunding]
 (
     [Id] VARCHAR(25) NOT NULL,
-    [EffecitveFrom] DATETIME NULL,
-    [EffecitveTo] DATETIME NULL,
+    [EffectiveFrom] DATETIME NULL,
+    [EffectiveTo] DATETIME NULL,
     [FundingCap] INT NOT NULL
 )
