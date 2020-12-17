5dadce3e2b0f6de49d8857c8da434f3a996ab4ef
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-17 11:27:27 +0000

    Register apiclient as singleton

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/DependencyResolution/ApprovalsOuterApiServiceRegistry.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/DependencyResolution/ApprovalsOuterApiServiceRegistry.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net.Http;
+using SFA.DAS.CommitmentsV2.Data;
 using SFA.DAS.CommitmentsV2.Domain.Interfaces;
 using SFA.DAS.CommitmentsV2.Infrastructure;
 using SFA.DAS.CommitmentsV2.Services;
 using StructureMap;
+using StructureMap.Pipeline;
 
 namespace SFA.DAS.CommitmentsV2.DependencyResolution
 {
@@ -10,7 +12,7 @@ namespace SFA.DAS.CommitmentsV2.DependencyResolution
     {
         public ApprovalsOuterApiServiceRegistry()
         {
-            For<IApiClient>().Use<ApiClient>().Ctor<HttpClient>().Is(new HttpClient());
+            For<IApiClient>().Use<ApiClient>().Ctor<HttpClient>().Is(new HttpClient()).Singleton();
             For<ITrainingProgrammeLookup>().Use<TrainingProgrammeLookup>().Singleton();
         }
     }
