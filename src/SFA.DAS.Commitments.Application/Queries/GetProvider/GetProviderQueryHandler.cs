5d49e9a2587f163dcea641f3f7d7379feda03a92
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-29 11:52:18 +0000

    Add handler for getting a provider by ukprn

---
+++ src/SFA.DAS.Commitments.Application/Queries/GetProvider/GetProviderQueryHandler.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Domain.Data;
+
+namespace SFA.DAS.Commitments.Application.Queries.GetProvider
+{
+    public class GetProviderQueryHandler : IAsyncRequestHandler<GetProviderQuery, GetProviderQueryResponse>
+    {
+        private readonly IProviderRepository _providerRepository;
+
+        public GetProviderQueryHandler (IProviderRepository providerRepository)
+        {
+            _providerRepository = providerRepository;
+        }
+        public async Task<GetProviderQueryResponse> Handle(GetProviderQuery message)
+        {
+            var result = await _providerRepository.GetProvider(message.Ukprn);
+            
+            return new GetProviderQueryResponse
+            {
+                Provider = result
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.Commitments.Application/Queries/GetProvider/GetProviderQueryResponse.cs
@@ -0,0 +1,9 @@
+using SFA.DAS.Commitments.Domain.Entities;
+
+namespace SFA.DAS.Commitments.Application.Queries.GetProvider
+{
+    public class GetProviderQueryResponse
+    {
+        public Provider Provider { get ; set ; }
+    }
+}
