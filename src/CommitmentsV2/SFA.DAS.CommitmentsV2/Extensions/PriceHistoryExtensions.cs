6b57f92873098a79821ee7d00fba9742f4030e13
Shahzad <hkf-tech@users.noreply.github.com>
2021-02-19 11:32:37 +0000

    further logging.

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/PriceHistoryExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/PriceHistoryExtensions.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Mapping.Apprenticeships;
 using SFA.DAS.CommitmentsV2.Models;
 
 namespace SFA.DAS.CommitmentsV2.Extensions
 {
     public static class PriceHistoryExtensions
     {
-        public static decimal GetPrice(this IEnumerable<PriceHistory> priceEpisodes, DateTime effectiveDate)
+        public static decimal GetPrice(this IEnumerable<PriceHistory> priceEpisodes, DateTime effectiveDate, long apprenticeshipId, ILogger<ApprenticeshipToApprenticeshipDetailsMapper> logger)
         {
-            var episodes = priceEpisodes.ToList();
-            var episode = episodes.FirstOrDefault(x =>
-                x.FromDate <= effectiveDate && (x.ToDate == null || x.ToDate >= effectiveDate));
-            return episode?.Cost ?? episodes.First().Cost;
+            try
+            {
+                var episodes = priceEpisodes.ToList();
+                var episode = episodes.FirstOrDefault(x =>
+                    x.FromDate <= effectiveDate && (x.ToDate == null || x.ToDate >= effectiveDate));
+                return episode?.Cost ?? episodes.First().Cost;
+            }
+            catch (Exception exp)
+            {
+                logger.LogError($"In exception {apprenticeshipId}:{exp.Message}" );
+                throw;
+            }
         }
     }
 }
