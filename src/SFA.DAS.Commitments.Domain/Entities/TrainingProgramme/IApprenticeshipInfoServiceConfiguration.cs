af5963808b3465c517d158af8f7dc86139e3e23e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-26 16:45:33 +0000

    FAT2-430 - Remove old FATv1 configuration code
    Removed old configuration code and updated container registration so that it no longer loaded the FATv1 url

--- src/SFA.DAS.Commitments.Domain/Entities/TrainingProgramme/IApprenticeshipInfoServiceConfiguration.cs
+++
@@ -1,7 +0,0 @@
-namespace SFA.DAS.Commitments.Domain.Entities.TrainingProgramme
-{
-    public interface IApprenticeshipInfoServiceConfiguration
-    {
-        string BaseUrl { get; set; }
-    }
-}
--- src/SFA.DAS.Commitments.Infrastructure/Configuration/ApprenticeshipInfoServiceConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.Commitments.Domain.Entities.TrainingProgramme;
-
-namespace SFA.DAS.Commitments.Infrastructure.Configuration
-{
-    public class ApprenticeshipInfoServiceConfiguration : IApprenticeshipInfoServiceConfiguration
-    {
-        public string BaseUrl { get; set; }
-    }
-}
