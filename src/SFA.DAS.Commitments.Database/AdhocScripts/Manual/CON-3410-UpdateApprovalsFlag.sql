7160f522f67f471ebb57e72b0d33ce51105cd9e2
Shahzad <hkf-tech@users.noreply.github.com>
2021-06-21 11:48:34 +0100

    CON-3410 The approval status wasn't getting updated when the Transfer sender was approving the cohort. This task fixes this bug also a script is added to fix the existing records.

---
+++ src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CON-3410-UpdateApprovalsFlag.sql
@@ -0,0 +1,25 @@
+﻿/*
+CON-3410 - Get all the records which are going to be affected 
+Usage instructions:
+Execute the below againsts the Commitments db.
+Copy the output, to keep a copy all the record which are going to be effected/
+*/
+
+Select * from Commitment
+Where TransferSenderId is not null
+and IsFullApprovalProcessed = 1
+and Approvals = 3
+
+
+/*
+CON-3410 - Update Approval flag where the commitment is funded by transfer, is fully approved and Approvals value is 3 
+Usage instructions:
+Execute the below againsts the Commitments db.
+Before running the below update statement make sure to run the above select statement to get a copy of the records which are going to be effected.
+*/
+
+Update Commitment
+set Approvals  = 7
+Where TransferSenderId is not null
+and IsFullApprovalProcessed = 1
+and Approvals = 3
