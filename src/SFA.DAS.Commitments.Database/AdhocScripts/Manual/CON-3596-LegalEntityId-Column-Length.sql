73f34ae3014265b9d02c81c78cee616ed0d785e2
vas <vasantha.kasirajan@digital.education.gov.uk>
2021-06-22 13:53:24 +0100

    CON-3596 - Failure to handled AddedLegalEntityEvent due to OrganisationReference string length

---
+++ src/SFA.DAS.Commitments.Database/AdhocScripts/Manual/CON-3596-LegalEntityId-Column-Length.sql
@@ -0,0 +1,6 @@
+﻿  /*
+  CON-3596 -  Failure to handled AddedLegalEntityEvent due to OrganisationReference string length
+  */
+  
+  ALTER TABLE [dbo].[AccountLegalEntities]
+  ALTER COLUMN [LegalEntityId] NVARCHAR(100) NOT NULL; 
