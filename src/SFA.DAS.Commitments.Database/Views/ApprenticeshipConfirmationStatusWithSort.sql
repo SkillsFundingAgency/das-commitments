ab5b086f64014d34e41732d35357060cfd1ebcd1
Paul Graham <paul.graham@coprime.co.uk>
2021-07-09 13:15:46 +0100

    chamge sort order (for n/a description)

--- src/SFA.DAS.Commitments.Database/Views/ApprenticeshipConfirmationStatusWithSort.sql
+++ src/SFA.DAS.Commitments.Database/Views/ApprenticeshipConfirmationStatusWithSort.sql
@@ -6,7 +6,7 @@ SELECT
 	[CommitmentsApprovedOn],
 	[ConfirmationOverdueOn],
 	CASE 
-		WHEN CommitmentsApprovedOn IS NULL THEN NULL
+		WHEN CommitmentsApprovedOn IS NULL THEN 'N'
 		WHEN ConfirmationOverdueOn < GETDATE() THEN 'O'
 		WHEN ApprenticeshipConfirmedOn IS NOT NULL THEN 'C'
 		ELSE 'U'
