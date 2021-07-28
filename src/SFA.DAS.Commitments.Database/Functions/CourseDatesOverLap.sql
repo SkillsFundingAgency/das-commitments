a4fa9a36cfc213be45f71bc4da0d888b12f875aa
Paul Graham <paul.graham@coprime.co.uk>
2021-07-28 14:10:04 +0100

    Added SQL function and SP

---
+++ src/SFA.DAS.Commitments.Database/Functions/CourseDatesOverLap.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[CourseDatesOverlap](@StartDate DATETIME2, @EndDate DATETIME2, @NewStartDate DATETIME2, @NewEndDate DATETIME2)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Start INT = YEAR(@StartDate) * 100 + MONTH(@StartDate)
+	DECLARE @End INT = YEAR(@EndDate) * 100 + MONTH(@EndDate)
+	DECLARE @NewStart INT = YEAR(@NewStartDate) * 100 + MONTH(@NewStartDate)
+	DECLARE @NewEnd INT = YEAR(@NewEndDate) * 100 + MONTH(@NewEndDate)
+
+	IF (@NewStart <= @Start AND @NewEnd <= @Start)
+	BEGIN
+		RETURN 0
+	END
+
+	IF (@NewStart >= @End AND @NewEnd >= @End)
+	BEGIN
+		RETURN 0
+	END
+
+	RETURN 1
+END
+GO
---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/CheckForOverlappingEmails.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[CheckForOverlappingEmails]
+	@Email NVARCHAR(200),
+	@StartDate DATETIME2,
+	@EndDate DATETIME2,
+	@ApprenticeshipId INT,
+	@CommitmentId INT = NULL
+AS
+
+SELECT 
+	A.Id, 
+	A.FirstName,
+	A.LastName,
+	A.DateOfBirth,
+	A.CommitmentId,
+	A.StartDate,
+	A.EndDate,
+	A.IsApproved,
+	A.Email,
+	dbo.CourseDatesOverlap(A.StartDate, A.EndDate, @StartDate, @EndDate) AS OverlapStatus
+FROM Apprenticeship A
+WHERE 
+	CASE 
+		WHEN @CommitmentId IS NULL AND A.IsApproved = 1 THEN 1
+		WHEN @CommitmentId IS NOT NULL AND A.CommitmentId = @CommitmentId AND A.IsApproved = 0 AND A.StartDate IS NOT NULL AND A.EndDate IS NOT NULL THEN 1
+		WHEN A.IsApproved = 1 THEN 1
+		ELSE 0
+	END = 1
+	AND A.Id != ISNULL(@ApprenticeshipId,0) 
+	AND A.Email = @Email 
+	AND dbo.CourseDatesOverlap(A.StartDate, A.EndDate, @StartDate, @EndDate) >= 1 
