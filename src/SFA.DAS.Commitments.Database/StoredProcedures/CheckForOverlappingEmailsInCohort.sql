ddcfaab0be3a99077a5eb28af7d418ed22b84c0a
Paul Graham <paul.graham@coprime.co.uk>
2021-08-02 17:13:00 +0100

    added corfe SQL

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/CheckForOverlappingEmailsInCohort.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[CheckForOverlappingEmailsInCohort](@CohortId INT)
+AS
+BEGIN
+	DECLARE @emails dbo.[EmailCheckTable] 
+
+	INSERT INTO @emails (RowId, Email, StartDate, EndDate, ApprenticeshipId)
+	SELECT Id, Email, StartDate, EndDate, Id FROM Apprenticeship WHERE CommitmentId = @CohortId AND Email IS NOT NULL AND StartDate IS NOT NULL AND EndDate IS NOT NULL
+
+	EXEC CheckForOverlappingEmailsForTable @emails, @CohortId
+END
