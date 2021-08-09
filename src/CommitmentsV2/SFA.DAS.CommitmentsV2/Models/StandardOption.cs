bfc44c45c7b0f969d6f4783ae0d4fad6eb1adf6e
Ben Stone <ben1.stone@education.gov.uk>
2021-08-09 16:32:48 +0100

    SV-939 Remove commented code

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/StandardOption.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/StandardOption.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace SFA.DAS.CommitmentsV2.Models
 {
     public class StandardOption
     {
-        //[Key]
-        //public Guid Id { get; set; }
         public string StandardUId { get; set; }
         public string Option { get; set; }
     }
