8b0afa0df004d0990c543de05878b2421333e487
chazzysb <cballagan@yahoo.com>
2021-08-17 10:25:25 +0100

    CMADApprovalsChanges-api 865 866 1030

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Extensions/QueryableApprenticeshipsExtensions/WhenFilteringApprenticeships.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Extensions/QueryableApprenticeshipsExtensions/WhenFilteringApprenticeships.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Extensions;
 using SFA.DAS.CommitmentsV2.Models;
