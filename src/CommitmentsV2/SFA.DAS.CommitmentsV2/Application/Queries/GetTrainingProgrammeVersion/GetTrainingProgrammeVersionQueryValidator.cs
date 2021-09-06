50b3e7d611de426a975590ed7b6f1b7e3190482f
Ben Stone <ben1.stone@education.gov.uk>
2021-09-06 16:06:58 +0100

    SV-1165 Fix validator + unit tests

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetTrainingProgrammeVersion/GetTrainingProgrammeVersionQueryValidator.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetTrainingProgrammeVersion/GetTrainingProgrammeVersionQueryValidator.cs
@@ -6,13 +6,13 @@ namespace SFA.DAS.CommitmentsV2.Application.Queries.GetTrainingProgrammeVersion
     {
         public GetTrainingProgrammeVersionQueryValidator()
         {
-            When(q => q.StandardUId == null, () =>
+            When(q => string.IsNullOrEmpty(q.StandardUId), () =>
             {
                 RuleFor(q => q.CourseCode).NotEmpty();
                 RuleFor(q => q.Version).NotEmpty();
             });
 
-            When(q => q.StandardUId == null && q.Version == null, () =>
+            When(q => string.IsNullOrEmpty(q.CourseCode) && string.IsNullOrEmpty(q.Version), () =>
             {
                 RuleFor(q => q.StandardUId).NotEmpty();
             });
