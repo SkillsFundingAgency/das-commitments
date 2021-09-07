61ef45530a20dc040c7df6c0f0cdcb8c10bef89e
Matt <matt.daniels@digital.education.gov.uk>
2021-09-07 07:12:02 +0100

    Ignoring unit test due to .Net Core 2.2 in-memory database not supporting calling stored procedures.

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetAllLearners/GetAllLearnersHandlerTests.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetAllLearners/GetAllLearnersHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetAllLearners;
 using SFA.DAS.CommitmentsV2.Data;
@@ -10,63 +11,70 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetAllLearners
 {
+    [TestFixture]
     public class GetAllLearnersHandlerTests
-    {
-        /*
-        private GetAllLearnersHandlerTestsFixture _fixture;
-
-        [SetUp]
-        public void Arrange()
-        {
-            _fixture = new GetAllLearnersHandlerTestsFixture();
-        }
-
-        [Test]
-        public async Task WhenGettingAllLearners_ThenLearnersAreMappedToLearnerResponse()
-        {
-            _fixture.AddApprenticeships().SeedDb();
-
-            var result = await _fixture.Handle();
-
-            Assert.AreEqual(3, result.Learners.Count);
-        }
-    }
-
-    class GetAllLearnersHandlerTestsFixture
     {
         private readonly GetAllLearnersQueryHandler _handler;
         private readonly ProviderCommitmentsDbContext _dbContext;
 
-        public List<Apprenticeship> Apprenticeships { get; set; }
+        /*
+         * SQL to generate test data in C# format
+         
+            SELECT TOP 10
+            CONCAT('new Apprenticeship { Id = ', Id, '') + 
+            CONCAT(', FirstName = "', FirstName, '"') +
+            CONCAT(', LastName = "', LastName, '"') +
+            CONCAT(', StartDate = new DateTime(', FORMAT(StartDate, 'yyyy'), ', ', FORMAT(StartDate, 'MM'  ), ', ', FORMAT(StartDate, 'dd'  ), ')' ) +
+            CONCAT(', EndDate = new DateTime(', FORMAT(EndDate, 'yyyy'), ', ', FORMAT(EndDate, 'MM'  ), ', ', FORMAT(EndDate, 'dd'  ), ')' ) +
+            CONCAT(', CreatedOn = new DateTime(', FORMAT(CreatedOn, 'yyyy'), ', ', FORMAT(CreatedOn, 'MM'  ), ', ', FORMAT(CreatedOn, 'dd'  ), ')' ) +
+            ' },'
+            FROM Apprenticeship
 
-        public GetAllLearnersHandlerTestsFixture()
+         */
+        private readonly List<Apprenticeship> Apprenticeships = new List<Apprenticeship>
+            {
+                new Apprenticeship { Id = 289438, FirstName = "Test", LastName = "Name8028125094", StartDate = new DateTime(2018, 07, 01), EndDate = new DateTime(2019, 10, 01), CreatedOn = new DateTime(2018, 07, 30) },
+                new Apprenticeship { Id = 505412, FirstName = "Test", LastName = "Name9612111701", StartDate = new DateTime(2019, 03, 01), EndDate = new DateTime(2020, 06, 01), CreatedOn = new DateTime(2019, 03, 28) },
+                new Apprenticeship { Id = 533717, FirstName = "Test", LastName = "Name1316758754", StartDate = new DateTime(2019, 04, 01), EndDate = new DateTime(2021, 04, 01), CreatedOn = new DateTime(2019, 05, 02) },
+                new Apprenticeship { Id = 543913, FirstName = "Test", LastName = "Name9462140496", StartDate = new DateTime(2019, 07, 01), EndDate = new DateTime(2021, 06, 01), CreatedOn = new DateTime(2019, 05, 21) },
+                new Apprenticeship { Id = 631487, FirstName = "Test", LastName = "Name3355170153", StartDate = new DateTime(2019, 09, 01), EndDate = new DateTime(2021, 11, 01), CreatedOn = new DateTime(2019, 09, 17) },
+                new Apprenticeship { Id = 725441, FirstName = "Test", LastName = "Name6092998573", StartDate = new DateTime(2019, 11, 01), EndDate = new DateTime(2021, 11, 01), CreatedOn = new DateTime(2019, 11, 28) },
+                new Apprenticeship { Id = 788611, FirstName = "Test", LastName = "Name9729801578", StartDate = new DateTime(2020, 02, 01), EndDate = new DateTime(2021, 05, 01), CreatedOn = new DateTime(2020, 02, 10) },
+                new Apprenticeship { Id = 890154, FirstName = "Test", LastName = "Name7979537789", StartDate = new DateTime(2020, 09, 01), EndDate = new DateTime(2022, 03, 01), CreatedOn = new DateTime(2020, 08, 12) },
+                new Apprenticeship { Id = 923289, FirstName = "Test", LastName = "Name3097091906", StartDate = new DateTime(2020, 10, 01), EndDate = new DateTime(2024, 04, 01), CreatedOn = new DateTime(2020, 09, 24) },
+                new Apprenticeship { Id = 935805, FirstName = "Test", LastName = "Name2297563030", StartDate = new DateTime(2020, 10, 01), EndDate = new DateTime(2022, 09, 01), CreatedOn = new DateTime(2020, 10, 02) },
+            };
+
+
+        public GetAllLearnersHandlerTests()
         {
             _dbContext = new ProviderCommitmentsDbContext(new DbContextOptionsBuilder<ProviderCommitmentsDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
             _handler = new GetAllLearnersQueryHandler(new Lazy<ProviderCommitmentsDbContext>(() => _dbContext));
         }
 
-        public GetAllLearnersHandlerTestsFixture AddApprenticeships()
+        [SetUp]
+        public void Arrange()
         {
-            Apprenticeships = new List<Apprenticeship>
-            {
-                new Apprenticeship { FirstName = "Matt", LastName = "Daniels" },
-            };
 
-            return this;
-        }
-
-        public GetAllLearnersHandlerTestsFixture SeedDb()
-        {
             _dbContext.Apprenticeships.AddRange(Apprenticeships);
             _dbContext.SaveChanges();
-
-            return this;
         }
 
-        public Task<GetAllLearnersQueryResult> Handle()
+        [Test]
+        [Ignore("In-memory database in Core 2.2 does not support .FromSql")]
+        public async Task When_GettingAllLearners_Then_LearnersAreMappedToLearnerResponse()
         {
-            return _handler.Handle(new GetAllLearnersQuery(), CancellationToken.None);
+            // Arrange.
+
+
+            // Act.
+
+            var result = await _handler.Handle(new GetAllLearnersQuery(null, 0, 0), CancellationToken.None);
+
+            // Assert
+
+            result.Learners.Should().NotBeNull();
+            result.Learners.Should().HaveCount(Apprenticeships.Count);
         }
-        */
     }
 }
