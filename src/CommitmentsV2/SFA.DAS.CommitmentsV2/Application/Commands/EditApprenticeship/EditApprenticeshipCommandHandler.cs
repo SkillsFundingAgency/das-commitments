f53654666190347e8ed8ea2809bd57b274828132
Ben Stone <ben1.stone@education.gov.uk>
2021-09-08 09:30:18 +0100

    SV-1165 Update Edit apprenticeship handler to update course name

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/EditApprenticeship/EditApprenticeshipCommandHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/EditApprenticeship/EditApprenticeshipCommandHandler.cs
@@ -85,8 +85,8 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.EditApprenticeship
 
                         var standardVersion = result.TrainingProgramme;
 
-                        apprenticeshipUpdate.TrainingName = standardVersion.Name;
-                        apprenticeshipUpdate.TrainingType = standardVersion.ProgrammeType;
+                        apprenticeshipUpdate.TrainingName = apprenticeship.CourseName != standardVersion.Name ? standardVersion.Name : null;
+                        apprenticeshipUpdate.TrainingType = apprenticeship.ProgrammeType != standardVersion.ProgrammeType ? standardVersion.ProgrammeType : null;
                         apprenticeshipUpdate.StandardUId = standardVersion.StandardUId;
                         apprenticeshipUpdate.TrainingCourseVersion = standardVersion.Version;
                     }
