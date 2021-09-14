4c937b6b773b2ec3662455b42509dd7e82c16e9a
Paul Graham <paul.graham@coprime.co.uk>
2021-09-14 10:51:51 +0100

    Added NoEmail Count view

---
+++ src/SFA.DAS.Commitments.Database/Views/ApprenticeshipsWithNoEmail.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[ApprenticeshipsWithNoEmail]
+	AS 
+SELECT 
+	C.EmployerAndProviderApprovedOn AS ApprovedOn,
+	A.Id AS ApprenticeshipId
+FROM Apprenticeship A
+INNER JOIN Commitment C ON A.CommitmentId = C.Id
+WHERE A.IsApproved = 1 AND C.EmployerAndProviderApprovedOn >= '2000-09-10' AND A.Email IS NULL
