8ea2dec047fbaf8c3101d0676c052e3b9a26330d
chrisfoster76 <chrisfoster186@googlemail.com>
2021-10-19 08:35:01 +0100

    Adds PledgeApplicationId to TransferRequestApprovedEvent

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/TransferRequestApprovedEvent.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/TransferRequestApprovedEvent.cs
@@ -7,15 +7,17 @@ namespace SFA.DAS.CommitmentsV2.Messages.Events
     {
         public long TransferRequestId { get; }
         public long CohortId { get; }
+        public int? PledgeApplicationId { get; }
         public DateTime ApprovedOn { get; }
         public UserInfo UserInfo { get; }
 
-        public TransferRequestApprovedEvent(long transferRequestId, long cohortId, DateTime approvedOn, UserInfo userInfo)
+        public TransferRequestApprovedEvent(long transferRequestId, long cohortId, DateTime approvedOn, UserInfo userInfo, int? pledgeApplicationId=null)
         {
             TransferRequestId = transferRequestId;
             CohortId = cohortId;
             ApprovedOn = approvedOn;
             UserInfo = userInfo;
+            PledgeApplicationId = pledgeApplicationId;
         }
     }
 }
