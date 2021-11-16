6f4c5f5ae0944617831d5a60d045c5ce8295b7a4
Shahzad <hkf-tech@users.noreply.github.com>
2021-11-16 23:58:49 +0000

    CON-4064 added some further code for basic authentication when running locally

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.Api.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            string username;
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials =  System.Text.Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                username = credentials[0];
+            }
+            catch
+            {
+                //if no auhtorization header specified default to employer.
+                username = "employer";
+            }
+
+            bool isProvider = false;
+            if (username == "provider")
+            {
+                isProvider = true;
+            }
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, username),
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role,  isProvider ? "Provider" : "Employer"),
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return await Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
