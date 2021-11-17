addf4550e8a0de6a979290691511ff04140d08ca
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-11-17 11:52:00 +0000

    Suppressing emails on rejected event if auto approval = true

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/TransferRequestRejectedEventHandlerForEmailNotifications.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/TransferRequestRejectedEventHandlerForEmailNotifications.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetCohortSummary;
 using SFA.DAS.CommitmentsV2.Configuration;
+using SFA.DAS.CommitmentsV2.Data;
 using SFA.DAS.CommitmentsV2.Messages.Commands;
 using SFA.DAS.CommitmentsV2.Messages.Events;
 using SFA.DAS.Encoding;
@@ -13,14 +16,18 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
     public class
         TransferRequestRejectedEventHandlerForEmailNotifications : IHandleMessages<TransferRequestRejectedEvent>
     {
+        private readonly Lazy<ProviderCommitmentsDbContext> _dbContext;
         private readonly IMediator _mediator;
         private readonly IEncodingService _encodingService;
         private readonly CommitmentsV2Configuration _commitmentsV2Configuration;
 
-        public TransferRequestRejectedEventHandlerForEmailNotifications(IMediator mediator,
+        public TransferRequestRejectedEventHandlerForEmailNotifications(
+            Lazy<ProviderCommitmentsDbContext> dbContext,
+            IMediator mediator,
             IEncodingService encodingService,
             CommitmentsV2Configuration commitmentsV2Configuration)
         {
+            _dbContext = dbContext;
             _mediator = mediator;
             _encodingService = encodingService;
             _commitmentsV2Configuration = commitmentsV2Configuration;
@@ -28,21 +35,31 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
 
         public async Task Handle(TransferRequestRejectedEvent message, IMessageHandlerContext context)
         {
+            var db = _dbContext.Value;
+
             var cohortSummary = await _mediator.Send(new GetCohortSummaryQuery(message.CohortId));
 
             var cohortReference = _encodingService.Encode(cohortSummary.CohortId, EncodingType.CohortReference);
 
-            var employerEncodedAccountId = _encodingService.Encode(cohortSummary.AccountId, EncodingType.AccountId);
+            var transferRequest = await db.TransferRequests.SingleAsync(x => x.Id == message.TransferRequestId);
 
-            var sendEmailToEmployerCommand = new SendEmailToEmployerCommand(cohortSummary.AccountId,
-                "SenderRejectedCommitmentEmployerNotification", new Dictionary<string, string>
-                {
-                    {"employer_name", cohortSummary.LegalEntityName},
-                    {"cohort_reference", cohortReference},
-                    {"sender_name", cohortSummary.TransferSenderName},
-                    {"RequestUrl", $"{_commitmentsV2Configuration.EmployerCommitmentsBaseUrl}{employerEncodedAccountId}/unapproved/{cohortReference}" }
-                },
-                cohortSummary.LastUpdatedByEmployerEmail);
+            var tasks = new List<Task>();
+
+            if (!transferRequest.AutoApproval) { 
+                var employerEncodedAccountId = _encodingService.Encode(cohortSummary.AccountId, EncodingType.AccountId);
+
+                var sendEmailToEmployerCommand = new SendEmailToEmployerCommand(cohortSummary.AccountId,
+                    "SenderRejectedCommitmentEmployerNotification", new Dictionary<string, string>
+                    {
+                        {"employer_name", cohortSummary.LegalEntityName},
+                        {"cohort_reference", cohortReference},
+                        {"sender_name", cohortSummary.TransferSenderName},
+                        {"RequestUrl", $"{_commitmentsV2Configuration.EmployerCommitmentsBaseUrl}{employerEncodedAccountId}/unapproved/{cohortReference}" }
+                    },
+                    cohortSummary.LastUpdatedByEmployerEmail);
+
+                tasks.Add(context.Send(sendEmailToEmployerCommand, new SendOptions()));
+            }
 
             var sendEmailToProviderCommand = new SendEmailToProviderCommand(cohortSummary.ProviderId.Value,
                 "SenderRejectedCommitmentProviderNotification",
@@ -53,10 +70,9 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers
                 },
                 cohortSummary.LastUpdatedByProviderEmail);
 
-            await Task.WhenAll(
-                context.Send(sendEmailToProviderCommand, new SendOptions()),
-                context.Send(sendEmailToEmployerCommand, new SendOptions())
-            );
+            tasks.Add(context.Send(sendEmailToProviderCommand, new SendOptions()));
+
+            await Task.WhenAll(tasks);
         }
     }
 }
