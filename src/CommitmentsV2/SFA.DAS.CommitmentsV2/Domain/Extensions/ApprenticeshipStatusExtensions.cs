8488b10b445230a84cfcb5c1715112ec1f491198
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2021-11-23 10:39:15 +0000

    CON-4043

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Domain/Extensions/ApprenticeshipStatusExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Domain/Extensions/ApprenticeshipStatusExtensions.cs
@@ -13,9 +13,7 @@ namespace SFA.DAS.CommitmentsV2.Domain.Extensions
             var waitingToStart = source.StartDate.HasValue && source.StartDate.Value > now;
 
             switch (source.PaymentStatus)
-            {
-                case PaymentStatus.PendingApproval: //TODO : Need to Check
-                    return ApprenticeshipStatus.WaitingToStart;
+            {               
                 case PaymentStatus.Active:
                     return waitingToStart ? ApprenticeshipStatus.WaitingToStart : ApprenticeshipStatus.Live;
                 case PaymentStatus.Paused:
