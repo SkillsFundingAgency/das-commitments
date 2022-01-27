bf8ec7a53567ae9542ab9750b6cc9ed62d17c999
Paul Howes <paul.howes@cognetics.net>
2022-01-27 10:57:58 +0000

    Package changes

--- src/SFA.DAS.Commitments.Infrastructure/Data/BaseRepository.cs
+++ src/SFA.DAS.Commitments.Infrastructure/Data/BaseRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using Microsoft.Azure.Services.AppAuthentication;
 using Polly;
