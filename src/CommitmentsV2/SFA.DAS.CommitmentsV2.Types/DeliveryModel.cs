c257a56fa0532697159aa906aa46cfda5ea51d6e
Ben Arnold <ben@arnoldfamily.me.uk>
2022-02-18 14:49:26 +0000

    Serialise DeliveryModel as string in events

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/DeliveryModel.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/DeliveryModel.cs
@@ -1,5 +1,9 @@
-﻿namespace SFA.DAS.CommitmentsV2.Types
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SFA.DAS.CommitmentsV2.Types
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum DeliveryModel : byte
     {
         Normal = 0,
