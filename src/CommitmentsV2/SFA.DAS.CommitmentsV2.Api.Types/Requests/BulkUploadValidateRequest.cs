ca960cd937005822a8a1d093c4e41466632cfb6e
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-03-04 09:37:16 +0000

    CON-4186 PR comment

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Requests/BulkUploadValidateRequest.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Requests/BulkUploadValidateRequest.cs
@@ -7,35 +7,4 @@ namespace SFA.DAS.CommitmentsV2.Api.Types.Requests
         public long ProviderId { get; set; }
         public IEnumerable<BulkUploadAddDraftApprenticeshipRequest> CsvRecords { get; set; }
     }
-
-    //public class CsvRecord
-    //{
-    //    public int RowNumber { get; set; }
-    //    public long CohortId { get; set; }
-    //    public string CohortRef { get; set; }
-
-    //    public string ULN { get; set; }
-
-    //    public string FamilyName { get; set; }
-
-    //    public string GivenNames { get; set; }
-
-    //    public string DateOfBirth { get; set; }
-
-    //    public string StdCode { get; set; }
-
-    //    public string StartDate { get; set; }
-
-    //    public string EndDate { get; set; }
-
-    //    public string TotalPrice { get; set; }
-
-    //    public string EPAOrgID { get; set; }
-
-    //    public string ProviderRef { get; set; }
-
-    //    public string AgreementId { get; set; }
-    //    public long LegalEntityId { get; set; }
-    //    public string EmailAddress { get; set; }
-    //}
 }
