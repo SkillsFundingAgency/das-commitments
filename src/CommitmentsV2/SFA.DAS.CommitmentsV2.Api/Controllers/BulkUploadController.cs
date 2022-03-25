f447b7b0d35a3b41a5dafaeb6c98e15934c245e8
Ben Arnold <ben@arnoldfamily.me.uk>
2022-03-25 14:47:47 +0000

    Add HttpGet to route to fix Swagger

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/BulkUploadController.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/BulkUploadController.cs
@@ -58,6 +58,7 @@ namespace SFA.DAS.CommitmentsV2.Api.Controllers
             return Ok(result);
         }
 
+        [HttpPost]
         [Route("validate")]
         public async Task<IActionResult> Validate(BulkUploadValidateApiRequest request, CancellationToken cancellationToken = default)
         {
