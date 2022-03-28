145d5feb26c5f7a0f38a408768077043f74a87e1
Akshay Chugh <akshay.chugh30@outlook.com>
2022-03-28 17:06:34 +0100

    PV2-2849 draft controller + endpoint

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprenticeshipStatisticsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.CommitmentsV2.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/apprenticeshipstatistics")]
+    public class ApprenticeshipStatisticsController
+    {
+
+        [HttpGet]
+        [Route("/stats")]
+        public async Task<IActionResult> GetStatistics(int lastNumberOfDays)
+        {
+
+        }
+    }
+}
