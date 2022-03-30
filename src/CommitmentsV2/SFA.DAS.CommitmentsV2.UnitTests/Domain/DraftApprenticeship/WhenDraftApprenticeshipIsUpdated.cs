e7117b1d99cb6baa2d4f3b6865ce66ae94748461
Paul Graham <paul.graham@coprime.co.uk>
2022-03-30 15:41:40 +0100

    force build

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Domain/DraftApprenticeship/WhenDraftApprenticeshipIsUpdated.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Domain/DraftApprenticeship/WhenDraftApprenticeshipIsUpdated.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using SFA.DAS.CommitmentsV2.Domain.Entities;
 using SFA.DAS.CommitmentsV2.Domain.Exceptions;
-using SFA.DAS.CommitmentsV2.Domain.Extensions;
 using SFA.DAS.CommitmentsV2.Types;
 
 namespace SFA.DAS.CommitmentsV2.UnitTests.Domain.DraftApprenticeship
