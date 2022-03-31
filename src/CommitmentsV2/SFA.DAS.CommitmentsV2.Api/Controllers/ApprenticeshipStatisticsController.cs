a6cdf1d887aa324aa07b7531705cc3cbdeece6da
Akshay Chugh <akshay.chugh30@outlook.com>
2022-03-31 17:11:05 +0100

    PV2-2849 initial controller logic

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprenticeshipStatisticsController.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprenticeshipStatisticsController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeshipStatistics;
 
 namespace SFA.DAS.CommitmentsV2.Api.Controllers
 {
@@ -9,12 +11,24 @@ namespace SFA.DAS.CommitmentsV2.Api.Controllers
     [Route("api/apprenticeshipstatistics")]
     public class ApprenticeshipStatisticsController
     {
+        private readonly IMediator _mediator;
+
+        public ApprenticeshipStatisticsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
 
         [HttpGet]
         [Route("/stats")]
         public async Task<IActionResult> GetStatistics(int lastNumberOfDays)
         {
+            var result = await _mediator.Send(new GetApprenticeshipStatisticsQuery { LastNumberOfDays = lastNumberOfDays });
 
+            if (result == null)
+            {
+                return NotFound();
+            }
         }
     }
 }
