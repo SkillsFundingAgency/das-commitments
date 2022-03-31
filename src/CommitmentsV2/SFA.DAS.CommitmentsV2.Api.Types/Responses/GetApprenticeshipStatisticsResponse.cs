8cf2d0a272b729f45166d18eb6aef0d3f6dcaf0f
Akshay Chugh <akshay.chugh30@outlook.com>
2022-03-31 17:10:33 +0100

    PV2-2849 GetApprenticeshipStatisticsResponse api response dto

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Responses/GetApprenticeshipStatisticsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.CommitmentsV2.Api.Types.Responses
+{
+    public class GetApprenticeshipStatisticsResponse
+    {
+        public long ApprovedApprenticeshipCount { get; set; }
+        public long PausedApprenticeshipCount { get; set; }
+        public long StoppedApprenticeshipCount { get; set; }
+    }
+}
