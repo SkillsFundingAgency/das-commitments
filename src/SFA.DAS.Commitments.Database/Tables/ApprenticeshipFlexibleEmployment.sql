c46d89ef33956fa19f886d64722b1c042c1c5db3
Ben Arnold <ben@arnoldfamily.me.uk>
2022-04-01 11:46:59 +0100

    Delete entire cohort including flexible employment data

--- src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipFlexibleEmployment.sql
+++ src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipFlexibleEmployment.sql
@@ -3,7 +3,7 @@
 	[ApprenticeshipId] BIGINT NOT NULL,
 	[EmploymentPrice] INT NULL,
 	[EmploymentEndDate] DATETIME2 NULL,
-	CONSTRAINT [FK_ApprenticeshipFlexibleEmployment_ApprenticeshipId] FOREIGN KEY ([ApprenticeshipId]) REFERENCES [Apprenticeship]([Id]),
+	CONSTRAINT [FK_ApprenticeshipFlexibleEmployment_ApprenticeshipId] FOREIGN KEY ([ApprenticeshipId]) REFERENCES [Apprenticeship]([Id]) ON DELETE CASCADE,
     CONSTRAINT [PK_ApprenticeshipFlexibleEmployment] PRIMARY KEY ([ApprenticeshipId])
 )
 GO
