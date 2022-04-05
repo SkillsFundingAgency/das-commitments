c3a986e94975a55bf71f6a4e591f45bb4f3d214e
Akshay Chugh <akshay.chugh30@outlook.com>
2022-04-05 16:12:13 +0100

    PV2-2849 GetApprenticeshipStatisticsQueryValidator unit tests

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.UnitTests/Application/Queries/GetApprenticeshipStatisticsTests/GetApprenticeshipStatisticsValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeshipStatistics;
+
+namespace SFA.DAS.CommitmentsV2.UnitTests.Application.Queries.GetApprenticeshipStatisticsTests
+{
+    [TestFixture]
+    public class GetApprenticeshipStatisticsValidatorTests
+    {
+        [TestCase(-10, false)]
+        [TestCase(0, false)]
+        [TestCase(10, true)]
+        public void WhenCallingValidate_ThenCorrectlyValidatesLastNumberOfDays(int lastNumberOfDays,
+            bool expectedValid)
+        {
+            //Arrange
+            var validator = new GetApprenticeshipStatisticsQueryValidator();
+
+            //Act
+            var result = validator.Validate(new GetApprenticeshipStatisticsQuery { LastNumberOfDays = lastNumberOfDays });
+
+            //Assert
+            result.IsValid.Should().Be(expectedValid);
+        }
+    }
+}
