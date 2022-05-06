bb5272b7f4162531bfd5a0c25a3555392823a658
Kola samuel <kola.samuel@digital.education.gov.uk>
2022-05-06 11:23:22 +0100

    clean up

--- src/SFA.DAS.Commitments.Support.SubSite.Old/DependencyResolution/ILoggingPropertyFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.Commitments.Support.SubSite.DependencyResolution
-{
-    public interface ILoggingPropertyFactory
-    {
-        IDictionary<string, object> GetProperties();
-    }
-}
