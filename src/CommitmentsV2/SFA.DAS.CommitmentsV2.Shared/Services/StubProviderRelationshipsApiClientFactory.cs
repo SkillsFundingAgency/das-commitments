3a505f216ef3f2c509510ef1d769ad74fb8bc42c
chrisfoster76 <chrisfoster186@googlemail.com>
2022-06-14 14:49:00 +0100

    Adds stub provider relationships api factory

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Shared/Services/StubProviderRelationshipsApiClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using SFA.DAS.ProviderRelationships.Api.Client;
+using SFA.DAS.ProviderRelationships.Api.Client.Http;
+
+namespace SFA.DAS.CommitmentsV2.Shared.Services
+{
+    public class StubProviderRelationshipsApiClientFactory : IProviderRelationshipsApiClientFactory
+    {
+        public IProviderRelationshipsApiClient CreateApiClient()
+        {
+            return new StubProviderRelationshipsApiClient();
+        }
+    }
+}
