c506297d53d21008b8fbb5c6bccd72b79e84f306
Ben Arnold <ben@arnoldfamily.me.uk>
2022-07-19 20:42:40 +0100

    Add dashboard reporting views for delivery model and flexible apprenticeships

---
+++ src/SFA.DAS.Commitments.Database/Views/ApprenticeshipsCreated.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[ApprenticeshipsCreated]
+	AS SELECT Id, CreatedOn,
+		CASE 
+			WHEN DeliveryModel IS NULL THEN 0
+			ELSE DeliveryModel
+		END AS DeliveryModel
+	FROM [Apprenticeship]
---
+++ src/SFA.DAS.Commitments.Database/Views/FlexibleApprenticeships.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[FlexibleApprenticeships]
+	AS SELECT
+		  Id
+		, CreatedOn
+		, DeliveryModel
+		, EmploymentPrice
+		, EmploymentEndDate
+	FROM [ApprenticeshipsCreated]
+	LEFT JOIN [ApprenticeshipFlexibleEmployment] ON [ApprenticeshipsCreated].Id = [ApprenticeshipFlexibleEmployment].ApprenticeshipId
