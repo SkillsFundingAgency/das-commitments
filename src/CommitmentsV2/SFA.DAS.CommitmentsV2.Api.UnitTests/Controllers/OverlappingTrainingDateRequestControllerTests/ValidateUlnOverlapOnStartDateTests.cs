75ab89b9c482b596425ef8d79f8b4097cbf30c59
Kola samuel <kola.samuel@digital.education.gov.uk>
2022-08-02 14:09:34 +0100

    More Test

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/Controllers/OverlappingTrainingDateRequestControllerTests/ValidateUlnOverlapOnStartDateTests.cs
@@ -0,0 +1,74 @@
+﻿using AutoFixture;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Api.Controllers;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
+using SFA.DAS.CommitmentsV2.Application.Commands.ValidateDraftApprenticeshipDetails;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetOverlappingApprenticeshipDetails;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CommitmentsV2.Api.UnitTests.Controllers.OverlappingTrainingDateRequestControllerTests
+{
+    public class ValidateUlnOverlapOnStartDateTests
+    {
+        private ValidateUlnOverlapOnStartDateFixture _fixture;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _fixture = new ValidateUlnOverlapOnStartDateFixture();
+        }
+
+        [Test]
+        public async Task ValidateUlnOverlapOnStartDate_VerifyQuerySent()
+        {
+            await _fixture.ValidateUlnOverlapOnStartDate();
+            _fixture.VerifyQuerySent();
+        }
+
+        private class ValidateUlnOverlapOnStartDateFixture
+        {
+            private readonly Mock<IMediator> _mediator;
+            private readonly OverlappingTrainingDateRequestController _controller;
+
+            private readonly Fixture _autoFixture;
+            public const int ProviderId = 1;
+            public string Uln;
+            public string StartDate;
+            public string EndDate;
+
+            public ValidateUlnOverlapOnStartDateFixture()
+            {
+                _mediator = new Mock<IMediator>();
+                _autoFixture = new Fixture();
+
+                Uln = "9235610906";
+                StartDate = "Jan 2022";
+                EndDate = "Dec 2022";
+
+                var queryResult = _autoFixture.Create<ValidateUlnOverlapOnStartDateQueryResult>();
+                _mediator
+                    .Setup(x => x.Send(It.IsAny<ValidateUlnOverlapOnStartDateQuery>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(queryResult);
+
+                _controller = new OverlappingTrainingDateRequestController(_mediator.Object);
+            }
+
+            public async Task ValidateUlnOverlapOnStartDate()
+            {
+                await _controller.ValidateUlnOverlapOnStartDate(ProviderId, Uln, StartDate, EndDate);
+            }
+
+            public void VerifyQuerySent()
+            {
+                _mediator.Verify(
+                    m => m.Send(
+                        It.Is<ValidateUlnOverlapOnStartDateQuery>(p => p.ProviderId == ProviderId
+                        && p.Uln == Uln),
+                        It.IsAny<CancellationToken>()), Times.Once);
+            }
+        }
+    }
+}
