005bae692383d8a17dbc443e6b80b39de0fbbe84
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2022-09-06 09:20:51 +0100

    oltd request rejected email changes

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/OverlappingTrainingDateRequest/OverlappingTrainingDateRequestRejectedEventHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/OverlappingTrainingDateRequest/OverlappingTrainingDateRequestRejectedEventHandler.cs
@@ -3,14 +3,11 @@ using Microsoft.Extensions.Logging;
 using NServiceBus;
 using SFA.DAS.CommitmentsV2.Configuration;
 using SFA.DAS.CommitmentsV2.Data;
-using SFA.DAS.CommitmentsV2.Data.Extensions;
 using SFA.DAS.CommitmentsV2.Messages.Commands;
 using SFA.DAS.CommitmentsV2.Messages.Events;
-using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.Encoding;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,30 +15,28 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers.OverlappingTrainin
 {
     public class OverlappingTrainingDateRequestRejectedEventHandler : IHandleMessages<OverlappingTrainingDateRequestRejectedEvent>
     {
-        private readonly ILogger<ApprenticeshipPausedEventHandler> _logger;
+        private readonly ILogger<OverlappingTrainingDateRequestRejectedEventHandler> _logger;
         private readonly Lazy<ProviderCommitmentsDbContext> _dbContext;
-        private readonly IEncodingService _encodingService;
         private readonly CommitmentsV2Configuration _commitmentsV2Configuration;
 
-        public OverlappingTrainingDateRequestRejectedEventHandler(Lazy<ProviderCommitmentsDbContext> dbContext, ILogger<ApprenticeshipPausedEventHandler> logger,
-            IEncodingService encodingService, CommitmentsV2Configuration commitmentsV2Configuration)
+        public OverlappingTrainingDateRequestRejectedEventHandler(Lazy<ProviderCommitmentsDbContext> dbContext, ILogger<OverlappingTrainingDateRequestRejectedEventHandler> logger,
+        CommitmentsV2Configuration commitmentsV2Configuration)
         {
             _dbContext = dbContext;
             _logger = logger;
-            _encodingService = encodingService;
             _commitmentsV2Configuration = commitmentsV2Configuration;
         }
         public async Task Handle(OverlappingTrainingDateRequestRejectedEvent message, IMessageHandlerContext context)
         {
             _logger.LogInformation($"Received {nameof(OverlappingTrainingDateRequestRejectedEvent)} for overlapping training date request {message.OverlappingTrainingDateRequestId}");
 
-          var oltd =  await _dbContext.Value.OverlappingTrainingDateRequests
-                 .Include(r => r.DraftApprenticeship)
-                 .ThenInclude(d => d.Cohort)
-                 .Include(r => r.PreviousApprenticeship)
-                 .ThenInclude(a => a.Cohort)
-                 .SingleOrDefaultAsync(c => c.Id == message.OverlappingTrainingDateRequestId
-                , CancellationToken.None);
+            var oltd = await _dbContext.Value.OverlappingTrainingDateRequests
+                   .Include(r => r.DraftApprenticeship)
+                   .ThenInclude(d => d.Cohort)
+                   .Include(r => r.PreviousApprenticeship)
+                   .ThenInclude(a => a.Cohort)
+                   .SingleOrDefaultAsync(c => c.Id == message.OverlappingTrainingDateRequestId
+                  , CancellationToken.None);
             var emailToProviderCommand = BuildEmailToProviderCommand(oltd);
 
             await context.Send(emailToProviderCommand, new SendOptions());
@@ -49,14 +44,12 @@ namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers.OverlappingTrainin
 
         private SendEmailToProviderCommand BuildEmailToProviderCommand(Models.OverlappingTrainingDateRequest oltd)
         {
-            var sendEmailToProviderCommand = new SendEmailToProviderCommand(apprenticeship.Cohort.ProviderId,
-                "ProviderApprenticeshipPauseNotification",
+            var sendEmailToProviderCommand = new SendEmailToProviderCommand(oltd.DraftApprenticeship.Cohort.ProviderId,
+                "OverlapTrainingDateRequestRejected",
                       new Dictionary<string, string>
                       {
-                                  {"EMPLOYER", apprenticeship.Cohort.AccountLegalEntity.Name},
-                                  {"APPRENTICE", $"{apprenticeship.FirstName} {apprenticeship.LastName}"},
-                          {"DATE", apprenticeship.PauseDate?.ToString("dd/MM/yyyy")},
-                                  {"URL", $"{_commitmentsV2Configuration.ProviderCommitmentsBaseUrl}/{apprenticeship.Cohort.ProviderId}/apprentices/{_encodingService.Encode(apprenticeship.Id, EncodingType.ApprenticeshipId)}"}
+                          {"CohortReference", oltd.DraftApprenticeship.Cohort.Reference},
+                          {"URL", $"{_commitmentsV2Configuration.ProviderCommitmentsBaseUrl}{oltd.DraftApprenticeship.Cohort.ProviderId}/unapproved/{oltd.DraftApprenticeship.Cohort.Reference}/details" }
                       });
 
             return sendEmailToProviderCommand;
