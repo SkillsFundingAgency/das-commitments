9fec5b22adc0a727dfa2a9586d0a8e53f81f719b
Paul Graham <paul.graham@coprime.co.uk>
2022-09-08 09:25:34 +0100

    Added View for Dashboard

---
+++ src/SFA.DAS.Commitments.Database/Views/ApprenticeshipsCreatedSummary.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [DashboardReporting].[ApprenticeshipsCreatedSummary]
+	AS 
+SELECT 
+	CASE WHEN TransferApprovalActionedOn IS NOT NULL THEN C.TransferApprovalActionedOn
+	ELSE C.EmployerAndProviderApprovedOn 
+	END AS ApprenticeshipApprovedOm, 
+	A.Id, 
+	ISNULL(A.DeliveryModel,0) AS DeliveryModel, 
+	ISNULL(PL.IsAccelerated, 0) AS AcceleratedDelivery
+FROM Apprenticeship A 
+INNER JOIN Commitment C ON A.CommitmentId = C.Id
+LEFT JOIN ApprenticeshipPriorLearning PL ON A.Id = PL.ApprenticeshipId
+WHERE A.IsApproved = 1
+GO
