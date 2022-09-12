912ae70033ffe30e390d3685663d2da4e85f8eb8
Michael Young <mick.young1981@googlemail.com>
2022-09-12 16:34:10 +0100

    Merge

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/DraftApprenticeshipCommandBase.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/DraftApprenticeshipCommandBase.cs
@@ -14,6 +14,7 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands
         public DateTime? EmploymentEndDate { get; set; }
         public int? Cost { get; set; }
         public DateTime? StartDate { get; set; }
+        public DateTime? ActualStartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public string OriginatorReference { get; set; }
         public Guid? ReservationId { get; set; }
