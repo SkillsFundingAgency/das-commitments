673ff8440eef681e7d78d61aa258d66cc6841e0e
ratheeshri1984 <ratheeshri1984@gmail.com>
2022-09-23 10:53:08 +0100

    MAP-103-Try Catch Removed

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/RemoveAccountLegalEntity/RemoveAccountLegalEntityCommandHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/RemoveAccountLegalEntity/RemoveAccountLegalEntityCommandHandler.cs
@@ -20,39 +20,29 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.RemoveAccountLegalEntity
 
         protected override async Task Handle(RemoveAccountLegalEntityCommand request, CancellationToken cancellationToken)
         {
-            try
+            var account = await _db.Value.Accounts.SingleAsync(a => a.Id == request.AccountId, cancellationToken);
+            var accountLegalEntity = await _db.Value.AccountLegalEntities
+                .IgnoreQueryFilters()
+                .SingleAsync(ale => ale.Id == request.AccountLegalEntityId, cancellationToken);
+
+            account.RemoveAccountLegalEntity(accountLegalEntity, request.Removed);
+
+            if (accountLegalEntity.Deleted != null)
             {
-                var account = await _db.Value.Accounts.SingleAsync(a => a.Id == request.AccountId, cancellationToken);
-                var accountLegalEntity = await _db.Value.AccountLegalEntities
-                    .IgnoreQueryFilters()
-                    .SingleAsync(ale => ale.Id == request.AccountLegalEntityId, cancellationToken);
+                var cohorts = await _db.Value.Cohorts.Include(c => c.Apprenticeships)
+                    .Where(c => c.AccountLegalEntityId == request.AccountLegalEntityId)
+                    .ToListAsync();
 
-                account.RemoveAccountLegalEntity(accountLegalEntity, request.Removed);
-
-
-                if (accountLegalEntity.Deleted != null)
+                foreach (var cohort in cohorts)
                 {
-                    var cohorts = await _db.Value.Cohorts.Include(c => c.Apprenticeships)
-                        .Where(c => c.AccountLegalEntityId == request.AccountLegalEntityId)
-                        .ToListAsync();
-
-                    foreach (var cohort in cohorts)
+                    if (cohort.Apprenticeships.Any(x => x.IsApproved))
                     {
-                        if (cohort.Apprenticeships.Any(x => x.IsApproved))
-                        {
-                            throw new DomainException(nameof(cohort), $"Cohort already has an approved apprenticeship");
-                        }
-
-                        cohort.Delete(cohort.WithParty, new UserInfo());
+                        throw new DomainException(nameof(cohort), $"Cohort already has an approved apprenticeship");
                     }
+
+                    cohort.Delete(cohort.WithParty, new UserInfo());
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
         }
     }
 }
