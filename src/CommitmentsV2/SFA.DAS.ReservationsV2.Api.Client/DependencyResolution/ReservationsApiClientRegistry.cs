daa313807184912259806e9bac59bc69c79c7471
ratheeshri1984 <ratheeshri1984@gmail.com>
2022-11-03 18:35:00 +0000

    MAP-45-Moved reservation config into the Commitments

--- src/CommitmentsV2/SFA.DAS.ReservationsV2.Api.Client/DependencyResolution/ReservationsApiClientRegistry.cs
+++ src/CommitmentsV2/SFA.DAS.ReservationsV2.Api.Client/DependencyResolution/ReservationsApiClientRegistry.cs
@@ -1,10 +1,4 @@
-﻿using System.Net.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using SFA.DAS.Http;
-using SFA.DAS.Reservations.Api.Client.DependencyResolution;
-using SFA.DAS.Reservations.Api.Types;
-using SFA.DAS.Reservations.Api.Types.Configuration;
+﻿using SFA.DAS.Reservations.Api.Types;
 using StructureMap;
 
 namespace SFA.DAS.ReservationsV2.Api.Client.DependencyResolution
@@ -13,52 +7,8 @@ namespace SFA.DAS.ReservationsV2.Api.Client.DependencyResolution
     {
         public ReservationsApiClientRegistry()
         {
-            For<IReservationsApiClient>().Use(ctx => CreateClient(ctx)).Singleton();
+            For<IReservationsApiClient>().Use(c => c.GetInstance<IReservationsApiClientFactory>().CreateClient()).Singleton();
+            For<IReservationsApiClientFactory>().Use<ReservationsApiClientFactory>();
         } 
-
-        private IReservationsApiClient CreateClient(IContext ctx)
-        {
-            var config = GetConfig(ctx);
-            var httpClient = CreateHttpClient(ctx, config);
-            var restHttpClient = new RestHttpClient(httpClient);
-            var httpHelper = new HttpHelper(restHttpClient, ctx.GetInstance<ILogger<ReservationsApiClient>>());
-            return new ReservationsApiClient(config, httpHelper);
-        }
-
-        private HttpClient CreateHttpClient(IContext ctx, ReservationsClientApiConfiguration config)
-        {
-            if (config.UseStub)
-            {
-                return new HttpClient();
-            }
-
-            var loggerFactory = ctx.GetInstance<ILoggerFactory>();
-
-            IHttpClientFactory httpClientFactory;
-
-            if (IsClientCredentialConfiguration(config.ClientId, config.ClientSecret, config.Tenant))
-            {
-                var activeDirectoryConfig = new ReservationsClientApiConfigurationADAdapter(config);
-                httpClientFactory = new AzureActiveDirectoryHttpClientFactory(activeDirectoryConfig, loggerFactory);
-            }
-            else
-            {
-                var miConfig = new ReservationsClientApiConfigurationMIAdapter(config);
-                httpClientFactory = new ManagedIdentityHttpClientFactory(miConfig, loggerFactory);
-            }
-
-            return httpClientFactory.CreateHttpClient();
-        }
-
-        private bool IsClientCredentialConfiguration(string clientId, string clientSecret, string tenant)
-        {
-            return !string.IsNullOrWhiteSpace(clientId) && !string.IsNullOrWhiteSpace(clientSecret) && !string.IsNullOrWhiteSpace(tenant);
-        }
-        private static ReservationsClientApiConfiguration GetConfig(IContext context)
-        {
-            var configuration = context.GetInstance<IConfiguration>();
-            var configSection = configuration.GetSection(ConfigurationKeys.ReservationsClientApiConfiguration);
-            return configSection.Get<ReservationsClientApiConfiguration>();
-        }
     }
 }
