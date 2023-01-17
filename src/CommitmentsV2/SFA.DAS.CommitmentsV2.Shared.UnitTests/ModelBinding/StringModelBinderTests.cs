f9a75162203d7360ba767a36bcd6acc5e404581b
chrisfoster76 <chrisfoster186@googlemail.com>
2023-01-17 16:10:51 +0000

    Adds a custom string model binder which trims input and removes tabs

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Shared.UnitTests/ModelBinding/StringModelBinderTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Shared.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.CommitmentsV2.Shared.UnitTests.ModelBinding
+{
+    [TestFixture]
+    public class StringModelBinderTests
+    {
+        private StringModelBinder _binder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _binder = new StringModelBinder(new SimpleTypeModelBinder(typeof(string), Mock.Of<ILoggerFactory>()));
+        }
+
+        [TestCase("test", "test")]
+        [TestCase(" test", "test")]
+        [TestCase("test", "test")]
+        [TestCase(" test ", "test")]
+        [TestCase("test this", "test this")]
+        [TestCase("test\tthis", "test this")]
+        [TestCase("\ttest ", "test")]
+        [TestCase("\t test \t", "test")]
+        public async Task BindModel_Produces_Expected_Result(string inputValue, string expectedResult)
+        {
+            var formCollection = new FormCollection(
+                new Dictionary<string, StringValues>()
+                {
+                    { "String", new StringValues(inputValue) }
+                });
+            var vp = new FormValueProvider(BindingSource.Form, formCollection, CultureInfo.CurrentCulture);
+
+            var context = GetBindingContext(vp, typeof(string));
+
+            await _binder.BindModelAsync(context);
+
+            var resultModel = context.Result.Model as string;
+
+            Assert.AreEqual(expectedResult, resultModel);
+
+        }
+        private static DefaultModelBindingContext GetBindingContext(IValueProvider valueProvider, Type modelType)
+        {
+            var metadataProvider = new EmptyModelMetadataProvider();
+            var bindingContext = new DefaultModelBindingContext
+            {
+                ModelMetadata = metadataProvider.GetMetadataForType(modelType),
+                ModelName = modelType.Name,
+                ModelState = new ModelStateDictionary(),
+                ValueProvider = valueProvider,
+            };
+            return bindingContext;
+        }
+    }
+}
