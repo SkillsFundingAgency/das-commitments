44aef7a6b062ef059dda55a868a7e2e6bfc194c7
Alasdair Reid <ali.reid@live.co.uk>
2023-02-20 08:45:10 +0000

    Database columns necessary for new view

--- src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipPriorLearning.sql
+++ src/SFA.DAS.Commitments.Database/Tables/ApprenticeshipPriorLearning.sql
@@ -4,8 +4,12 @@
 	[DurationReducedBy] INT NULL,
 	[PriceReducedBy] INT NULL,
 	[IsAccelerated] AS (CASE WHEN [DurationReducedBy] >= 12 THEN CONVERT([BIT], 1) WHEN DurationReducedBy IS NULL THEN NULL ELSE CONVERT([BIT], 0) END), 
+	[DurationReducedByHours] [decimal](18, 2) NULL,
+	[WeightageReducedBy] [decimal](18, 2) NULL,
+	[Qualification] [nchar](1000) NULL,
+	[Reason] [text] NULL,
+	[UpdatedDate] [datetime] NULL,
     CONSTRAINT [PK_ApprenticeshipRecognisePriorLearning] PRIMARY KEY ([ApprenticeshipId]),
 	CONSTRAINT [FK_ApprenticeshipRecognisePriorLearning_ApprenticeshipId] FOREIGN KEY ([ApprenticeshipId]) REFERENCES [Apprenticeship]([Id]) ON DELETE CASCADE
 )
 GO
-
