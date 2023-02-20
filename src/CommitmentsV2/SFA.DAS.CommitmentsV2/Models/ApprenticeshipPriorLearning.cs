02b8a4baa918019312b4bbc31e0fc797e7ebcebb
Alasdair Reid <ali.reid@live.co.uk>
2023-02-20 08:48:14 +0000

    Model to suit updated schema

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprenticeshipPriorLearning.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprenticeshipPriorLearning.cs
@@ -7,5 +7,10 @@ namespace SFA.DAS.CommitmentsV2.Models
         public ApprenticeshipBase Apprenticeship { get; private set; }
         public int? DurationReducedBy { get; set; }
         public int? PriceReducedBy { get; set; }
+        public double? DurationReducedByHours { get; set; }
+        public double? WeightageReducedBy { get; set; }
+        public string Qualification { get; set; }
+        public string Reason { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
