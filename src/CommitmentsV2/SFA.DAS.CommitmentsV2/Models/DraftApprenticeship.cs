be45b8da6f68e16559006504d02e506527a68025
Alasdair Reid <ali.reid@live.co.uk>
2023-03-07 13:28:17 +0000

    Validation again

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/DraftApprenticeship.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/DraftApprenticeship.cs
@@ -310,11 +310,11 @@ namespace SFA.DAS.CommitmentsV2.Models
             {
                 errors.Add(new DomainError("DurationReducedByHours", "You must enter the number of hours"));
             }
-            else if (durationReducedByHours.Value < 0)
+            if (durationReducedByHours.Value < 0)
             {
                 errors.Add(new DomainError("DurationReducedByHours", "The number can't be negative"));
             }
-            else if (durationReducedByHours.Value > 999)
+            if (durationReducedByHours.Value > 999)
             {
                 errors.Add(new DomainError("DurationReducedByHours", "The number of hours must be 999 or less"));
             }
@@ -323,11 +323,11 @@ namespace SFA.DAS.CommitmentsV2.Models
             {
                 errors.Add(new DomainError("ReducedPrice", "You must enter a price"));
             }
-            else if (priceReduction.Value < 0)
+            if (priceReduction.Value < 0)
             {
                 errors.Add(new DomainError("ReducedPrice", "The price can't be negative"));
             }
-            else if (priceReduction.Value > Constants.MaximumApprenticeshipCost)
+            if (priceReduction.Value > Constants.MaximumApprenticeshipCost)
             {
                 errors.Add(new DomainError("ReducedPrice", "The price must be 100,000 or less"));
             }
@@ -336,16 +336,16 @@ namespace SFA.DAS.CommitmentsV2.Models
             {
                 errors.Add(new DomainError("WeightageReducedBy", "You must enter a percentage"));
             }
-            else if (weightageReducedBy.Value < 0)
+            if (weightageReducedBy.Value < 0)
             {
                 errors.Add(new DomainError("WeightageReducedBy", "The percentage can't be negative"));
             }
-            else if (weightageReducedBy.Value > 99)
+            if (weightageReducedBy.Value > 99)
             {
                 errors.Add(new DomainError("WeightageReducedBy", "The percentage can't be more than 99"));
             }
 
-            if (qualificationsForRplReduction?.Trim().Length > 1000)
+            if (qualificationsForRplReduction.Trim().Length > 1000)
             {
                 errors.Add(new DomainError("QualificationsForRplReduction", "You can't exceed 1000 characters for qualifications"));
             }
@@ -354,7 +354,7 @@ namespace SFA.DAS.CommitmentsV2.Models
             {
                 errors.Add(new DomainError("ReasonForRplReduction", "You must specify a reason"));
             }
-            else if (reasonForRplReduction.Trim().Length > 1000)
+            if (reasonForRplReduction.Trim().Length > 1000)
             {
                 errors.Add(new DomainError("ReasonForRplReduction", "You can't exceed 1000 characters for a reason"));
             }
