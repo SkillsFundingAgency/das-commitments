2e2d2d6d969a689646d218e3eb9a5d13d2cbad67
chrisfoster76 <chrisfoster186@googlemail.com>
2023-03-07 15:10:54 +0000

    Adjusts timer trigger

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Jobs/ScheduledJobs/DataLockUpdaterJobs.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Jobs/ScheduledJobs/DataLockUpdaterJobs.cs
@@ -16,7 +16,7 @@ namespace SFA.DAS.CommitmentsV2.Jobs.ScheduledJobs
             _dataLockUpdaterServicer = dataLockUpdaterServicer;
         }
 
-        public async Task Update([TimerTrigger("0 */30 * * * *", RunOnStartup = true)] TimerInfo timer)
+        public async Task Update([TimerTrigger("*/30 * * * * *", RunOnStartup = true)] TimerInfo timer)
         {
             _logger.LogInformation($"DataLockUpdaterJobs - Started{(timer?.IsPastDue ?? false ? " later than expected" : string.Empty)}");
 
