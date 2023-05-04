cee63340323a67cb02f8f939da4506544bfada8f
Paul Graham <paul.graham@coprime.co.uk>
2023-05-04 16:15:44 +0100

    added controller tests

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Responses/GetDraftApprenticeshipPriorLearningSummaryResponse.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Responses/GetDraftApprenticeshipPriorLearningSummaryResponse.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using SFA.DAS.CommitmentsV2.Types;
-
-namespace SFA.DAS.CommitmentsV2.Api.Types.Responses
+﻿namespace SFA.DAS.CommitmentsV2.Api.Types.Responses
 {
     public sealed class GetDraftApprenticeshipPriorLearningSummaryResponse
     {
