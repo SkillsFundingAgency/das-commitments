ed20498a47edbee45fc944b8460fdf546fe30121
Paul Graham <paul.graham@coprime.co.uk>
2023-07-20 15:42:12 +0100

    Added minimum price reduction as config value

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/PriorLearningData/RecognisePriorLearningDataHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/PriorLearningData/RecognisePriorLearningDataHandler.cs
@@ -5,6 +5,7 @@ using SFA.DAS.CommitmentsV2.Data.Extensions;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using SFA.DAS.CommitmentsV2.Configuration;
 
 namespace SFA.DAS.CommitmentsV2.Application.Commands.PriorLearningData
 {
@@ -12,12 +13,15 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.PriorLearningData
     {
         private readonly ILogger<RecognisePriorLearningDataHandler> _logger;
         private readonly Lazy<ProviderCommitmentsDbContext> _dbContext;
+        private readonly RplSettingsConfiguration _config;
 
         public RecognisePriorLearningDataHandler(
             Lazy<ProviderCommitmentsDbContext> dbContext,
+            RplSettingsConfiguration config,
             ILogger<RecognisePriorLearningDataHandler> logger)
         {
             _dbContext = dbContext;
+            _config = config;
             _logger = logger;
         }
 
@@ -25,7 +29,7 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.PriorLearningData
         {
             var apprenticeship = await _dbContext.Value.GetDraftApprenticeshipAggregate(command.CohortId, command.ApprenticeshipId, cancellationToken);
 
-            apprenticeship.SetPriorLearningData(command.TrainingTotalHours, command.DurationReducedByHours, command.IsDurationReducedByRpl, command.DurationReducedBy, command.PriceReducedBy);
+            apprenticeship.SetPriorLearningData(command.TrainingTotalHours, command.DurationReducedByHours, command.IsDurationReducedByRpl, command.DurationReducedBy, command.PriceReducedBy, _config.MinimumPriceReduction);
 
             _logger.LogInformation($"Set PriorLearning data draft Apprenticeship:{command.ApprenticeshipId}");
         }
