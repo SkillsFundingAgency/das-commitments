b1f2111d96004e5e0251bbcca7a2e9631d34312d
Paul Graham <paul.graham@coprime.co.uk>
2023-08-31 13:46:52 +0100

    fix layout issue

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/FileUploadCohortLog.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/FileUploadCohortLog.cs
@@ -4,10 +4,10 @@ namespace SFA.DAS.CommitmentsV2.Models
 {
     public class FileUploadCohortLog
     {
-		public long Id { get; set; } 
-		public long FileUploadLogId { get; set; } 
-		public long CommitmentId { get; set; } 
-		public int RowCount { get; set; }
+        public long Id { get; set; } 
+        public long FileUploadLogId { get; set; } 
+        public long CommitmentId { get; set; } 
+        public int RowCount { get; set; }
         public FileUploadLog FileUploadLog { get; set; }
     }
 }
--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/FileUploadLog.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/FileUploadLog.cs
@@ -9,16 +9,16 @@ namespace SFA.DAS.CommitmentsV2.Models
         {
             CohortLogs = new List<FileUploadCohortLog>();
         }
-		public long Id { get; set; } 
-		public long? ProviderId { get; set; } 
-		public string FileName { get; set; } 
-		public int? RplCount { get; set; } 
-		public int? RowCount { get; set; } 
-		public string ProviderAction { get; set; } 
-		public string FileContent { get; set; } 
-		public DateTime CreatedOn { get; set; } 
-		public DateTime? CompletedOn { get; set; } 
-		public string Error { get; set; }
+        public long Id { get; set; } 
+        public long? ProviderId { get; set; } 
+        public string FileName { get; set; } 
+        public int? RplCount { get; set; } 
+        public int? RowCount { get; set; } 
+        public string ProviderAction { get; set; } 
+        public string FileContent { get; set; } 
+        public DateTime CreatedOn { get; set; } 
+        public DateTime? CompletedOn { get; set; } 
+        public string Error { get; set; }
         public virtual ICollection<FileUploadCohortLog> CohortLogs { get; set; }
     }
 }
