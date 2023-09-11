0de61df5c34f0a23b54d53ac3a40eefb83eef1d9
Paul Graham <paul.graham@coprime.co.uk>
2023-09-11 15:17:38 +0100

    correct foreign key constraint

--- src/SFA.DAS.Commitments.Database/Tables/FileUploadCohortLog.sql
+++ src/SFA.DAS.Commitments.Database/Tables/FileUploadCohortLog.sql
@@ -4,7 +4,9 @@
     [FileUploadLogId] BIGINT NOT NULL,
     [CommitmentId] BIGINT NOT NULL,
     [RowCount] INT NULL,
-	CONSTRAINT [FK_FileUploadCohortLog_FileUploadCohortLog] FOREIGN KEY([Id]) REFERENCES [dbo].[FileUploadCohortLog] ([Id]),
+	CONSTRAINT [FK_FileUploadCohortLog_FileUploadLog] FOREIGN KEY([FileUploadLogId]) REFERENCES [dbo].[FileUploadCohortLog] ([Id]),
 	CONSTRAINT [FK_FileUploadCohortLog_Commitment] FOREIGN KEY([CommitmentId]) REFERENCES [dbo].[Commitment] ([Id]),
 )
+GO
+CREATE NONCLUSTERED INDEX [IDX_FileUploadCohortLog_FileUploadLogId] ON [dbo].[FileUploadCohortLog] ([FileUploadLogId])
 GO
