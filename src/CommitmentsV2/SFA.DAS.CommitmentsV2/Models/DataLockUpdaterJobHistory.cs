f91fb598b5c59c31ecdc3d4405fc905ebbdf32a1
chrisfoster76 <chrisfoster186@googlemail.com>
2023-09-21 15:24:29 +0100

    New history objects

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/DataLockUpdaterJobHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.CommitmentsV2.Models;
+
+public class DataLockUpdaterJobHistory
+{
+    public long Id { get; set; }
+    public DateTime StartedOn { get; set; }
+    public DateTime FinishedOn { get; set; }
+    public long FromEventId { get; set; }
+    public int SkippedCount { get; set; }
+    public int DuplicateCount { get; set; }
+    public int ExpiredCount { get; set; }
+    public int PagesRemaining { get; set; }
+}
---
+++ src/SFA.DAS.Commitments.Database/Tables/DataLockUpdaterJobHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DataLockUpdaterJobHistory]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[StartedOn] DATETIME NOT NULL,
+	[FinishedOn] DATETIME NOT NULL,
+	[FromEventId] BIGINT NOT NULL,
+	[SkippedCount] INT NOT NULL,
+	[DuplicateCount] INT NOT NULL,
+	[ExpiredCount] INT NOT NULL,
+	[PagesRemaining] INT NOT NULL
+)
