4e472bb2ed489659c4d19629a6fbbf7765562d53
Deven Shah <devenshah1974@gmail.com>
2023-10-19 16:21:02 +0100

    CSP-1072 Added FindLearners stored procedure

---
+++ src/SFA.DAS.Commitments.Database/StoredProcedures/FindLearner.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[FindLearner]
+	@firstName NVARCHAR(100),
+	@lastName NVARCHAR(100),
+	@dateOfBirth DATETIME
+AS
+BEGIN
+	SELECT 
+		[ApprenticeshipId]
+		,[FirstName]
+		,[LastName]
+		,[DateOfBirth]
+		,[ULN]
+		,[TrainingCode]
+		,[TrainingCourseVersion]
+		,[TrainingCourseVersionConfirmed]
+		,[TrainingCourseOption]
+		,[StandardUId]
+		,[StartDate]
+		,[EndDate]
+		,[CreatedOn]
+		,[UpdatedOn]
+		,[StopDate]
+		,[PauseDate]
+		,[CompletionDate]
+		,[UKPRN]
+		,[LearnRefNumber]
+		,[PaymentStatus]
+		,[EmployerAccountId]
+		,[EmployerName]
+	FROM [dbo].[GetLearners] (NULL)
+	WHERE 
+		[FirstName] = @firstName
+	AND
+		[LastName] = @lastName
+	AND
+		[DateOfBirth] = @dateOfBirth
+END
