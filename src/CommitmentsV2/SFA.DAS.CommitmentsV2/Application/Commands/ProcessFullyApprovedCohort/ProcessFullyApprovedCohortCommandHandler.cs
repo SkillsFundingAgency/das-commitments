1e41c0e9d70328085dc42f10b4d2efd86cc7edf5
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-21 15:15:41 +0000

    Fix following merge

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/ProcessFullyApprovedCohort/ProcessFullyApprovedCohortCommandHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/ProcessFullyApprovedCohort/ProcessFullyApprovedCohortCommandHandler.cs
@@ -12,6 +12,7 @@ using SFA.DAS.CommitmentsV2.Extensions;
 using SFA.DAS.CommitmentsV2.Messages.Events;
 using SFA.DAS.CommitmentsV2.Types;
 using SFA.DAS.EAS.Account.Api.Client;
+using SFA.DAS.Encoding;
 using SFA.DAS.NServiceBus.Services;
 
 namespace SFA.DAS.CommitmentsV2.Application.Commands.ProcessFullyApprovedCohort
@@ -21,13 +22,15 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.ProcessFullyApprovedCohort
         private readonly IAccountApiClient _accountApiClient;
         private readonly Lazy<ProviderCommitmentsDbContext> _db;
         private readonly IEventPublisher _eventPublisher;
+        private readonly IEncodingService _encodingService;
         private readonly ILogger<ProcessFullyApprovedCohortCommandHandler> _logger;
 
-        public ProcessFullyApprovedCohortCommandHandler(IAccountApiClient accountApiClient, Lazy<ProviderCommitmentsDbContext> db, IEventPublisher eventPublisher, ILogger<ProcessFullyApprovedCohortCommandHandler> logger)
+        public ProcessFullyApprovedCohortCommandHandler(IAccountApiClient accountApiClient, Lazy<ProviderCommitmentsDbContext> db, IEventPublisher eventPublisher, IEncodingService encodingService, ILogger<ProcessFullyApprovedCohortCommandHandler> logger)
         {
             _accountApiClient = accountApiClient;
             _db = db;
             _eventPublisher = eventPublisher;
+            _encodingService = encodingService;
             _logger = logger;
         }
 
@@ -82,7 +85,8 @@ namespace SFA.DAS.CommitmentsV2.Application.Commands.ProcessFullyApprovedCohort
                     ActualStartDate = a.ActualStartDate,
                     IsOnFlexiPaymentPilot = a.IsOnFlexiPaymentPilot,
                     FirstName = a.FirstName,
-                    LastName = a.LastName
+                    LastName = a.LastName,
+                    ApprenticeshipHashedId = _encodingService.Encode(a.Id, EncodingType.ApprenticeshipId),
                 })
                 .ToListAsync(cancellationToken);
 
