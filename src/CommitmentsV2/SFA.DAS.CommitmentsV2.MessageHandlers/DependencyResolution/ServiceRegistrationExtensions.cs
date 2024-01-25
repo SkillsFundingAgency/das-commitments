23195ea5a9982e2de3be8049905c8244a6390c87
Paul Graham <paul.graham@coprime.co.uk>
2024-01-25 16:24:17 +0000

    remove mediatr

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/DependencyResolution/ServiceRegistrationExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/DependencyResolution/ServiceRegistrationExtensions.cs
@@ -1,19 +1,19 @@
-﻿using System.Reflection;
-using MediatR;
+﻿using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Authorization.Features.DependencyResolution.Microsoft;
-using SFA.DAS.CommitmentsV2.Application.Commands.AddHistory;
+using SFA.DAS.CommitmentsV2.Application.Commands.AcceptApprenticeshipUpdates;
 using SFA.DAS.CommitmentsV2.Configuration;
 using SFA.DAS.CommitmentsV2.Data;
 using SFA.DAS.CommitmentsV2.DependencyResolution;
 using SFA.DAS.CommitmentsV2.Domain.Interfaces;
+using SFA.DAS.CommitmentsV2.MessageHandlers.CommandHandlers;
 using SFA.DAS.CommitmentsV2.Services;
 using SFA.DAS.CommitmentsV2.Shared.DependencyInjection;
 using SFA.DAS.CommitmentsV2.Startup;
 using SFA.DAS.Encoding;
-using SFA.DAS.PAS.Account.Api.ClientV2.Configuration;
 using SFA.DAS.UnitOfWork.DependencyResolution.Microsoft;
 using SFA.DAS.UnitOfWork.EntityFrameworkCore.DependencyResolution.Microsoft;
 using SFA.DAS.UnitOfWork.NServiceBus.Features.ClientOutbox.DependencyResolution.Microsoft;
@@ -37,9 +37,13 @@ public static class ServiceRegistrationExtensions
                 .AddEntityFrameworkUnitOfWork<ProviderCommitmentsDbContext>();
 
             services.AddDatabaseRegistration();
-            services.AddMediatR(cfg =>
-                cfg.RegisterServicesFromAssemblies(typeof(AddHistoryCommand).GetTypeInfo().Assembly));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            //services.AddTransient<IMediator, Mediator>();
+            //services.AddMediatR(serviceConfiguration => serviceConfiguration.RegisterServicesFromAssemblies(
+            //    typeof(ApprenticeshipResendInvitationCommandHandler).Assembly,
+            //    typeof(AcceptApprenticeshipUpdatesCommand).Assembly)
+            //);
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            //services.
             services.AddNServiceBusClientUnitOfWork();
 
             services.AddEmployerAccountServices(context.Configuration);
@@ -56,8 +60,31 @@ public static class ServiceRegistrationExtensions
             // todo wireup IPasAccountApiClient via outerAPI
             //services.AddTransient<IPasAccountApiClient, ...);
 
+            services.AddDefaultMessageHandlerServices();
+
         });
 
         return hostBuilder;
     }
+
+    public static IServiceCollection AddDefaultMessageHandlerServices(this IServiceCollection services)
+    {
+        services.AddTransient<IDbContextFactory, SynchronizedDbContextFactory>();
+        services.AddTransient<IFundingCapService, FundingCapService>();
+        services.AddTransient<ITrainingProgrammeLookup, TrainingProgrammeLookup>();
+        services.AddTransient<ITopicClientFactory, TopicClientFactory>();
+        services.AddTransient<ILegacyTopicMessagePublisher>(s =>
+        {
+            var config = s.GetService<CommitmentsV2Configuration>();
+            return new LegacyTopicMessagePublisher(s.GetService<ITopicClientFactory>(),
+                s.GetService<ILogger<LegacyTopicMessagePublisher>>(), config.MessageServiceBusConnectionString);
+        });
+        services.AddTransient<IEmailOptionalService, EmailOptionalService>();
+        services.AddTransient<IFilterOutAcademicYearRollOverDataLocks, FilterOutAcademicYearRollOverDataLocks>();
+
+        return services;
+    }
+
+
+
 }
