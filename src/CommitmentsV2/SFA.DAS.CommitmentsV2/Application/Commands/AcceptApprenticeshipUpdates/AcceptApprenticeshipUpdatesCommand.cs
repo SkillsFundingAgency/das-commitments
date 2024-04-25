8f4e026e965ae2594b85b745b5c2000c7d398201
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-04-25 15:34:34 +0100

    Another namespace reverted

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/AcceptApprenticeshipUpdates/AcceptApprenticeshipUpdatesCommand.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/AcceptApprenticeshipUpdates/AcceptApprenticeshipUpdatesCommand.cs
@@ -1,25 +1,26 @@
 ﻿using MediatR;
 using SFA.DAS.CommitmentsV2.Types;
 
-namespace SFA.DAS.CommitmentsV2.Application.Commands.AcceptApprenticeshipUpdates;
-
-public class AcceptApprenticeshipUpdatesCommand : IRequest
+namespace SFA.DAS.CommitmentsV2.Application.Commands.AcceptApprenticeshipUpdates
 {
-    public long AccountId { get; set; }
-    public long ApprenticeshipId { get; set; }
-    public UserInfo UserInfo { get; set; }
-    public Party Party { get; }
-
-    public AcceptApprenticeshipUpdatesCommand()
+    public class AcceptApprenticeshipUpdatesCommand : IRequest
     {
-        Party = Party.None;
-    }
+        public long AccountId { get; set; }
+        public long ApprenticeshipId { get; set; }
+        public UserInfo UserInfo { get; set; }
+        public Party Party { get; }
 
-    public AcceptApprenticeshipUpdatesCommand(Party party, long accountId, long apprenticeshipId, UserInfo userInfo)
-    {
-        Party = party;
-        AccountId = accountId;
-        ApprenticeshipId = apprenticeshipId;
-        UserInfo = userInfo;
+        public AcceptApprenticeshipUpdatesCommand()
+        {
+            Party = Party.None;
+        }
+
+        public AcceptApprenticeshipUpdatesCommand(Party party, long accountId, long apprenticeshipId, UserInfo userInfo)
+        {
+            Party = party;
+            AccountId = accountId;
+            ApprenticeshipId = apprenticeshipId;
+            UserInfo = userInfo;
+        }
     }
 }
