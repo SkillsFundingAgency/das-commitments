043df226932d83038f2d81e171dbbb72657f5768
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-04-25 16:13:59 +0100

    Update NotAuthenticatedAuthenticationService.cs

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/Services/NotAuthenticatedAuthenticationService.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/Services/NotAuthenticatedAuthenticationService.cs
@@ -4,34 +4,36 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
-namespace SFA.DAS.CommitmentsV2.MessageHandlers.Services;
-
-/// <summary>
-/// IAuthenticationService service is required by some commandhandlers. The Concreate implementation of this service
-/// uses IHttpContext which is not availble for SFA.DAS.CommitmentsV2.MessageHandler which does not expose request
-/// endpoints.
-/// 
-/// </summary>
-[ExcludeFromCodeCoverage]
-public class NotAuthenticatedAuthenticationService : IAuthenticationService
+namespace SFA.DAS.CommitmentsV2.MessageHandlers.Services
 {
+    /// <summary>
+    /// IAuthenticationService service is required by some commandhandlers. The Concreate implementation of this service
+    /// uses IHttpContext which is not availble for SFA.DAS.CommitmentsV2.MessageHandler which does not expose request
+    /// endpoints.
+    /// 
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class NotAuthenticatedAuthenticationService : IAuthenticationService
+    {
 
-	private readonly string _exceptionMessage = "Can not authenticate events";
+        private readonly string _exceptionMessage = "Can not authenticate events";
 
-	public AuthenticationServiceType AuthenticationServiceType => AuthenticationServiceType.MessageHandler;
+        public AuthenticationServiceType AuthenticationServiceType => AuthenticationServiceType.MessageHandler;
 
-	public IEnumerable<string> GetAllUserRoles()
-	{
-		throw new NotImplementedException(_exceptionMessage);
-	}
+        public IEnumerable<string> GetAllUserRoles()
+        {
+            throw new NotImplementedException(_exceptionMessage);
+        }
 
-	public Party GetUserParty()
-	{
-		throw new NotImplementedException(_exceptionMessage);
-	}
+        public Party GetUserParty()
+        {
+            throw new NotImplementedException(_exceptionMessage);
+        }
 
-	public bool IsUserInRole(string role)
-	{
-		throw new NotImplementedException(_exceptionMessage);
-	}
+        public bool IsUserInRole(string role)
+        {
+            throw new NotImplementedException(_exceptionMessage);
+        }
+    }
+
 }
