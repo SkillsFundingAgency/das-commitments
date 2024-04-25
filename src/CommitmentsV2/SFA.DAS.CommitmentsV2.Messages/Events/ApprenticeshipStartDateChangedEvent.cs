43868e889a74dd42c238ab2a32417a31b1d2c548
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-04-25 14:37:29 +0100

    Update ApprenticeshipStartDateChangedEvent.cs

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/ApprenticeshipStartDateChangedEvent.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Messages/Events/ApprenticeshipStartDateChangedEvent.cs
@@ -1,30 +1,32 @@
 ﻿using System;
 
-namespace SFA.DAS.CommitmentsV2.Messages.Events;
-
-public class ApprenticeshipStartDateChangedEvent
+namespace SFA.DAS.CommitmentsV2.Messages.Events
 {
-	public Guid ApprenticeshipKey { get; set; }
+    public class ApprenticeshipStartDateChangedEvent
+    {
+        public Guid ApprenticeshipKey { get; set; }
 
-	public int ApprenticeshipId { get; set; }
+        public int ApprenticeshipId { get; set; }
 
-	public DateTime ActualStartDate { get; set; }
+        public DateTime ActualStartDate { get; set; }
 
-	public int EmployerAccountId { get; set; }
+        public int EmployerAccountId { get; set; }
 
-	public int ProviderId { get; set; }
+        public int ProviderId { get; set; }
 
-	public DateTime ApprovedDate { get; set; }
+        public DateTime ApprovedDate { get; set; }
 
-    public ChangeUser ProviderUser { get; set; } = new ChangeUser();
-    public ChangeUser EmployerUser { get; set; } = new ChangeUser();
+        public ChangeUser ProviderUser { get; set; } = new ChangeUser();
+        public ChangeUser EmployerUser { get; set; } = new ChangeUser();
 
-    public string Initiator { get; set; }
-}
+        public string Initiator { get; set; }
+    }
 
-public class  ChangeUser
-{
-	public string UserId { get; set; }
-	public string UserDisplayName { get; set; }
-	public string UserEmail { get; set; }
+    public class ChangeUser
+    {
+        public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
+        public string UserEmail { get; set; }
+    }
+
 }
