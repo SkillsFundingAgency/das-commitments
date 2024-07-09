41bb023b026ccd5a7b86376112318b878d020f78
Paul Graham <paul.graham@coprime.co.uk>
2024-07-09 16:49:10 +0100

    made Message message

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Validators/CreateCohortWithOtherPartyRequestValidator.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Validators/CreateCohortWithOtherPartyRequestValidator.cs
@@ -10,7 +10,7 @@ namespace SFA.DAS.CommitmentsV2.Validators
             RuleFor(r => r.AccountId).Must(accountId => accountId > 0).WithMessage("The Account Id valid");
             RuleFor(r => r.AccountLegalEntityId).Must(accountLegalEntityId => accountLegalEntityId > 0).WithMessage("The Account Legal Entity must be valid");
             RuleFor(r => r.ProviderId).Must(providerId => providerId > 0).WithMessage("The provider id must be valid");
-            RuleFor(r => r.Message).Must(message => message == null || message.Length <= 500).WithMessage("The Message cannot be more than 500 characters");
+            RuleFor(r => r.Message).Must(message => message == null || message.Length <= 500).WithMessage("The message cannot be more than 500 characters");
             RuleFor(r => r.UserInfo).SetValidator(new UserInfoValidator()).When(r => r.UserInfo != null);
         }
     }
