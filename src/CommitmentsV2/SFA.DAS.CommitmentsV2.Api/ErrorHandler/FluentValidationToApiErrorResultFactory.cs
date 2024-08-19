36ec48225419391c8842f1fcade7cb1de7aa466e
Paul Graham <paul.graham@coprime.co.uk>
2024-08-19 11:33:32 +0100

    Ensure we set substatus when returning a validation error

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/ErrorHandler/FluentValidationToApiErrorResultFactory.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/ErrorHandler/FluentValidationToApiErrorResultFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using SFA.DAS.CommitmentsV2.Api.Extensions;
+using SFA.DAS.CommitmentsV2.Api.Http;
+using SFA.DAS.CommitmentsV2.Api.Types.Http;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Results;
 
 namespace SFA.DAS.CommitmentsV2.Api.ErrorHandler;
@@ -8,6 +10,26 @@ public class FluentValidationToApiErrorResultFactory : IFluentValidationAutoVali
 {
     public IActionResult CreateActionResult(ActionExecutingContext context, ValidationProblemDetails? validationProblemDetails)
     {
-        return new BadRequestObjectResult(context.ModelState.CreateErrorResponse());
+        return new BadRequestObjectResultWithSubStatusSet(context.ModelState.CreateErrorResponse(), HttpSubStatusCode.DomainException);
     }
 }
+
+public class BadRequestObjectResultWithSubStatusSet : BadRequestObjectResult
+{
+    public BadRequestObjectResultWithSubStatusSet(object error, HttpSubStatusCode subStatusCode) : base(error)
+    {
+        SubStatusCode = subStatusCode;
+    }
+
+    public HttpSubStatusCode SubStatusCode { get; }
+
+    public override void OnFormatting(ActionContext context)
+    {
+        base.OnFormatting(context);
+        context.HttpContext.Response.SetSubStatusCode(SubStatusCode);
+    }
+}
+
+
+
+
