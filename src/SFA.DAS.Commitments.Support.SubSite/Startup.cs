6b8b66e135f4ffb377ddee638ccbe62d3bdeec80
Jon Harris <jon.harris@fuitsu.com>
2024-10-02 10:20:44 +0100

    Sorting out logging and a bit of refactoring

--- src/SFA.DAS.Commitments.Support.SubSite/Startup.cs
+++ src/SFA.DAS.Commitments.Support.SubSite/Startup.cs
@@ -1,93 +1,90 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SFA.DAS.Commitments.Support.SubSite.DependencyResolution;
-using System.Reflection;
-using SFA.DAS.Commitments.Support.SubSite.Caching;
-using SFA.DAS.Commitments.Support.SubSite.Extensions;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using SFA.DAS.CommitmentsV2.DependencyResolution;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using SFA.DAS.Commitments.Support.SubSite.Application.Queries.GetSupportApprenticeship;
+using SFA.DAS.Commitments.Support.SubSite.Caching;
+using SFA.DAS.Commitments.Support.SubSite.DependencyResolution;
+using SFA.DAS.Commitments.Support.SubSite.Extensions;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetApprenticeshipUpdate;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetChangeOfProviderChain;
-using SFA.DAS.CommitmentsV2.Application.Queries.GetPriceEpisodes;
 using SFA.DAS.CommitmentsV2.Application.Queries.GetOverlappingTrainingDateRequest;
+using SFA.DAS.CommitmentsV2.Application.Queries.GetPriceEpisodes;
+using SFA.DAS.CommitmentsV2.DependencyResolution;
+using ServiceCollectionExtensions = SFA.DAS.Authorization.DependencyResolution.Microsoft.ServiceCollectionExtensions;
 
-namespace SFA.DAS.Commitments.Support.SubSite
+namespace SFA.DAS.Commitments.Support.SubSite;
+
+public class Startup
 {
-    public class Startup
+    private readonly IWebHostEnvironment _env;
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        private readonly IWebHostEnvironment _env;
-        public IConfiguration Configuration { get; }
+        _configuration = configuration;
+        _env = env;
+    }
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging(builder =>
         {
-            Configuration = configuration;
-            _env = env;
-        }
+            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+        });
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        services.AddActiveDirectoryAuthentication(_configuration);
+        services.AddMvc(options =>
         {
-            services.AddRazorPages();
-
-            services.AddActiveDirectoryAuthentication(Configuration);
-            services.AddMvc(options =>
+            if (!_env.IsDevelopment())
             {
-                if (!_env.IsDevelopment())
-                {
-                    options.Filters.Add(new AuthorizeFilter("default"));
-                }
-            });
-
-            services.AddDasDistributedMemoryCache(Configuration, _env.IsDevelopment());
-            services.AddMemoryCache();
-            services.AddHealthChecks();
-            services.AddApplicationInsightsTelemetry();
-
-            DAS.Authorization.DependencyResolution.Microsoft.ServiceCollectionExtensions.AddAuthorization(services);
-            services.AddSupportConfigurationSections(Configuration);
-            services.AddDatabaseRegistration();
-
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetSupportApprenticeshipQuery).GetTypeInfo().Assembly));
-            services.AddTransient<IRequestHandler<GetApprenticeshipUpdateQuery, GetApprenticeshipUpdateQueryResult>, GetApprenticeshipUpdateQueryHandler>();
-            services.AddTransient<IRequestHandler<GetChangeOfProviderChainQuery, GetChangeOfProviderChainQueryResult>, GetChangeOfProviderChainQueryHandler>();
-            services.AddTransient<IRequestHandler<GetOverlappingTrainingDateRequestQuery, GetOverlappingTrainingDateRequestQueryResult>, GetOverlappingTrainingDateRequestQueryHandler>();
-            services.AddTransient<IRequestHandler<GetPriceEpisodesQuery, GetPriceEpisodesQueryResult>, GetPriceEpisodesQueryHandler>();
-
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-            services.AddSupportSiteDefaultServices(Configuration);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+                options.Filters.Add(new AuthorizeFilter("default"));
             }
-            else
-            {
-                app.UseHsts();
-                app.UseAuthentication();
-            }
+        });
 
-            app.UseStaticFiles();
+        services.AddDasDistributedMemoryCache(_configuration, _env.IsDevelopment());
+        services.AddMemoryCache();
+        services.AddHealthChecks();
 
-            app.UseRouting();
+        ServiceCollectionExtensions.AddAuthorization(services);
+        services.AddSupportConfigurationSections(_configuration);
+        services.AddDatabaseRegistration();
 
-            app.UseAuthorization();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetSupportApprenticeshipQuery).GetTypeInfo().Assembly));
+        services.AddTransient<IRequestHandler<GetApprenticeshipUpdateQuery, GetApprenticeshipUpdateQueryResult>, GetApprenticeshipUpdateQueryHandler>();
+        services.AddTransient<IRequestHandler<GetChangeOfProviderChainQuery, GetChangeOfProviderChainQueryResult>, GetChangeOfProviderChainQueryHandler>();
+        services.AddTransient<IRequestHandler<GetOverlappingTrainingDateRequestQuery, GetOverlappingTrainingDateRequestQueryResult>, GetOverlappingTrainingDateRequestQueryHandler>();
+        services.AddTransient<IRequestHandler<GetPriceEpisodesQuery, GetPriceEpisodesQueryResult>, GetPriceEpisodesQueryHandler>();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-            });
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
-            app.UseHealthChecks("/health");
+        services.AddSupportSiteDefaultServices(_configuration);
+
+        services.AddApplicationInsightsTelemetry();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseHsts();
+            app.UseAuthentication();
+        }
+
+        app.UseStaticFiles()
+            .UseRouting()
+            .UseAuthorization()
+            .UseEndpoints(endpoints => { endpoints.MapControllers(); })
+            .UseHealthChecks("/health");
     }
 }
