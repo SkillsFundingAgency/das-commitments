6ea81d8707235d8c39df54d430f81d0b21017a18
Jon Harris <jon.harris@fuitsu.com>
2024-10-02 10:32:12 +0100

    Merge conflicts

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Startup/IHostBuilderExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Startup/IHostBuilderExtensions.cs
@@ -4,29 +4,28 @@ using SFA.DAS.CommitmentsV2.Configuration;
 using SFA.DAS.Configuration;
 using SFA.DAS.Configuration.AzureTableStorage;
 
-namespace SFA.DAS.CommitmentsV2.Startup
+namespace SFA.DAS.CommitmentsV2.Startup;
+
+public static class HostBuilderExtensions
 {
-    public static class HostBuilderExtensions
+    public static IHostBuilder ConfigureDasAppConfiguration(this IHostBuilder hostBuilder, string[] args)
     {
-        public static IHostBuilder ConfigureDasAppConfiguration(this IHostBuilder hostBuilder, string[] args)
+        return hostBuilder.ConfigureAppConfiguration((context, builder) =>
         {
-            return hostBuilder.ConfigureAppConfiguration((context, builder) =>
-            {
-                builder.AddAzureTableStorage(CommitmentsConfigurationKeys.CommitmentsV2)
-                    .AddAzureTableStorage(CommitmentsConfigurationKeys.EncodingConfiguration)
-                    .AddJsonFile("appsettings.json", true, true)
-                    .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddEnvironmentVariables()
-                    .AddCommandLine(args);
-            });
-        }
+            builder.AddAzureTableStorage(CommitmentsConfigurationKeys.CommitmentsV2)
+                .AddAzureTableStorage(CommitmentsConfigurationKeys.EncodingConfiguration)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+        });
+    }
 
-        public static IHostBuilder UseDasEnvironment(this IHostBuilder hostBuilder)
-        {
-            var environmentName = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName);
-            var mappedEnvironmentName = DasEnvironmentName.Map[environmentName];
+    public static IHostBuilder UseDasEnvironment(this IHostBuilder hostBuilder)
+    {
+        var environmentName = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName);
+        var mappedEnvironmentName = DasEnvironmentName.Map[environmentName];
 
-            return hostBuilder.UseEnvironment(mappedEnvironmentName);
-        }
+        return hostBuilder.UseEnvironment(mappedEnvironmentName);
     }
 }
