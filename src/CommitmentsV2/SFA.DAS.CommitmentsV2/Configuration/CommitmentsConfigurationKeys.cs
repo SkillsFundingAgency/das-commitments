8e0915a11f9eadb5ef4a1c71c03b3ab91576d52b
Jon Harris <jon.harris@fuitsu.com>
2024-10-09 08:10:44 +0100

    Tidy

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Configuration/CommitmentsConfigurationKeys.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Configuration/CommitmentsConfigurationKeys.cs
@@ -4,8 +4,6 @@ public static class CommitmentsConfigurationKeys
 {
     public const string CommitmentsV2 = "SFA.DAS.CommitmentsV2";
     public static string AccountApi => $"{CommitmentsV2}:AccountApi";
-    public static string ProviderRelationshipsApi => $"{CommitmentsV2}:ProviderRelationshipsApi";
-    public static string ApprenticeshipInfoService => $"{CommitmentsV2}:ApprenticeshipInfoService";
     public static string AzureActiveDirectoryApiConfiguration => $"{CommitmentsV2}:AzureADApiAuthentication";
     public static string DatabaseConnectionString => $"{CommitmentsV2}:DatabaseConnectionString";
     public static string Features => $"{CommitmentsV2}:Features";
@@ -14,11 +12,8 @@ public static class CommitmentsConfigurationKeys
     public static string ApprovalsOuterApiConfiguration => $"{CommitmentsV2}:ApprovalsOuterApiConfiguration";
     public static string EmailOptionalConfiguration => $"{CommitmentsV2}:EmailOptionalConfiguration";
     public static string LevyTransferMatchingApiConfiguration => $"{CommitmentsV2}:LevyTransferMatchingInnerApiConfiguration";
-    public static string ProviderAccountApiConfiguration => $"{CommitmentsV2}:ProviderAccountApiConfiguration";
     public static string ReservationsClientApiConfiguration => $"{CommitmentsV2}:ReservationsClientApi";
     public static string CommitmentPaymentsWebJobConfiguration => $"{CommitmentsV2}:CommitmentPaymentsWebJobConfiguration";
     public static string RplSettingsConfiguration => $"{CommitmentsV2}:RplSettings";
     public static string ProviderUrlConfiguration => $"{CommitmentsV2}:ProviderUrl";
-
-
 }
