bc79022f9cca35abf8aa749e29ea3076782d0c71
Jon Harris <jon.harris@fuitsu.com>
2024-10-10 13:54:06 +0100

    Added comment for fix

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Data/Configuration/ApprenticeshipBuilder.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Data/Configuration/ApprenticeshipBuilder.cs
@@ -12,6 +12,8 @@ public class ApprenticeshipBuilder : IEntityTypeConfiguration<Apprenticeship>
         builder.Property(e => e.PauseDate).HasColumnType("date");
         builder.Property(e => e.StopDate).HasColumnType("date");
 
+        // Fix for "Could not save changes because the target table has database triggers" exception.
+        // https://learn.microsoft.com/en-gb/ef/core/what-is-new/ef-core-7.0/breaking-changes?tabs=v7#sqlserver-tables-with-triggers
         builder.ToTable(x => x.HasTrigger("Trg_Apprenticeship_Update"));
     }
 }
