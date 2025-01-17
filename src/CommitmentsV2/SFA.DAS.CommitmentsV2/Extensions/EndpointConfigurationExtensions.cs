33b38b6ade2eed62c1574acb3ba2ff6e0017a04e
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-01-17 13:10:53 +0000

    FLP-1027-UseServiceBusLocally

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/EndpointConfigurationExtensions.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Extensions/EndpointConfigurationExtensions.cs
@@ -2,21 +2,23 @@
 using System.Diagnostics.CodeAnalysis;
 using SFA.DAS.NServiceBus.Configuration;
 using SFA.DAS.NServiceBus.Configuration.AzureServiceBus;
+using Microsoft.Extensions.Hosting;
+using SFA.DAS.CommitmentsV2.Configuration;
 
 namespace SFA.DAS.CommitmentsV2.Extensions;
 
 [ExcludeFromCodeCoverage]
 public static class EndpointConfigurationExtensions
 {
-    public static EndpointConfiguration ConfigureServiceBusTransport(this EndpointConfiguration config, Func<string> connectionStringBuilder, bool isLocal, string learningTransportFolderPath = null)
+    public static EndpointConfiguration ConfigureServiceBusTransport(this EndpointConfiguration config, Func<string> connectionStringBuilder, bool isLocal, NServiceBusConfiguration nServiceBusConfiguration = null)
     {
-        if (isLocal)
+        if (ShouldUseServiceBus(isLocal,nServiceBusConfiguration))
         {
-            config.UseLearningTransport(learningTransportFolderPath: learningTransportFolderPath);
+            config.UseAzureServiceBusTransport(connectionStringBuilder(), s => s.AddRouting());
         }
         else
         {
-            config.UseAzureServiceBusTransport(connectionStringBuilder(), s => s.AddRouting());
+            config.UseLearningTransport(learningTransportFolderPath: nServiceBusConfiguration.LearningTransportFolderPath);
         }
 
         config.UseMessageConventions();
@@ -35,5 +37,17 @@ public static class EndpointConfigurationExtensions
         transportExtensions.Transactions(TransportTransactionMode.ReceiveOnly);
         routing?.Invoke(transportExtensions.Routing());
         return config;
+    }
+
+    private static bool ShouldUseServiceBus(bool isLocal, NServiceBusConfiguration configuration)
+    {
+        if (!isLocal)
+            return true; // Always use service bus in non-development environments
+
+        if (configuration.UseServiceBusInDev)
+            return true; // Use service bus in development if configured to do so
+
+        return false; // Otherwise, use learning transport
+
     }
 }
