49580b7ede094f1840144d89565d5dddfc965e01
HussainsGitHub <85938019+HussainsGitHub@users.noreply.github.com>
2025-04-11 12:41:58 +0100

    DASD-10922 - Added Smoke Test suite trigger

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/OverlappingTrainingDateRequestAutomaticStopAfter2Weeks/OverlappingTrainingDateRequestAutomaticStopAfter2WeeksCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SFA.DAS.CommitmentsV2.Application.Commands.OverlappingTrainingDateRequestAutomaticStopAfter2Weeks;
-
-public class OverlappingTrainingDateRequestAutomaticStopAfter2WeeksCommand: IRequest
-{
-}
--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/OverlappingTrainingDateRequestNotificationToEmployer/OverlappingTrainingDateRequestNotificationToEmployerCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SFA.DAS.CommitmentsV2.Application.Commands.OverlappingTrainingDateRequestNotificationToEmployer;
-
-public class OverlappingTrainingDateRequestNotificationToEmployerCommand : IRequest
-{
-}
