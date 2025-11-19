5dce7550f7e28a01dd32190017f6481286bf45d8
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-11-19 09:13:10 +0000

    tidy

--- src/CommitmentsV2/SFA.DAS.ReservationsV2.Api.Types/ReservationValidationResult.cs
+++ src/CommitmentsV2/SFA.DAS.ReservationsV2.Api.Types/ReservationValidationResult.cs
@@ -1,22 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿namespace SFA.DAS.Reservations.Api.Types;
 
-namespace SFA.DAS.Reservations.Api.Types
+public class ReservationValidationResult
 {
-    public class ReservationValidationResult
-    {
-        public ReservationValidationResult(IEnumerable<ReservationValidationError> errors)
-        {
-            ValidationErrors = errors.ToArray();
-        }
-
-        public ReservationValidationResult()
-        {
-            ValidationErrors = new ReservationValidationError[0];
-        }
-
-        public bool HasErrors => ValidationErrors.Length > 0;
-        public bool IsOkay => !HasErrors;
-        public ReservationValidationError[] ValidationErrors { get; set; }
-    }
+    public ReservationValidationError[] ValidationErrors { get; } = [];
 }
