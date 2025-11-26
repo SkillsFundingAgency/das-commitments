a0e7f23687058782d76c92520473267d27ce859c
Paul Graham <paul.graham@coprime.co.uk>
2025-11-26 15:19:04 +0000

    WIP

---
+++ src/SFA.DAS.Commitments.Database/Tables/Change.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Change]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,							-- Richard - this makes sense if this value is publicly exposed, but is it?
+	[ChangeTypeId] [tinyint] NOT NULL,
+	[Old] [nvarchar](100) NULL,								-- Richard Old and New - is 100 characters enough? What about data types other than string?
+	[New] [nvarchar](100) NULL,
+	[ChangeLogId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Change] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Change_ChangeLog_ChangeLogId] FOREIGN KEY ([ChangeLogId]) REFERENCES [ChangeLog] ([Id]),
+	CONSTRAINT [FK_Change_ChangeType_ChangeTypeId] FOREIGN KEY ([ChangeTypeId]) REFERENCES [ChangeType] ([Id]),
+	INDEX [IX_Change_ChangeLogId] NONCLUSTERED ([ChangeLogId] ASC)
+)
+
