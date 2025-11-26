30d05cbf2a95e2d681f7e1a47eb169d9f605124e
Witek <witek.poreda@education.gov.uk>
2025-11-26 13:27:12 +0000

    APPMAN-2036-display-RPL-options-for-draft-cohorts

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/Dtos/DraftApprenticeshipDto.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Types/Dtos/DraftApprenticeshipDto.cs
@@ -32,5 +32,6 @@ namespace SFA.DAS.CommitmentsV2.Types.Dtos
         public bool HasLearnerDataChanges { get; set; }
         public DateTime? LastLearnerDataSync { get; set; }
         public long? LearnerDataId { get; set; }
+        public int? TrainingTotalHours { get; set; }
     }
 }
