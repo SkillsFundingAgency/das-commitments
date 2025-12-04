79286f074f838114bafa71dda88c3c3b79a8b053
Paul Graham <paul.graham@coprime.co.uk>
2025-12-04 09:07:55 +0000

    updated structures

--- src/SFA.DAS.Commitments.Database/Tables/Change.sql
+++ src/SFA.DAS.Commitments.Database/Tables/Change.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Change]
 (
-    [Id] UNIQUEIDENTIFIER NOT NULL,							-- Richard - this makes sense if this value is publicly exposed, but is it?
+    [Id] BIGINT NOT NULL,							
 	[ChangeTypeId] [tinyint] NOT NULL,
-	[Old] [nvarchar](100) NULL,								-- Richard Old and New - is 100 characters enough? What about data types other than string?
+	[Old] [nvarchar](100) NULL,						
 	[New] [nvarchar](100) NULL,
 	[ChangeLogId] UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT [PK_Change] PRIMARY KEY CLUSTERED ([Id] ASC),
--- src/SFA.DAS.Commitments.Database/Tables/ChangeLog.sql
+++ src/SFA.DAS.Commitments.Database/Tables/ChangeLog.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[ChangeLog]
 (
-    [Id] UNIQUEIDENTIFIER NOT NULL,                             -- Richard - this makes sense if this value is publicly exposed
-    [CreatedOn] DATETIME2 NOT NULL DEFAULT (GETDATE()),         -- Richard - added default value and set type to DATETIME2 
-	[Status] [tinyint] NOT NULL,                                -- Richard - Nothing in the HLD states what this value should be
-    [LearningUri] UNIQUEIDENTIFIER NOT NULL,                    -- Richard - I assume this is a UNIQUEIDENTIFIER as per LearningRecord (for approved changes?)
-    [UKPRN] BIGINT NOT NULL,                                    -- Richard - changed from varchar(100) to BIGINT
-    [AgreementId] NVARCHAR(6) NOT NULL,                         -- Richard - changed from VARCHAR(10) to NVARCHAR(6). Should we be storing the AccountLegalEntityId here?
-    [ApprovalUserId] UNIQUEIDENTIFIER NULL,                     -- Richard - does this record who approved or rejected the change?
+    [Id] UNIQUEIDENTIFIER NOT NULL,                             
+    [CreatedOn] DATETIME2 NOT NULL DEFAULT (GETDATE()),         
+	[Status] [tinyint] NOT NULL,                                
+    [LearningKey] UNIQUEIDENTIFIER NOT NULL,                    
+    [UKPRN] BIGINT NOT NULL,                                    
+    [AgreementId] NVARCHAR(6) NOT NULL,                         
+    [ApprovalUserId] UNIQUEIDENTIFIER NULL,                     
     CONSTRAINT [PK_ChangeLog] PRIMARY KEY CLUSTERED ([Id] ASC),
 )
