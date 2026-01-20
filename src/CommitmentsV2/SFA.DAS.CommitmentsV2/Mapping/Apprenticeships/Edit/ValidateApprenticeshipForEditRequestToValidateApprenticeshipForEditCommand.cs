dfb36171132fabbb51eb720b17576ba934ade92c
Paul Graham <paul.graham@coprime.co.uk>
2026-01-20 14:21:17 +0000

    fix incorrect update

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Mapping/Apprenticeships/Edit/ValidateApprenticeshipForEditRequestToValidateApprenticeshipForEditCommand.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Mapping/Apprenticeships/Edit/ValidateApprenticeshipForEditRequestToValidateApprenticeshipForEditCommand.cs
@@ -13,6 +13,28 @@ public class ValidateApprenticeshipForEditRequestToValidateApprenticeshipForEdit
         {
             ApprenticeshipValidationRequest = new EditApprenticeshipValidationRequest
             {
+                ProviderId = request.ProviderId,
+                EmployerAccountId = request.EmployerAccountId,
+                ApprenticeshipId = request.ApprenticeshipId,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                DateOfBirth = request.DateOfBirth,
+                ULN = request.ULN,
+                Cost = request.Cost,
+                EmployerReference = request.EmployerReference,
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                DeliveryModel = request.DeliveryModel,
+                CourseCode = request.TrainingCode,
+                Version = request.Version,
+                Option = request.Option,
+                ProviderReference = request.ProviderReference,
+                Email = request.Email,
+                EmploymentEndDate = request.EmploymentEndDate,
+                EmploymentPrice = request.EmploymentPrice,
+                MinimumAgeAtApprenticeshipStart = request.MinimumAgeAtApprenticeshipStart,
+                MaximumAgeAtApprenticeshipStart = request.MaximumAgeAtApprenticeshipStart,
+                Party = request.Party
             }
         });
     }
