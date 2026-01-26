5ef23e078612dce187988b501b15369ef2ddad79
Paul Graham <paul.graham@coprime.co.uk>
2026-01-26 09:59:50 +0000

    added controller tests

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.UnitTests/Controllers/ApprovalsControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Api.Controllers;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
+using SFA.DAS.CommitmentsV2.Application.Commands.CocApprovals;
+using SFA.DAS.CommitmentsV2.Shared.Interfaces;
+
+namespace SFA.DAS.CommitmentsV2.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    [Parallelizable]
+    public class ApprovalsControllerTests
+    {
+        private Fixture _fixture;
+        private Mock<IMediator> _mediator;
+        private Mock<IModelMapper> _mapper;
+        private ApprovalsController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _mediator = new Mock<IMediator>();
+            _mapper = new Mock<IModelMapper>();
+            _controller = new ApprovalsController(_mediator.Object, _mapper.Object, Mock.Of<ILogger<ApprovalsController>>());
+        }
+
+        [Test]
+        public async Task PostApprovals_Processes_The_Request_Then_ReturnsResponse()
+        {
+            // Arrange
+            var request = _fixture.Create<CocApprovalRequest>();
+            var command = _fixture.Create<PostCocApprovalCommand>();
+            var commandResult = _fixture.Create<CocApprovalResult>();
+
+            _mapper.Setup(m => m.Map<PostCocApprovalCommand>(request)).ReturnsAsync(command);
+            _mediator.Setup(m => m.Send(command, It.IsAny<CancellationToken>())).ReturnsAsync(commandResult);
+
+            // Act
+            var result = await _controller.PostApprovals(Guid.NewGuid(), request);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<OkObjectResult>();
+            var jsonResult = result as OkObjectResult;
+            jsonResult.StatusCode.Should().Be(200);
+            jsonResult.Value.Should().BeEquivalentTo(commandResult.Items);
+        }
+    }
+}
--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprovalsController.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api/Controllers/ApprovalsController.cs
@@ -1,5 +1,5 @@
-﻿using SFA.DAS.CommitmentsV2.Api.Types.Requests;
-using SFA.DAS.CommitmentsV2.Application.Commands.CocApprovals;
+﻿using Microsoft.AspNetCore.Authorization;
+using SFA.DAS.CommitmentsV2.Api.Types.Requests;
 using SFA.DAS.CommitmentsV2.Shared.Interfaces;
 
 namespace SFA.DAS.CommitmentsV2.Api.Controllers;
@@ -8,14 +8,13 @@ namespace SFA.DAS.CommitmentsV2.Api.Controllers;
 [ApiController]
 public class ApprovalsController(IMediator mediator, IModelMapper modelMapper, ILogger<ApprovalsController> logger) : ControllerBase
 {
-    //[Authorize]
+    [Authorize]
     [HttpPost("{learningKey}")]
     public async Task<ActionResult> PostApprovals([FromRoute] Guid learningKey, [FromBody] CocApprovalRequest request)
     {
-        var command = await modelMapper.Map<PostCocApprovalCommand>(request);
+        var command = await modelMapper.Map<    >(request);
         var result = await mediator.Send(command);
-
+        logger.LogInformation("=== COMMITMENTS API: ApprovalsController.PostApprovals completed === Returning status of {0}", result?.Status);
         return Ok(result.Items);
     }
 }
-
