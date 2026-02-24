e8b8a1fc7af70812abc5ef84ea685adced539b90
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-24 12:43:03 +0000

    switch to TinyInt LearningType for consistency

--- src/SFA.DAS.Commitments.Database/UserDefinedTypes/Courses.sql
+++ src/SFA.DAS.Commitments.Database/UserDefinedTypes/Courses.sql
@@ -3,7 +3,7 @@ CREATE TYPE [dbo].[Courses] AS TABLE
     [LarsCode] VARCHAR(20) NOT NULL,
     [Title] NVARCHAR(500) NOT NULL,
     [Level] VARCHAR(20) NOT NULL,
-    [LearningType] NVARCHAR(50) NULL,
+    [LearningType] TINYINT NULL,
     [MaxFunding] INT NULL,
     [EffectiveFrom] DATETIME NULL,
     [EffectiveTo] DATETIME NULL
