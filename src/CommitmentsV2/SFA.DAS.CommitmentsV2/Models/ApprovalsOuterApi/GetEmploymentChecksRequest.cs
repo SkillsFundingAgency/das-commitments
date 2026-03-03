c67bd0ff7556bfc3878981de40c4d424e9b96e33
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-03-03 22:40:09 +0000

    comma separate id's

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprovalsOuterApi/GetEmploymentChecksRequest.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Models/ApprovalsOuterApi/GetEmploymentChecksRequest.cs
@@ -4,5 +4,5 @@ public class GetEmploymentChecksRequest(IReadOnlyList<long> apprenticeshipIds) :
 {
     private IReadOnlyList<long> ApprenticeshipIds { get; } = apprenticeshipIds;
 
-    public string GetUrl => "api/EmploymentChecks?" + string.Join("&", ApprenticeshipIds.Select(id => "apprenticeshipIds=" + id));
+    public string GetUrl => "api/EmploymentChecks?apprenticeshipIds=" + Uri.EscapeDataString(string.Join(",", ApprenticeshipIds));
 }
