d5f7dde0e51b84820254b04396196679fb3cfebc
Witek <witek.poreda@education.gov.uk>
2026-03-06 14:56:03 +0000

    APPMAN-2340 add courses controller

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.Api.Types/Responses/GetAllCoursesResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.CommitmentsV2.Api.Types.Responses;
+
+public class GetAllCoursesResponse
+{
+    public List<Course> Courses { get; set; }
+}
+
+public class Course
+{
+    public string LarsCode { get; set; }
+    public string Title { get; set; }
+    public string Level { get; set; }
+    public string LearningType { get; set; }
+    public int MaxFunding { get; set; }
+    public DateTime? EffectiveFrom { get; set; }
+    public DateTime? EffectiveTo { get; set; }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Queries/GetAllCourses/GetAllCoursesQueryResult.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.CommitmentsV2.Api.Types.Responses;
+
+namespace SFA.DAS.CommitmentsV2.Application.Queries.GetAllCourses;
+
+public class GetAllCoursesQueryResult
+{
+    public List<Course> Courses { get; set; }
+
+}
