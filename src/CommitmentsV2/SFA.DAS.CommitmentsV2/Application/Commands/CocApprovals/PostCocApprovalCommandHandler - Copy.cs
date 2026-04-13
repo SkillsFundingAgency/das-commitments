8dc1d5897452e86fb8ca314a1c63ed9531e8d5dc
Paul Graham <paul.graham@coprime.co.uk>
2026-04-13 16:21:37 +0100

    wip

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/CocApprovals/PostCocApprovalCommandHandler - Copy.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Application.Commands.CocApprovals;
+using SFA.DAS.CommitmentsV2.Domain.Exceptions;
+using SFA.DAS.CommitmentsV2.Domain.Interfaces;
+using SFA.DAS.CommitmentsV2.Models;
+
+namespace SFA.DAS.CommitmentsV2.Application.Commands.EditApprenticeship;
+
+public class PostCocApprovalCommandHandler(
+    Lazy<ProviderCommitmentsDbContext> dbContext,
+    ICocApprovalRules cocApprovalRules,
+    ILogger<PostCocApprovalCommandHandler> logger)
+    : IRequestHandler<PostCocApprovalCommand, CocApprovalResult>
+{
+    public async Task<CocApprovalResult> Handle(PostCocApprovalCommand postCommand, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("PostCocApprovalCommandHandler.Handle called");
+
+        if (postCommand?.CocApprovalDetails == null)
+        {
+            throw new ArgumentNullException(nameof(postCommand));
+        }
+
+        var cocApprovalDetails = postCommand.CocApprovalDetails;
+
+        var db = dbContext.Value;
+        var existingApprovalRequests = db.ApprovalRequests.Where(r => r.LearningKey == cocApprovalDetails.LearningKey && r.Status == CocApprovalResultStatus.Pending);
+
+        if (existingApprovalRequests.Any())
+        {
+            throw new DomainException("LearningKey", "An approval request for this learning key already exists.");
+        }
+
+        var approvalState = cocApprovalRules.DetermineApprovalState(cocApprovalDetails);
+
+        db.ApprovalRequests.Add(approvalState.ApprovalRequest);
+
+        return approvalState.ApprovalResult;
+    }
+}
