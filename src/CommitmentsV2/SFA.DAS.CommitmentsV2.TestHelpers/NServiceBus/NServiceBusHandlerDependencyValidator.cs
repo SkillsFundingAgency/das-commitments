4d72e81ec23c3a4f0268c79f4f6b9eeb89a3df34
Paul Graham <paul.graham@coprime.co.uk>
2026-06-15 16:44:13 +0100

    remove complexities

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.TestHelpers/NServiceBus/NServiceBusHandlerDependencyValidator.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.TestHelpers/NServiceBus/NServiceBusHandlerDependencyValidator.cs
@@ -1,69 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
 
-namespace SFA.DAS.CommitmentsV2.TestHelpers.NServiceBus;
+//namespace SFA.DAS.CommitmentsV2.TestHelpers.NServiceBus;
 
-public static class NServiceBusHandlerDependencyValidator
-{
-    private static readonly HashSet<string> ForbiddenDependencyTypeNames = new(StringComparer.Ordinal)
-    {
-        "NServiceBus.IMessageSession",
-        "NServiceBus.IEndpointInstance"
-    };
+//public static class NServiceBusHandlerDependencyValidator
+//{
+//    private static readonly HashSet<string> ForbiddenDependencyTypeNames = new(StringComparer.Ordinal)
+//    {
+//        "NServiceBus.IMessageSession",
+//        "NServiceBus.IEndpointInstance"
+//    };
 
-    public static IReadOnlyCollection<HandlerDependencyViolation> GetViolations(params Assembly[] assemblies) =>
-        assemblies.SelectMany(GetViolationsFromAssembly).ToList();
+//    public static IReadOnlyCollection<HandlerDependencyViolation> GetViolations(params Assembly[] assemblies) =>
+//        assemblies.SelectMany(GetViolationsFromAssembly).ToList();
 
-    private static IEnumerable<HandlerDependencyViolation> GetViolationsFromAssembly(Assembly assembly)
-    {
-        foreach (var handlerType in GetHandlerTypes(assembly))
-        {
-            foreach (var constructor in handlerType.GetConstructors(BindingFlags.Public | BindingFlags.Instance))
-            {
-                foreach (var parameter in constructor.GetParameters())
-                {
-                    if (IsForbiddenDependency(parameter.ParameterType))
-                    {
-                        yield return new HandlerDependencyViolation(handlerType, parameter.ParameterType);
-                    }
-                }
-            }
-        }
-    }
+//    private static IEnumerable<HandlerDependencyViolation> GetViolationsFromAssembly(Assembly assembly)
+//    {
+//        foreach (var handlerType in GetHandlerTypes(assembly))
+//        {
+//            foreach (var constructor in handlerType.GetConstructors(BindingFlags.Public | BindingFlags.Instance))
+//            {
+//                foreach (var parameter in constructor.GetParameters())
+//                {
+//                    if (IsForbiddenDependency(parameter.ParameterType))
+//                    {
+//                        yield return new HandlerDependencyViolation(handlerType, parameter.ParameterType);
+//                    }
+//                }
+//            }
+//        }
+//    }
 
-    private static IEnumerable<Type> GetHandlerTypes(Assembly assembly)
-    {
-        Type[] types;
+//    private static IEnumerable<Type> GetHandlerTypes(Assembly assembly)
+//    {
+//        Type[] types;
 
-        try
-        {
-            types = assembly.GetTypes();
-        }
-        catch (ReflectionTypeLoadException ex)
-        {
-            types = ex.Types.Where(type => type is not null).ToArray()!;
-        }
+//        try
+//        {
+//            types = assembly.GetTypes();
+//        }
+//        catch (ReflectionTypeLoadException ex)
+//        {
+//            types = ex.Types.Where(type => type is not null).ToArray()!;
+//        }
 
-        return types.Where(IsNServiceBusHandler);
-    }
+//        return types.Where(IsNServiceBusHandler);
+//    }
 
-    private static bool IsNServiceBusHandler(Type type) =>
-        type is { IsAbstract: false, IsInterface: false } &&
-        type.GetInterfaces().Any(IsIHandleMessagesInterface);
+//    private static bool IsNServiceBusHandler(Type type) =>
+//        type is { IsAbstract: false, IsInterface: false } &&
+//        type.GetInterfaces().Any(IsIHandleMessagesInterface);
 
-    private static bool IsIHandleMessagesInterface(Type type) =>
-        type.IsGenericType &&
-        type.GetGenericTypeDefinition().FullName == "NServiceBus.IHandleMessages`1";
+//    private static bool IsIHandleMessagesInterface(Type type) =>
+//        type.IsGenericType &&
+//        type.GetGenericTypeDefinition().FullName == "NServiceBus.IHandleMessages`1";
 
-    private static bool IsForbiddenDependency(Type type) =>
-        type.FullName is not null && ForbiddenDependencyTypeNames.Contains(type.FullName);
-}
+//    private static bool IsForbiddenDependency(Type type) =>
+//        type.FullName is not null && ForbiddenDependencyTypeNames.Contains(type.FullName);
+//}
 
-public sealed record HandlerDependencyViolation(Type HandlerType, Type ForbiddenDependencyType)
-{
-    public override string ToString() =>
-        $"{HandlerType.FullName} injects {ForbiddenDependencyType.FullName} in its constructor. " +
-        "Use the IMessageHandlerContext parameter in Handle instead.";
-}
+//public sealed record HandlerDependencyViolation(Type HandlerType, Type ForbiddenDependencyType)
+//{
+//    public override string ToString() =>
+//        $"{HandlerType.FullName} injects {ForbiddenDependencyType.FullName} in its constructor. " +
+//        "Use the IMessageHandlerContext parameter in Handle instead.";
+//}
