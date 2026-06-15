da9b42a27a2a2f6bbc848fd33a53af0212bdbe5b
Paul Graham <paul.graham@coprime.co.uk>
2026-06-15 14:24:18 +0100

    SEE IF THIS GET REGISTERED

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/EventHandlers/TestEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.CommitmentsV2.MessageHandlers.EventHandlers;
+
+public class TestEventHandler() : IHandleMessages<TestEvent>
+{
+    public Task Handle(TestEvent message, IMessageHandlerContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+
+public class TestEvent
+{
+    public int? Id { get; set; }
+}
