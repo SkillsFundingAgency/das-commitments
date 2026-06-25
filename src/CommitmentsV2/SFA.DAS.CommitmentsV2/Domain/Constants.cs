e3c738a51d67377b61379a0210e202661c6c8c40
Paul Graham <paul.graham@coprime.co.uk>
2026-06-25 09:57:47 +0100

    Added description

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Domain/Constants.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Domain/Constants.cs
@@ -25,4 +25,23 @@ public static class Constants
         public const int ProviderReference = 50;
         public const int Uln = 50;
     }
+
+    public static Dictionary<short, string> IlrWithdrawalReasons = new Dictionary<short, string>
+    {
+        { 2, "Learner has transferred to another provider" },
+        { 3, "Learner injury / illness" },
+        { 7, "Learner has transferred between providers due to intervention by or with the written agreement of the ESFA" },
+        { 29, "Learner has been made redundant" },
+        { 40, "Learner has transferred to a new learning aim with the same provider" },
+        { 41, "Learner has transferred to another provider to undertake learning that meets a specific government strategy" },
+        { 42, "Academic failure / left in bad standing / not permitted to progress – HE learning aims only" },
+        { 43, "Financial reasons" },
+        { 44, "Other personal reasons" },
+        { 45, "Written off after lapse of time – HE learning aims only" },
+        { 46, "Exclusion" },
+        { 47, "Learner has transferred to another provider due to merger" },
+        { 48, "Industry placement learner has withdrawn due to circumstances outside the providers' control" },
+        { 97, "Other" },
+        { 98, "Reason not known" }
+    };
 }
