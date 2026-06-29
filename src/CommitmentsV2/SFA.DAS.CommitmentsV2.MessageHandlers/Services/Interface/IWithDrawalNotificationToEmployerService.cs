09ae1da27434105d8d5af3da2b25d00fa2445061
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-06-29 16:22:34 +0100

    refactored code

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2.MessageHandlers/Services/Interface/IWithDrawalNotificationToEmployerService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SFA.DAS.CommitmentsV2.MessageHandlers.Services.Interface;
-
-public interface IWithDrawalNotificationToEmployerService
-{
-    Task SendWithdrawalNotificationToEmployer(long apprenticeshipId, IMessageHandlerContext context);
-}
