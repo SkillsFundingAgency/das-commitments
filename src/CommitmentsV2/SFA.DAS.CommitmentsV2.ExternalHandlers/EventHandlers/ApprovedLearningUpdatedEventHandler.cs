eb09d7f00f28528a9263983760f2a0fa940c5732
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-08-12 16:57:05 +0100

    updated the apprroved learner details

---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.ExternalHandlers/EventHandlers/ApprovedLearningUpdatedEventHandler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Domain.Exceptions;
+using SFA.DAS.CommitmentsV2.Domain.Interfaces;
+using SFA.DAS.CommitmentsV2.ExternalHandlers.LearningEvents;
+using SFA.DAS.CommitmentsV2.Types;
+
+namespace SFA.DAS.CommitmentsV2.ExternalHandlers.EventHandlers;
+
+public class ApprovedLearningUpdatedEventHandler(
+    Lazy<ProviderCommitmentsDbContext> dbContext,
+    ILogger<ApprovedLearningUpdatedEventHandler> logger)
+
+{
+    public async Task Handle(ApprovedLearningUpdatedEvent message)
+    {
+        try
+        {
+            logger.LogInformation("Started executing {Event}", nameof(ApprovedLearningUpdatedEvent));
+
+            if (message is null)
+            {
+                logger.LogInformation(" {Event} received null message : {Event}", nameof(ApprovedLearningUpdatedEvent), message == null);
+                return;
+            }
+
+            logger.LogInformation("ApprovedLearningUpdatedEvent for ApprenticeshipId {ApprenticeshipId} with LearningKey {LearningKey}",
+                message.ApprenticeshipId, message.LearningKey);
+            var db = dbContext.Value;
+            var apprentice = await db.Apprenticeships
+                .Include(a => a.Cohort)
+                    .ThenInclude(c => c.Provider)
+                .SingleOrDefaultAsync(t => t.Id == message.ApprenticeshipId);
+
+            if (apprentice == null)
+            {
+                throw new DomainException(nameof(apprentice), $"Apprenticeship with Id {message.ApprenticeshipId} not found.");
+            }
+
+            foreach (var change in message.Changes)
+            {
+                if (change?.Data == null) continue;
+
+                if (Enum.TryParse<ApprovedLearnerChangeType>(change.ChangeType, ignoreCase: true, out var changeType))
+                {
+                    switch (changeType)
+                    {
+                        case ApprovedLearnerChangeType.Firstname:
+                            apprentice.FirstName = change.Data.New;
+                            break;
+
+                        case ApprovedLearnerChangeType.Surname:
+                            apprentice.LastName = change.Data.New;
+                            break;
+
+                        case ApprovedLearnerChangeType.DOB:
+                            apprentice.DateOfBirth = ParseDate(change.Data.New);
+                            break;
+
+                        case ApprovedLearnerChangeType.PlannedStartDate:
+                            apprentice.StartDate = ParseDate(change.Data.New);
+                            break;
+
+                        case ApprovedLearnerChangeType.PlannedEndDate:
+                            apprentice.EndDate = ParseDate(change.Data.New);
+                            break;
+
+                        case ApprovedLearnerChangeType.Email:
+                            apprentice.Email = change.Data.New;
+                            break;
+                    }
+                }
+                else
+                {
+                    logger.LogWarning("Unknown change type '{ChangeType}' for ApprenticeshipId {ApprenticeshipId}", change.ChangeType, message.ApprenticeshipId);
+                }
+            }
+
+            logger.LogInformation(" Executing {Event} completed", nameof(ApprovedLearningUpdatedEvent));
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error processing ApprovedLearningUpdatedEventHandler for ApprenticeshipId {0}", message.ApprenticeshipId);
+            throw;
+        }
+    }
+
+    private static DateTime? ParseDate(string dateString)
+    {
+        if (string.IsNullOrWhiteSpace(dateString))
+        {
+            return null;
+        }
+
+        if (DateTime.TryParse(dateString, out var parsedDate))
+        {
+            return parsedDate;
+        }
+        return null;
+    }
+}
---
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2.ExternalMessageHandlers.UnitTests/EventHandlers/ApprovedLearningUpdatedEventHandlerTests.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CommitmentsV2.Data;
+using SFA.DAS.CommitmentsV2.Domain.Exceptions;
+using SFA.DAS.CommitmentsV2.ExternalHandlers.EventHandlers;
+using SFA.DAS.CommitmentsV2.ExternalHandlers.LearningEvents;
+using SFA.DAS.CommitmentsV2.Models;
+using SFA.DAS.CommitmentsV2.Types;
+using SFA.DAS.UnitOfWork.Context;
+
+namespace SFA.DAS.CommitmentsV2.ExternalMessageHandlers.UnitTests.EventHandlers;
+
+public class ApprovedLearningUpdatedEventHandlerTests
+{
+    public ApprovedLearningUpdatedEventHandlerTestsFixture _fixture;
+
+    [SetUp]
+    public void Arrange()
+    {
+        _fixture = new ApprovedLearningUpdatedEventHandlerTestsFixture();
+    }
+
+    [TearDown]
+    public void TearDown() => _fixture.Dispose();
+
+    [Test]
+    public async Task Handle_WhenApprovedLearningUpdatedEventReceived_ChangeReceived()
+    {
+        await _fixture.SetEvent().Handle();
+        _fixture.VerifyLearnerUpdated();
+    }
+
+    [Test]
+    public async Task ThenLogsWarning_WhenUnknownChangeTypeReceived()
+    {
+        await _fixture.SetEventWithUnKnownChangeType().Handle();
+        _fixture.VerifyLoggerWarning("Unknown change type");
+    }
+
+    [Test]
+    public async Task ThenLogsMessage_WhenEventIsNull()
+    {
+        await _fixture.Handle();
+        _fixture.VerifyLoggerInformation($"{nameof(ApprovedLearningUpdatedEvent)} received null message");
+    }
+
+    [Test]
+    public async Task ThenThrowsDomainException_WhenApprenticeshipNotfound()
+    {
+        var act = async () => await _fixture.SetEventApprenticeshipId(_fixture.fixture.Create<long>()).Handle();
+        await act.Should().ThrowAsync<DomainException>().Where(ex => ex.DomainErrors.First().ErrorMessage.Contains("not found"));
+    }
+}
+
+public class ApprovedLearningUpdatedEventHandlerTestsFixture
+{
+    public Fixture fixture { get; set; }
+    private ProviderCommitmentsDbContext _dbContext;
+    private readonly Mock<ILogger<ApprovedLearningUpdatedEventHandler>> _mockLogger;
+    private ApprovedLearningUpdatedEventHandler _handler;
+    private ApprovedLearningUpdatedEvent _event;
+    public UnitOfWorkContext UnitOfWorkContext { get; set; }
+    public long apprenticeshipId { get; set; }
+
+    public ApprovedLearningUpdatedEventHandlerTestsFixture()
+    {
+        fixture = new Fixture();
+        _mockLogger = new Mock<ILogger<ApprovedLearningUpdatedEventHandler>>();
+        UnitOfWorkContext = new UnitOfWorkContext();
+
+        _dbContext = new ProviderCommitmentsDbContext(new DbContextOptionsBuilder<ProviderCommitmentsDbContext>()
+                                            .UseInMemoryDatabase(Guid.NewGuid().ToString(), b => b.EnableNullChecks(false))
+                                            .Options);
+
+        apprenticeshipId = fixture.Create<long>();
+
+        var provider = new Provider()
+        {
+            UkPrn = 12345,
+            Name = "Test Provider"
+        };
+
+        var cohort = new Cohort
+        {
+            Id = fixture.Create<long>(),
+            WithParty = Party.Provider,
+            Reference = fixture.Create<string>(),
+            Provider = provider,
+            EmployerAccountId = 101
+        };
+
+        var Apprenticeship = new Apprenticeship
+        {
+            Id = apprenticeshipId,
+            HasLearnerDataChanges = false,
+            FirstName = "Test",
+            LastName = "User",
+            DateOfBirth = DateTime.UtcNow.AddYears(-20),
+            Uln = fixture.Create<long>().ToString(),
+            Cohort = cohort,
+            StartDate = DateTime.UtcNow.AddMonths(1),
+            EndDate = DateTime.UtcNow.AddMonths(13)
+        };
+
+        _dbContext.Cohorts.Add(cohort);
+        _dbContext.Apprenticeships.Add(Apprenticeship);
+        _dbContext.SaveChanges();
+        _handler = new ApprovedLearningUpdatedEventHandler(new Lazy<ProviderCommitmentsDbContext>(() => _dbContext),
+            _mockLogger.Object);
+    }
+
+    public ApprovedLearningUpdatedEventHandlerTestsFixture SetEventApprenticeshipId(long id)
+    {
+        SetEvent();
+        _event.ApprenticeshipId = id;
+        return this;
+    }
+
+    public ApprovedLearningUpdatedEventHandlerTestsFixture SetEvent()
+    {
+        _event = new ApprovedLearningUpdatedEvent()
+        {
+            ApprenticeshipId = apprenticeshipId,
+            LearningKey = Guid.NewGuid(),
+            Changes =
+             [
+                 new() {
+                     ChangeType = "Firstname",
+                     Data = new ApprenticeshipData
+                     {
+                         Old = "Test",
+                         New = "Updated"
+                     }
+                 },
+                    new() {
+                        ChangeType = "Surname",
+                        Data = new ApprenticeshipData
+                        {
+                            Old = "User",
+                            New = "Updated"
+                        }
+                    },
+                   new() {
+                        ChangeType = "Email",
+                        Data = new ApprenticeshipData
+                        {
+                            Old = "test.user@domain.com",
+                            New = "updated.user@domain.com"
+                        }
+                   },
+
+                   new() { ChangeType = "DOB", Data = new ApprenticeshipData { Old = DateTime.Now.AddYears(-20).ToShortDateString(), New = DateTime.Now.AddYears(-1).ToShortDateString()} },
+                   new() { ChangeType = "PlannedStartDate", Data = new ApprenticeshipData { Old = DateTime.Now.AddMonths(2).ToShortDateString(), New = DateTime.Now.AddMonths(4).ToShortDateString()} },
+                   new() { ChangeType = "PlannedEndDate", Data = new ApprenticeshipData { Old = DateTime.Now.AddMonths(8).ToShortDateString(), New = DateTime.Now.AddMonths(11).ToShortDateString()} }
+            ]
+        };
+        return this;
+    }
+
+    public ApprovedLearningUpdatedEventHandlerTestsFixture SetEventWithUnKnownChangeType()
+    {
+        _event = new ApprovedLearningUpdatedEvent()
+        {
+            ApprenticeshipId = apprenticeshipId,
+            LearningKey = Guid.NewGuid(),
+            Changes =
+            [
+                new() {
+                     ChangeType = "TestName",
+                     Data = new ApprenticeshipData
+                     {
+                         Old = "Test",
+                         New = "Updated"
+                     }
+                 }]
+        };
+
+        return this;
+    }
+
+    public async Task Handle()
+    {
+        await _handler.Handle(_event);
+    }
+
+    public void VerifyLearnerUpdated()
+    {
+        var updatedApprenticeship = _dbContext.Apprenticeships.Find(apprenticeshipId);
+        updatedApprenticeship.Should().NotBeNull();
+        updatedApprenticeship.FirstName.Should().Be(GetValue(ApprovedLearnerChangeType.Firstname));
+        updatedApprenticeship.LastName.Should().Be(GetValue(ApprovedLearnerChangeType.Surname));
+        updatedApprenticeship.Email.Should().Be(GetValue(ApprovedLearnerChangeType.Email));
+        updatedApprenticeship.DateOfBirth.Should().Be(ParseDate(GetValue(ApprovedLearnerChangeType.DOB)));
+        updatedApprenticeship.StartDate.Should().Be(ParseDate(GetValue(ApprovedLearnerChangeType.PlannedStartDate)));
+        updatedApprenticeship.EndDate.Should().Be(ParseDate(GetValue(ApprovedLearnerChangeType.PlannedEndDate)));
+    }
+
+    private string GetValue(ApprovedLearnerChangeType changeType)
+    {
+        return _event.Changes.Where(t => t.ChangeType == Enum.GetName(changeType)).Select(t => t.Data.New).FirstOrDefault();
+    }
+
+    public void VerifyLoggerInformation(string message)
+    {
+        _mockLogger.Verify(x => x.Log(
+            LogLevel.Information,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => v.ToString().Contains(message)),
+            null,
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
+    }
+
+    public void VerifyLoggerWarning(string message)
+    {
+        _mockLogger.Verify(x => x.Log(
+            LogLevel.Warning,
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => v.ToString().Contains(message)),
+            null,
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
+    }
+
+    public void Dispose() => _dbContext?.Dispose();
+
+    public DateTime? ParseDate(string dateString)
+    {
+        if (DateTime.TryParse(dateString, out var parsedDate))
+        {
+            return parsedDate;
+        }
+        return null;
+    }
+}
