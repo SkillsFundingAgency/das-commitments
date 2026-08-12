66bdc61d613bc1ba0cc383f33bad70395778bcfa
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-08-12 14:50:15 +0100

    auto reject transfer requests to employers who have switched to non-levy

--- src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/AddTransferRequest/AddTransferRequestCommandHandler.cs
+++ src/CommitmentsV2/SFA.DAS.CommitmentsV2/Application/Commands/AddTransferRequest/AddTransferRequestCommandHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SFA.DAS.CommitmentsV2.Data;
@@ -5,6 +6,7 @@ using SFA.DAS.CommitmentsV2.Data.Extensions;
 using SFA.DAS.CommitmentsV2.Domain.Entities;
 using SFA.DAS.CommitmentsV2.Domain.Interfaces;
 using SFA.DAS.CommitmentsV2.Models.ApprovalsOuterApi;
+using SFA.DAS.CommitmentsV2.Types;
 
 namespace SFA.DAS.CommitmentsV2.Application.Commands.AddTransferRequest;
 
@@ -23,8 +25,10 @@ public class AddTransferRequestCommandHandler(
 
             var cohort = await db.GetCohortAggregate(request.CohortId, cancellationToken: cancellationToken);
 
+            var transferSenderIsNonLevy = await IsTransferSenderNonLevy(db, cohort.TransferSenderId, cancellationToken);
+
             var autoApproval = false;
-            if (cohort.PledgeApplicationId.HasValue)
+            if (!transferSenderIsNonLevy && cohort.PledgeApplicationId.HasValue)
             {
                 var apiRequest = new GetPledgeApplicationRequest(cohort.PledgeApplicationId.Value);
                 var pledgeApplication = await apiClient.Get<PledgeApplication>(apiRequest);
@@ -40,6 +44,18 @@ public class AddTransferRequestCommandHandler(
                 request.LastApprovedByParty,
                 autoApproval);
 
+            if (transferSenderIsNonLevy)
+            {
+                var transferRequest = cohort.TransferRequests.Single(tr => tr.Status == TransferApprovalStatus.Pending);
+                transferRequest.Cohort = cohort;
+                transferRequest.Reject(UserInfo.System, DateTime.UtcNow);
+
+                logger.LogInformation(
+                    "Auto-rejected transfer request for cohort {CohortId} because transfer sender {TransferSenderId} is NonLevy",
+                    cohort.Id,
+                    cohort.TransferSenderId);
+            }
+
             await db.SaveChangesAsync(cancellationToken);
         }
         catch (Exception e)
@@ -47,5 +63,23 @@ public class AddTransferRequestCommandHandler(
             logger.LogError(e, "Error Adding Transfer Request");
             throw;
         }
+    }
+
+    private static async Task<bool> IsTransferSenderNonLevy(
+        ProviderCommitmentsDbContext db,
+        long? transferSenderId,
+        CancellationToken cancellationToken)
+    {
+        if (!transferSenderId.HasValue)
+        {
+            return false;
+        }
+
+        var levyStatus = await db.Accounts
+            .Where(a => a.Id == transferSenderId.Value)
+            .Select(a => (ApprenticeshipEmployerType?)a.LevyStatus)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        return levyStatus == ApprenticeshipEmployerType.NonLevy;
     }
 }
